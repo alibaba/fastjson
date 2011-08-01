052f82bae217b7d4cb7592c3738cfeedc0ab736e
wenshao <szujobs@hotmail.com>
2011-08-01 04:32:08 +0000

    参考CgLib实现classloader
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1261 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/main/java/com/alibaba/fastjson/util/ASMClassLoader.java
+++ src/main/java/com/alibaba/fastjson/util/ASMClassLoader.java
@@ -1,15 +1,29 @@
 package com.alibaba.fastjson.util;
 
+import java.security.PrivilegedAction;
+
 import com.alibaba.fastjson.JSONException;
 
 public class ASMClassLoader extends ClassLoader {
+
+    private static java.security.ProtectionDomain DOMAIN;
+
+    static {
+
+        DOMAIN = (java.security.ProtectionDomain) java.security.AccessController.doPrivileged(new PrivilegedAction<Object>() {
+
+            public Object run() {
+                return ASMClassLoader.class.getProtectionDomain();
+            }
+        });
+    }
 
     public ASMClassLoader(){
         super(ASMClassLoader.class.getClassLoader());
     }
 
     public Class<?> defineClassPublic(String name, byte[] b, int off, int len) throws ClassFormatError {
-        Class<?> clazz = defineClass(name, b, off, len, null);
+        Class<?> clazz = defineClass(name, b, off, len, DOMAIN);
 
         return clazz;
     }

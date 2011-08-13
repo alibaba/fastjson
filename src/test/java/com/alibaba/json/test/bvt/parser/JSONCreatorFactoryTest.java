a4c376ad2ce08974c8de079a3caab7558de73acf
wenshao <szujobs@hotmail.com>
2011-08-13 00:09:58 +0000

    支持JSONCreator
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1305 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/test/java/com/alibaba/json/test/bvt/parser/JSONCreatorFactoryTest.java
+++ src/test/java/com/alibaba/json/test/bvt/parser/JSONCreatorFactoryTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONCreator;
 import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.parser.ParserConfig;
 
 public class JSONCreatorFactoryTest extends TestCase {
 
@@ -14,6 +15,18 @@ public class JSONCreatorFactoryTest extends TestCase {
         String text = JSON.toJSONString(entity);
 
         Entity entity2 = JSON.parseObject(text, Entity.class);
+        Assert.assertEquals(entity.getId(), entity2.getId());
+        Assert.assertEquals(entity.getName(), entity2.getName());
+    }
+    
+    public void test_create_2() throws Exception {
+        Entity entity = new Entity(123, "菜姐");
+        String text = JSON.toJSONString(entity);
+        
+        ParserConfig config = new ParserConfig();
+        config.setAsmEnable(false);
+        
+        Entity entity2 = JSON.parseObject(text, Entity.class, config, 0);
         Assert.assertEquals(entity.getId(), entity2.getId());
         Assert.assertEquals(entity.getName(), entity2.getName());
     }

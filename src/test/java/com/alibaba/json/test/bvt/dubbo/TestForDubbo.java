1615ff39df26b5600e95b45c91e176849ae94239
wenshao <szujobs@hotmail.com>
2011-08-19 10:35:53 +0000

    完善循环引用支持
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1330 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/test/java/com/alibaba/json/test/bvt/dubbo/TestForDubbo.java
+++ src/test/java/com/alibaba/json/test/bvt/dubbo/TestForDubbo.java
@@ -1,5 +1,7 @@
 package com.alibaba.json.test.bvt.dubbo;
 
+import java.util.ArrayList;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
@@ -7,20 +9,67 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class TestForDubbo extends TestCase {
+
+    static Person            person;
+
+    static {
+        person = new Person();
+        person.setPersonId("superman111");
+        person.setLoginName("superman");
+        person.setEmail("sm@1.com");
+        person.setPenName("pname");
+        person.setStatus(PersonStatus.ENABLED);
+
+        ArrayList<Phone> phones = new ArrayList<Phone>();
+        Phone phone1 = new Phone("86", "0571", "87654321", "001");
+        Phone phone2 = new Phone("86", "0571", "87654322", "002");
+        phones.add(phone1);
+        phones.add(phone2);
+        PersonInfo pi = new PersonInfo();
+        pi.setPhones(phones);
+        Phone fax = new Phone("86", "0571", "87654321", null);
+        pi.setFax(fax);
+        FullAddress addr = new FullAddress("CN", "zj", "3480", "wensanlu", "315000");
+        pi.setFullAddress(addr);
+        pi.setMobileNo("13584652131");
+        pi.setMale(true);
+        pi.setDepartment("b2b");
+        pi.setHomepageUrl("www.capcom.com");
+        pi.setJobTitle("qa");
+        pi.setName("superman");
+        person.setInfoProfile(pi);
+    }
+
     private HelloServiceImpl helloService = new HelloServiceImpl();
 
-    public void testParamType4() {
+    public void f_testParamType4() {
         Tiger tiger = new Tiger();
         tiger.setTigerName("东北虎");
         tiger.setTigerSex(true);
         Tigers tigers = helloService.eatTiger(tiger);
-        
+
         String text = JSON.toJSONString(tigers, SerializerFeature.WriteClassName);
         System.out.println(text);
-        
+
         Tigers tigers2 = JSON.parseObject(text, Tigers.class);
-        
+
         Assert.assertEquals(text, JSON.toJSONString(tigers2, SerializerFeature.WriteClassName));
     }
 
+    public void testPerson() {
+        Person p = helloService.showPerson(person);
+        String text = JSON.toJSONString(p, SerializerFeature.WriteClassName);
+        System.out.println(text);
+        
+        Person result = JSON.parseObject(text, Person.class);
+        
+        assertEquals(result.getInfoProfile().getPhones().get(0).getArea(),
+                     person.getInfoProfile().getPhones().get(0).getArea());
+        assertEquals(result.getInfoProfile().getPhones().get(0).getCountry(),
+                     person.getInfoProfile().getPhones().get(0).getCountry());
+        assertEquals(result.getInfoProfile().getPhones().get(0).getExtensionNumber(),
+                     person.getInfoProfile().getPhones().get(0).getExtensionNumber());
+        assertEquals(result.getInfoProfile().getPhones().get(0).getNumber(),
+                     person.getInfoProfile().getPhones().get(0).getNumber());
+    }
 }

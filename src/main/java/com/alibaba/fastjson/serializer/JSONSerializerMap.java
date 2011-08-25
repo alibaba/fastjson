b9f9c59a1b81bf6dac4856d89a0149bb6c358412
wenshao <szujobs@hotmail.com>
2011-08-25 06:59:50 +0000

    接口兼容
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1333 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/main/java/com/alibaba/fastjson/serializer/JSONSerializerMap.java
+++ src/main/java/com/alibaba/fastjson/serializer/JSONSerializerMap.java
@@ -1,6 +1,10 @@
 package com.alibaba.fastjson.serializer;
 
+import java.lang.reflect.Type;
+
 @Deprecated
 public class JSONSerializerMap extends SerializeConfig {
-
+    public boolean put(Type key, ObjectSerializer value) {
+        return super.put(key, value);
+    }
 }

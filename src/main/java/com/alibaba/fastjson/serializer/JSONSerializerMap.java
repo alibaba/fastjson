04e69c5664a87d49e2c1e80f82a3273544e6bb63
wenshao <szujobs@hotmail.com>
2011-08-26 01:31:14 +0000

    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1334 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/main/java/com/alibaba/fastjson/serializer/JSONSerializerMap.java
+++ src/main/java/com/alibaba/fastjson/serializer/JSONSerializerMap.java
@@ -1,10 +1,9 @@
 package com.alibaba.fastjson.serializer;
 
-import java.lang.reflect.Type;
 
 @Deprecated
 public class JSONSerializerMap extends SerializeConfig {
-    public boolean put(Type key, ObjectSerializer value) {
-        return super.put(key, value);
+    public final boolean put(Class<?> clazz, ObjectSerializer serializer) {
+        return super.put(clazz, serializer);
     }
 }

1a7a0eb1fecc11220e29dfbf9629121abd01fc55
wenshao <szujobs@hotmail.com>
2011-09-21 12:46:42 +0000

    优化WriteClassName
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1376 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/test/java/com/alibaba/json/test/bvt/writeClassName/WriteClassNameTest2.java
+++ src/test/java/com/alibaba/json/test/bvt/writeClassName/WriteClassNameTest2.java
@@ -6,21 +6,22 @@ import junit.framework.TestCase;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
-public class WriteClassNameTest extends TestCase {
+public class WriteClassNameTest2 extends TestCase {
 
     public void test_list() throws Exception {
         A a = new A();
         a.setB(new B());
         String text = JSON.toJSONString(a, SerializerFeature.WriteClassName);
         System.out.println(text);
-        Assert.assertEquals("{\"@type\":\"com.alibaba.json.test.bvt.writeClassName.WriteClassNameTest$A\",\"b\":{}}", text);
-        
+        Assert.assertEquals("{\"@type\":\"com.alibaba.json.test.bvt.writeClassName.WriteClassNameTest2$A\",\"b\":{\"id\":0}}",
+                            text);
+
         A a1 = (A) JSON.parse(text);
-        
+
         Assert.assertNotNull(a1.getB());
     }
 
-    private static class A {
+    public static class A {
 
         private B b;
 
@@ -34,7 +35,17 @@ public class WriteClassNameTest extends TestCase {
 
     }
 
-    private static final class B {
-        
+    public static final class B {
+
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
     }
 }

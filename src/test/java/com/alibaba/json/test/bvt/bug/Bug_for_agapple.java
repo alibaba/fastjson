ed2f62cf9921a1118bbf35e817d6de1461d371fa
wenshao <szujobs@hotmail.com>
2011-09-22 01:52:08 +0000

    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1387 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/test/java/com/alibaba/json/test/bvt/bug/Bug_for_agapple.java
+++ src/test/java/com/alibaba/json/test/bvt/bug/Bug_for_agapple.java
@@ -6,7 +6,7 @@ import com.alibaba.fastjson.JSON;
 
 import junit.framework.TestCase;
 
-public class Bug_for_Properties extends TestCase {
+public class Bug_for_agapple extends TestCase {
 
     public void test_for_agapple() throws Exception {
         Entity entity = new Entity();

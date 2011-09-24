bdfcaa687fc2921b2f19682d6faca997c4a84157
wenshao <szujobs@hotmail.com>
2011-09-24 13:15:32 +0000

    删除无用代码
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1409 fb902949-7fc4-4485-a80f-4d6ff335c879

--- src/test/java/com/derbysoft/spitfire/fastjson/dto/AbstractRS.java
+++
@@ -1,7 +0,0 @@
-package com.derbysoft.spitfire.fastjson.dto;
-
-public class AbstractRS extends AbstractDTO {
-    private SuccessDTO success;
-    private WarningsDTO warnings;
-    private ErrorsDTO errors;
-}
--- src/test/java/com/derbysoft/spitfire/fastjson/dto/MealsIncludedDTO.java
+++
@@ -1,6 +0,0 @@
-package com.derbysoft.spitfire.fastjson.dto;
-
-public class MealsIncludedDTO extends AbstractDTO{
-    public MealsIncludedDTO(MealsIncludedType type) {
-    }
-}

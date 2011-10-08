02627bb3f614f42d3113fd04be95e956313228ad
wenshao <szujobs@hotmail.com>
2011-10-08 13:27:40 +0000

    http://code.alibabatech.com/jira/browse/FASTJSON-104
    git-svn-id: http://code.alibabatech.com/svn/fastjson/trunk/fastjson@1430 fb902949-7fc4-4485-a80f-4d6ff335c879

---
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/ClassDerializer.java
@@ -0,0 +1,33 @@
+package com.alibaba.fastjson.parser.deserializer;
+
+import java.lang.reflect.Type;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.parser.DefaultJSONParser;
+import com.alibaba.fastjson.parser.JSONLexer;
+import com.alibaba.fastjson.parser.JSONToken;
+import com.alibaba.fastjson.util.TypeUtils;
+
+
+public class ClassDerializer implements ObjectDeserializer {
+    public final static ClassDerializer instance = new ClassDerializer();
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+        JSONLexer lexer = parser.getLexer();
+        if (lexer.token() != JSONToken.LITERAL_STRING) {
+            throw new JSONException("expect className");
+        }
+        String className = lexer.stringVal();
+        lexer.nextToken(JSONToken.COMMA);
+        
+        return (T) TypeUtils.loadClass(className);
+    }
+
+    @Override
+    public int getFastMatchToken() {
+        return JSONToken.LITERAL_STRING;
+    }
+
+}

e025d16c60b431fb6a009cac7edf6d31dbfe2784
wenshao <szujobs@qq.com>
2011-11-09 11:33:10 +0800

    asm can not handle non-public class error

---
+++ src/main/java/com/alibaba/fastjson/asm/ASMException.java
@@ -0,0 +1,12 @@
+package com.alibaba.fastjson.asm;
+
+import com.alibaba.fastjson.JSONException;
+
+public class ASMException extends JSONException {
+
+	private static final long serialVersionUID = 1L;
+
+    public ASMException(String message){
+        super(message);
+    }
+}

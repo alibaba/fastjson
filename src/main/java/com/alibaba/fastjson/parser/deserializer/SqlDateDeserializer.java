fc70a550a16886281f8d8c61a62482eff19d5f59
wenshao <szujobs@qq.com>
2011-12-15 22:34:05 +0800

    bug fixed : date parse use local calendar add testcase

--- src/main/java/com/alibaba/fastjson/parser/deserializer/SqlDateDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/SqlDateDeserializer.java
@@ -12,47 +12,48 @@ import com.alibaba.fastjson.parser.JSONScanner;
 import com.alibaba.fastjson.parser.JSONToken;
 
 public class SqlDateDeserializer implements ObjectDeserializer {
+
     public final static SqlDateDeserializer instance = new SqlDateDeserializer();
 
     @SuppressWarnings("unchecked")
     public <T> T deserialze(DefaultJSONParser parser, Type clazz, Object fieldName) {
         final JSONScanner lexer = (JSONScanner) parser.getLexer();
-        
+
         if (lexer.token() == JSONToken.NULL) {
             lexer.nextToken(JSONToken.COMMA);
             return null;
         }
-        
+
         if (lexer.token() == JSONToken.COMMA) {
             String key = lexer.scanSymbol(parser.getSymbolTable());
-            
+
             if ("val" != key) {
                 throw new JSONException("syntax error");
             }
-            
+
             lexer.nextTokenWithColon(JSONToken.LITERAL_INT);
-            
+
             if (lexer.token() != JSONToken.LITERAL_INT) {
                 throw new JSONException("syntax error");
             }
-            
+
             long val = lexer.longValue();
-            
+
             lexer.nextToken(JSONToken.RBRACE);
-            
+
             if (lexer.token() != JSONToken.RBRACE) {
                 throw new JSONException("syntax error");
             }
             lexer.nextToken(JSONToken.COMMA);
-            
+
             return (T) new java.sql.Date(val);
         }
-        
+
         Object val = parser.parse();
         if (val == null) {
             return null;
         }
-        
+
         if (val instanceof java.util.Date) {
             val = new java.sql.Date(((Date) val).getTime());
         } else if (val instanceof Number) {
@@ -62,16 +63,24 @@ public class SqlDateDeserializer implements ObjectDeserializer {
             if (strVal.length() == 0) {
                 return null;
             }
-            
-            SimpleDateFormat dateFormat = new SimpleDateFormat(JSON.DEFFAULT_DATE_FORMAT);
-            try {
-                java.util.Date date = (java.util.Date) dateFormat.parse(strVal);
-                return (T) new java.sql.Date(date.getTime());
-            } catch (ParseException e) {
-                // skip
+
+            long longVal;
+
+            JSONScanner dateLexer = new JSONScanner(strVal);
+            if (dateLexer.scanISO8601DateIfMatch()) {
+                longVal = dateLexer.getCalendar().getTimeInMillis();
+            } else {
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat(JSON.DEFFAULT_DATE_FORMAT);
+                try {
+                    java.util.Date date = (java.util.Date) dateFormat.parse(strVal);
+                    return (T) new java.sql.Date(date.getTime());
+                } catch (ParseException e) {
+                    // skip
+                }
+
+                longVal = Long.parseLong(strVal);
             }
-            
-            long longVal = Long.parseLong(strVal);
             return (T) new java.sql.Date(longVal);
         } else {
             throw new JSONException("parse error : " + val);

50f88dcd1faab4749068c4468ccfe0cb31c037b2
wenshao <szujobs@qq.com>
2011-12-16 14:31:53 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/parser/deser/ColorDeserializerTest.java
@@ -0,0 +1,57 @@
+package com.alibaba.json.bvt.parser.deser;
+
+import java.awt.Color;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.parser.deserializer.ColorDeserializer;
+
+
+public class ColorDeserializerTest extends TestCase {
+    public void test_0 () throws Exception {
+        new ColorDeserializer().getFastMatchToken();
+    }
+    
+    public void test_error() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("[]", Color.class);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_error_1() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("{33:44}", Color.class);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_error_2() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("{\"r\":44.}", Color.class);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_error_3() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("{\"x\":44}", Color.class);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+}

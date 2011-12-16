d4fd0f19923750302762a5a80ee8fa14828ab947
wenshao <szujobs@qq.com>
2011-12-16 15:33:53 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/parser/deser/TimeDeserializerTest2.java
@@ -0,0 +1,51 @@
+package com.alibaba.json.bvt.parser.deser;
+
+import java.awt.Color;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+
+public class TimeDeserializerTest2 extends TestCase {
+    public void test_0 () throws Exception {
+        long millis = System.currentTimeMillis();
+        JSON.parse("{\"@type\":\"java.sql.Time\",\"value\":" + millis + "}");
+    }
+    
+    public void test_error() throws Exception {
+        long millis = System.currentTimeMillis();
+        
+        Exception error = null;
+        try {
+            JSON.parse("{\"@type\":\"java.sql.Time\",33:" + millis + "}");
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_error_1() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parse("{\"@type\":\"java.sql.Time\",\"value\":true}");
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_error_2() throws Exception {
+        long millis = System.currentTimeMillis();
+        
+        Exception error = null;
+        try {
+            JSON.parse("{\"@type\":\"java.sql.Time\",\"value\":" + millis + ",}");
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+}

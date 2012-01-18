205b9497a3386bc5bf26666ca766cda9fd4101f8
温少 <szujobs@hotmail.com>
2012-01-18 16:40:21 +0800

    add tests

---
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_lenolix.java
@@ -0,0 +1,22 @@
+package com.alibaba.json.bvt.bug;
+
+import java.util.Map;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+public class Bug_for_lenolix extends TestCase {
+    public void test_FieldMap() throws Exception {
+        Map<String, String[]> map = JSON.parseObject("{\"key\":[\"value1\",\"value2\"]}",
+                                                                     new TypeReference<Map<String, String[]>>() {
+                                                                     });
+        String[] array = map.get("key");
+        Assert.assertEquals("value1", array[0]);
+        Assert.assertEquals("value2", array[1]);
+    }
+
+    
+}

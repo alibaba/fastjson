dee22f7029508340624522ecbc8a8278ce0577f5
温少 <szujobs@hotmail.com>
2012-02-02 14:29:22 +0800

    support external class

---
+++ src/test/java/com/alibaba/json/bvt/TestExternal3.java
@@ -0,0 +1,41 @@
+package com.alibaba.json.bvt;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+
+public class TestExternal3 extends TestCase {
+    public void test_0 () throws Exception {
+        ExtClassLoader classLoader = new ExtClassLoader();
+        Class<?> clazz = classLoader.loadClass("external.VO");
+        Method method = clazz.getMethod("setName", new Class[] {String.class});
+        Object obj = clazz.newInstance();
+        method.invoke(obj, "jobs");
+        
+        String text = JSON.toJSONString(obj, SerializerFeature.WriteClassName);
+        System.out.println(text);
+        JSON.parseObject(text, clazz);
+        JSON.parseObject(text);
+    }
+    
+    public static class ExtClassLoader extends ClassLoader {
+        public ExtClassLoader() throws IOException{
+            super(Thread.currentThread().getContextClassLoader());
+            
+            byte[] bytes;
+            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("external/VO.clazz");
+            bytes = IOUtils.toByteArray(is);
+            is.close();
+            
+            super.defineClass("external.VO", bytes, 0, bytes.length);
+        }
+    }
+}

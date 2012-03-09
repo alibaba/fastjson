890c55ae195b5000f14f7013d09814972e478dd0
温少 <szujobs@hotmail.com>
2012-03-09 17:32:45 +0800

    bug fixed for date parse

--- src/main/java/com/alibaba/fastjson/parser/deserializer/AbstractDateDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/AbstractDateDeserializer.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Type;
 
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.parser.DefaultJSONParser;
+import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.parser.JSONScanner;
 import com.alibaba.fastjson.parser.JSONToken;
 
@@ -11,11 +12,29 @@ public abstract class AbstractDateDeserializer implements ObjectDeserializer {
 
     @SuppressWarnings("unchecked")
     public <T> T deserialze(DefaultJSONParser parser, Type clazz, Object fieldName) {
+        JSONScanner lexer = (JSONScanner) parser.getLexer();
+
         Object val;
-        if (parser.getResolveStatus() == DefaultJSONParser.TypeNameRedirect) {
+        if (lexer.token() == JSONToken.LITERAL_INT) {
+            val = lexer.longValue();
+            lexer.nextToken(JSONToken.COMMA);
+        } else if (lexer.token() == JSONToken.LITERAL_STRING) {
+            String strVal = lexer.stringVal();
+            val = strVal;
+            lexer.nextToken(JSONToken.COMMA);
+            
+            if (lexer.isEnabled(Feature.AllowISO8601DateFormat)) {
+                JSONScanner iso8601Lexer = new JSONScanner(strVal);
+                if (iso8601Lexer.scanISO8601DateIfMatch()) {
+                    val = iso8601Lexer.getCalendar().getTime();
+                }
+            }
+        } else if (lexer.token() == JSONToken.NULL) {
+            lexer.nextToken();
+            val = null;
+        } else if (parser.getResolveStatus() == DefaultJSONParser.TypeNameRedirect) {
             parser.accept(JSONToken.COMMA);
 
-            JSONScanner lexer = (JSONScanner) parser.getLexer();
             if (lexer.token() == JSONToken.LITERAL_STRING) {
                 if (!"val".equals(lexer.stringVal())) {
                     throw new JSONException("syntax error");
@@ -35,7 +54,7 @@ public abstract class AbstractDateDeserializer implements ObjectDeserializer {
         } else {
             val = parser.parse();
         }
-        
+
         return (T) cast(parser, clazz, fieldName, val);
     }
 
---
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_lenolix_7.java
@@ -0,0 +1,103 @@
+package com.alibaba.json.bvt.bug;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.json.bvt.parser.GenericTest.Address;
+
+public class Bug_for_lenolix_7 extends TestCase {
+
+    public void test_for_objectKey() throws Exception {
+        User user = new User();
+        user.setId(1);
+        user.setName("leno.lix");
+        user.setIsBoy(true);
+        user.setBirthDay(new Date());
+        user.setGmtCreate(new java.sql.Date(new Date().getTime()));
+        user.setGmtModified(new java.sql.Timestamp(new Date().getTime()));
+        String userJSON = JSON.toJSONString(user, SerializerFeature.WriteClassName, SerializerFeature.WriteMapNullValue);
+
+        System.out.println(userJSON);
+
+        User returnUser = (User) JSON.parse(userJSON);
+
+    }
+
+    public static class User implements Serializable {
+
+        /**
+             *
+             */
+
+        private static final long  serialVersionUID = 6192533820796587011L;
+
+        private Integer            id;
+        private String             name;
+        private Boolean            isBoy;
+        private Address            address;
+        private Date               birthDay;
+        private java.sql.Date      gmtCreate;
+        private java.sql.Timestamp gmtModified;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Boolean getIsBoy() {
+            return isBoy;
+        }
+
+        public void setIsBoy(Boolean isBoy) {
+            this.isBoy = isBoy;
+        }
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public Date getBirthDay() {
+            return birthDay;
+        }
+
+        public void setBirthDay(Date birthDay) {
+            this.birthDay = birthDay;
+        }
+
+        public java.sql.Date getGmtCreate() {
+            return gmtCreate;
+        }
+
+        public void setGmtCreate(java.sql.Date gmtCreate) {
+            this.gmtCreate = gmtCreate;
+        }
+
+        public java.sql.Timestamp getGmtModified() {
+            return gmtModified;
+        }
+
+        public void setGmtModified(java.sql.Timestamp gmtModified) {
+            this.gmtModified = gmtModified;
+        }
+
+    }
+}

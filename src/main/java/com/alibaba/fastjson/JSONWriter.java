551aebccd647809348ff8303d2241fcfc1353383
温少 <szujobs@hotmail.com>
2012-06-01 08:02:56 +0800

    add manual JSONWriter support

--- src/main/java/com/alibaba/fastjson/JSONWriter.java
+++ src/main/java/com/alibaba/fastjson/JSONWriter.java
@@ -11,15 +11,37 @@ public class JSONWriter {
     private SerializeWriter writer;
     private final Writer    out;
 
-    private Stat            stat = Stat.None;
     private JSONSerializer  serializer;
 
-    public static enum Stat {
-        None, BeginObject, Key, Value
+    private Context         context;
+
+    public static enum State {
+        None, BeginObject, PropertyKey, PropertyValue, BeginArray, ArrayValue
     }
-    
+
     public static class Context {
-        
+
+        private final Context parent;
+
+        private State         state = State.None;
+
+        public Context(Context parent, State state){
+            this.parent = parent;
+            this.state = state;
+        }
+
+        public Context getParent() {
+            return parent;
+        }
+
+        public State getState() {
+            return state;
+        }
+
+        public void setState(State state) {
+            this.state = state;
+        }
+
     }
 
     public JSONWriter(Writer out){
@@ -41,22 +63,85 @@ public class JSONWriter {
     }
 
     public void writeStartObject() {
-        writer.write('{');
-        stat = Stat.None;
-    }
-
-    public void writeKey(String key) {
-        if (stat == Stat.Value) {
-            writer.write(',');
+        if (context == null) {
+            context = new Context(null, State.BeginObject);
+        } else {
+            if (context.getState() == State.PropertyKey) {
+                writer.write(':');
+            } else if (context.getState() == State.ArrayValue) {
+                writer.write(',');
+            } else if (context.getState() == State.BeginObject) {
+                // skip
+            } else if (context.getState() == State.BeginArray) {
+                // skip
+            } else {
+                throw new JSONException("illegal state : " + context.getState());
+            }
+            context = new Context(context, State.BeginObject);
         }
-        writer.writeString(key);
-    }
-
-    public void writeValue(Object object) {
-        serializer.write(object);
+        writer.write('{');
     }
 
     public void writeEndObject() {
         writer.write('}');
+        context = context.getParent();
+        if (context == null) {
+            // skip
+        } else if (context.getState() == State.PropertyKey) {
+            context.setState(State.PropertyValue);
+        } else if (context.getState() == State.BeginArray) {
+            context.setState(State.ArrayValue);
+        } else if (context.getState() == State.ArrayValue) {
+            // skip
+        }
+    }
+
+    public void writeKey(String key) {
+        if (context.getState() == State.PropertyValue) {
+            writer.write(',');
+        }
+        writer.writeString(key);
+        context.setState(State.PropertyKey);
+    }
+
+    public void writeValue(Object object) {
+        if (context.getState() == State.PropertyKey) {
+            writer.write(':');
+        }
+        serializer.write(object);
+        context.setState(State.PropertyValue);
+    }
+
+    public void writeStartArray() {
+        if (context == null) {
+            context = new Context(null, State.BeginArray);
+        } else {
+            if (context.getState() == State.PropertyKey) {
+                writer.write(':');
+            } else if (context.getState() == State.ArrayValue) {
+                writer.write(',');
+            } else if (context.getState() == State.BeginArray) {
+                // skipe
+            } else {
+                throw new JSONException("illegal state : " + context.getState());
+            }
+            context = new Context(context, State.BeginArray);
+        }
+        writer.write('[');
+    }
+
+    public void writeEndArray() {
+        writer.write(']');
+        context = context.getParent();
+
+        if (context == null) {
+            // skip
+        } else if (context.getState() == State.PropertyKey) {
+            context.setState(State.PropertyValue);
+        } else if (context.getState() == State.BeginArray) {
+            context.setState(State.ArrayValue);
+        } else if (context.getState() == State.ArrayValue) {
+            // skip
+        }
     }
 }

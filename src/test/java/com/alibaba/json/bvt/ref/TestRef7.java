3ccdca2c057de661dcaae96bcb2b560c9d7a527a
温少 <szujobs@hotmail.com>
2012-06-07 15:53:54 +0800

    bug fixed for reference

--- src/test/java/com/alibaba/json/bvt/ref/TestRef7.java
+++ src/test/java/com/alibaba/json/bvt/ref/TestRef7.java
@@ -5,14 +5,8 @@ import junit.framework.TestCase;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 
 public class TestRef7 extends TestCase {
-
-    public void test_intArray() throws Exception {
-        JSONObject json = JSON.parseObject("{\"values\":[1,2,3]}");
-        int[] values = json.getObject("values", int[].class);
-    }
 
     public void test_0() throws Exception {
 
@@ -31,14 +25,16 @@ public class TestRef7 extends TestCase {
         fB.setMaster(pC);
 
         Family[] familyArray = new Family[] { fA, fB };
-        String text = JSON.toJSONString(familyArray);
+        String text = JSON.toJSONString(familyArray, true);
         System.out.println(text);
 
         JSONArray array = JSON.parseArray(text);
 
         Assert.assertSame(array.getJSONObject(0).get("master"), array.getJSONObject(0).getJSONArray("members").get(0));
-        // Assert.assertSame(result[1].getMaster(), result[1].getMembers()[0]);
-
+        
+        Family family = array.getObject(0, Family.class);
+        Assert.assertNotNull(family.getMembers()[0]);
+        Assert.assertNotNull(family.getMembers()[1]);
     }
 
     public static class Family {

8e1a38b3387681e64110dba0294cb38ada23efd7
wenshao <szujobs@hotmail.com>
2012-10-01 15:47:27 +0800

    avoid hard code class name

--- src/test/java/com/alibaba/json/bvt/asm/ASMUtilsTest.java
+++ src/test/java/com/alibaba/json/bvt/asm/ASMUtilsTest.java
@@ -3,11 +3,16 @@ package com.alibaba.json.bvt.asm;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import com.alibaba.fastjson.parser.ParseContext;
 import com.alibaba.fastjson.util.ASMUtils;
 
 
 public class ASMUtilsTest extends TestCase {
     public void test_isAnroid() throws Exception {
         Assert.assertTrue(ASMUtils.isAndroid("Dalvik"));
+    }
+    
+    public void test_getDescs() throws Exception {
+        Assert.assertEquals("Lcom/alibaba/fastjson/parser/ParseContext;",ASMUtils.getDesc(ParseContext.class));
     }
 }

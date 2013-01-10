c42ad699115d4df31087e988697a498a9938ede1
wenshao <szujobs@hotmail.com>
2013-01-10 17:19:47 +0800

    add BenchmarkMain

--- src/test/java/com/alibaba/json/test/benchmark/BenchmarkTest.java
+++ src/test/java/com/alibaba/json/test/benchmark/BenchmarkTest.java
@@ -2,59 +2,9 @@ package com.alibaba.json.test.benchmark;
 
 import junit.framework.TestCase;
 
-import com.alibaba.json.test.benchmark.encode.EishayEncode;
-import com.alibaba.json.test.codec.FastjsonCodec;
-import com.alibaba.json.test.codec.JacksonCodec;
-
 public class BenchmarkTest extends TestCase {
 
     public void test_benchmark() throws Exception {
-        // 注意，byte[]在jackson中是使用base64编码的，不正确的。
-
-        BenchmarkExecutor executor = new BenchmarkExecutor();
-        executor.setExecuteCount(5);
-        executor.getCodecList().add(new FastjsonCodec());
-//        executor.getCodecList().add(new JacksonCodec());
-//        executor.getCodecList().add(new Jackson2Codec());
-        //
-        // executor.getCodecList().add(new SimpleJsonCodec());
-        // executor.getCodecList().add(new JsonLibCodec());
-        // executor.getCodecList().add(new JsonSmartCodec());
-
-        executor.setLoopCount(1000 * 1000 * 100);
-
-//        executor.getCaseList().add(new EishayDecodeBytes());
-//        executor.getCaseList().add(new EishayDecode2Bytes());
-//         executor.getCaseList().add(new EishayDecode());
-//         executor.getCaseList().add(new EishayDecodeByClassName());
-//         executor.getCaseList().add(new EishayTreeDecode());
-         executor.getCaseList().add(new EishayEncode());
-//         executor.getCaseList().add(new EishayEncodeManual());
-        // executor.getCaseList().add(new IntArray1000Decode());
-        // executor.getCaseList().add(new StringArray1000Decode());
-        // executor.getCaseList().add(new Map1000StringDecode());
-        // executor.getCaseList().add(new Entity100StringDecode());
-
-        // executor.getCaseList().add(new ListBoolean1000Encode());
-        // executor.getCaseList().add(new ArrayBoolean1000Encode());
-        // executor.getCaseList().add(new IntArray1000Decode());
-        // executor.getCaseList().add(new StringArray1000Decode());
-        // executor.getCaseList().add(new GroupEncode());
-        // executor.getCaseList().add(new CategoryEncode());
-        // executor.getCaseList().add(new GroupEncode());
-        // executor.getCaseList().add(new Map1Decode());
-        // executor.getCaseList().add(new Entity100IntDecode());
-        // executor.getCaseList().add(new Entity100StringDecode());
-        // executor.getCaseList().add(new Entity100IntEncode());
-        // executor.getCaseList().add(new ArrayByte1000Encode());
-        // executor.getCaseList().add(new ArrayInt1000Encode());
-        // executor.getCaseList().add(new ArrayLong1000Encode());
-        // executor.getCaseList().add(new ArrayString1000Encode());
-        // executor.getCaseList().add(new ArrayEmptyList1000Encode());
-        // executor.getCaseList().add(new ArrayEmptyMap1000Encode());
-        // executor.getCaseList().add(new ArrayObjectEmptyMap1000Encode());
-        // executor.getCaseList().add(new Map1000Encode());
-
-        executor.execute();
+    	BenchmarkMain.main(new String[]{});
     }
 }

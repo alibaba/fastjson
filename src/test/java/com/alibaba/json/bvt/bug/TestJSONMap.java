f9f862357372a400c441a5974e4987e3d649dca9
wenshao <szujobs@gmail.com>
2013-04-25 21:25:36 +0800

    bug fixed

---
+++ src/test/java/com/alibaba/json/bvt/bug/TestJSONMap.java
@@ -0,0 +1,35 @@
+package com.alibaba.json.bvt.bug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+public class TestJSONMap extends TestCase {
+
+    public void test_0() throws Exception {
+        Record record = new Record();
+        Map map = new HashMap();
+        record.setRecord(map);
+        String s = JSON.toJSONString(record, SerializerFeature.WriteClassName);
+        System.out.println(s);
+        record = (Record)JSON.parse(s); //此处抛出异常
+        System.out.println(record.getRecord().size());
+    }
+
+    public static class Record {
+
+        private Map<Integer, Integer> record;
+
+        public Map<Integer, Integer> getRecord() {
+            return record;
+        }
+
+        public void setRecord(Map<Integer, Integer> record) {
+            this.record = record;
+        }
+    }
+}

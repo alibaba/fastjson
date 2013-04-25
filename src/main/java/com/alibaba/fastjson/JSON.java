c797daabf2686865774d91ed0926ec685be0c5b1
wenshao <szujobs@gmail.com>
2013-04-25 20:55:15 +0800

    1.1.30-SNAPSHOT

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -684,5 +684,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
 		return TypeUtils.cast(json, clazz, ParserConfig.getGlobalInstance());
 	}
 
-	public final static String VERSION = "1.1.29";
+	public final static String VERSION = "1.1.30";
 }

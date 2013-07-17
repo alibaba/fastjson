85c592f14432782837373d711e00dafa5ca45c4b
wenshao <szujobs@hotmail.com>
2013-07-17 15:46:42 +0800

    rename arguemnt's name

--- src/main/java/com/alibaba/fastjson/serializer/PropertyPreFilter.java
+++ src/main/java/com/alibaba/fastjson/serializer/PropertyPreFilter.java
@@ -2,5 +2,5 @@ package com.alibaba.fastjson.serializer;
 
 public interface PropertyPreFilter extends SerializeFilter {
 
-    boolean apply(JSONSerializer serializer, Object source, String name);
+    boolean apply(JSONSerializer serializer, Object object, String name);
 }

89a4352e54257266615c0cb3a13ccf9149f8fd04
wenshao <szujobs@hotmail.com>
2013-07-17 21:23:52 +0800

    improve performance

--- src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
@@ -15,11 +15,15 @@
  */
 package com.alibaba.fastjson.parser;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.util.Base64;
 
 //这个类，为了性能优化做了很多特别处理，一切都是为了性能！！！
@@ -71,6 +75,58 @@ public final class JSONScanner extends JSONLexer {
         text.getChars(offset, offset + count, dest, 0);
     }
 
+    protected final static char[] typeFieldName = ("\"" + JSON.DEFAULT_TYPE_KEY + "\":\"").toCharArray();
+
+    public final int scanType(String type) {
+        matchStat = UNKOWN;
+
+        if (!charArrayCompare(text, bp, typeFieldName)) {
+            return NOT_MATCH_NAME;
+        }
+
+        int bpLocal = this.bp + typeFieldName.length;
+
+        final int typeLength = type.length();
+        for (int i = 0; i < typeLength; ++i) {
+            if (type.charAt(i) != charAt(bpLocal + i)) {
+                return NOT_MATCH;
+            }
+        }
+        bpLocal += typeLength;
+        if (charAt(bpLocal) != '"') {
+            return NOT_MATCH;
+        }
+
+        this.ch = charAt(++bpLocal);
+
+        if (ch == ',') {
+            this.ch = charAt(++bpLocal);
+            this.bp = bpLocal;
+            token = JSONToken.COMMA;
+            return VALUE;
+        } else if (ch == '}') {
+            ch = charAt(++bpLocal);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bpLocal);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bpLocal);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bpLocal);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                return NOT_MATCH;
+            }
+            matchStat = END;
+        }
+
+        this.bp = bpLocal;
+        return matchStat;
+    }
+
     static final boolean charArrayCompare(String src, int offset, char[] dest) {
         final int destLen = dest.length;
         if (destLen + offset > src.length()) {
@@ -102,12 +158,16 @@ public final class JSONScanner extends JSONLexer {
         return Base64.decodeFast(text, np + 1, sp);
     }
 
-    protected void arrayCopy(int srcPos, char[] dest, int destPos, int length) {
-        text.getChars(srcPos, srcPos + length, dest, destPos);
-    }
+    // public int scanField2(char[] fieldName, Object object, FieldDeserializer fieldDeserializer) {
+    // return NOT_MATCH;
+    // }
 
+    /**
+     * The value of a literal token, recorded as a string. For integers, leading 0x and 'l' suffixes are suppressed.
+     */
     public final String stringVal() {
         if (!hasSpecial) {
+            // return new String(buf, np + 1, sp);
             return text.substring(np + 1, np + 1 + sp);
         } else {
             return new String(sbuf, 0, sp);
@@ -127,6 +187,7 @@ public final class JSONScanner extends JSONLexer {
         }
 
         return text.substring(np, np + sp);
+        // return new String(buf, np, sp);
     }
 
     public final int ISO8601_LEN_0 = "0000-00-00".length();
@@ -458,4 +519,525 @@ public final class JSONScanner extends JSONLexer {
         return bp == text.length() || ch == EOI && bp + 1 == text.length();
     }
 
+    public int scanFieldInt(char[] fieldName) {
+        matchStat = UNKOWN;
+        int startPos = this.bp;
+        char startChar = this.ch;
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return 0;
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+
+        int value;
+        if (ch >= '0' && ch <= '9') {
+            value = digits[ch];
+            for (;;) {
+                ch = charAt(index++);
+                if (ch >= '0' && ch <= '9') {
+                    value = value * 10 + digits[ch];
+                } else if (ch == '.') {
+                    matchStat = NOT_MATCH;
+                    return 0;
+                } else {
+                    bp = index - 1;
+                    break;
+                }
+            }
+            if (value < 0) {
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+        } else {
+            matchStat = NOT_MATCH;
+            return 0;
+        }
+
+        if (ch == ',') {
+            this.ch = charAt(++bp);
+            matchStat = VALUE;
+            token = JSONToken.COMMA;
+            return value;
+        }
+
+        if (ch == '}') {
+            ch = charAt(++bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                this.bp = startPos;
+                this.ch = startChar;
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+            matchStat = END;
+        }
+
+        return value;
+    }
+
+    public String scanFieldString(char[] fieldName) {
+        matchStat = UNKOWN;
+        int startPos = this.bp;
+        char startChar = this.ch;
+
+        // final int fieldNameLength = fieldName.length;
+        // for (int i = 0; i < fieldNameLength; ++i) {
+        // if (fieldName[i] != buf[bp + i]) {
+        // matchStat = NOT_MATCH_NAME;
+        //
+        // return stringDefaultValue();
+        // }
+        // }
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return stringDefaultValue();
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+        if (ch != '"') {
+            matchStat = NOT_MATCH;
+
+            return stringDefaultValue();
+        }
+
+        boolean hasSpecial = false;
+        final String strVal;
+        {
+            int startIndex = index;
+            int endIndex = text.indexOf('"', startIndex);
+            if (endIndex == -1) {
+                throw new JSONException("unclosed str");
+            }
+
+            String stringVal = subString(startIndex, endIndex - startIndex);
+            for (int i = 0; i < stringVal.length(); ++i) {
+                if (stringVal.charAt(i) == '\\') {
+                    hasSpecial = true;
+                    break;
+                }
+            }
+
+            if (hasSpecial) {
+                matchStat = NOT_MATCH;
+
+                return stringDefaultValue();
+            }
+
+            bp = endIndex + 1;
+            this.ch = ch = charAt(bp);
+            strVal = stringVal;
+            // this.stringVal = stringVal;
+            // int pos = endIndex + 1;
+            // char ch = charAt(pos);
+            // if (ch != '\'') {
+            // this.pos = pos;
+            // this.ch = ch;
+            // token = LITERAL_CHARS;
+            // return;
+            // }
+        }
+
+        // final int start = index;
+        // for (;;) {
+        // ch = charAt(index++);
+        // if (ch == '\"') {
+        // bp = index;
+        // this.ch = ch = charAt(bp);
+        // strVal = text.substring(start, index - 1);
+        // // strVal = new String(buf, start, index - start - 1);
+        // break;
+        // }
+        //
+        // if (ch == '\\') {
+        // matchStat = NOT_MATCH;
+        //
+        // return stringDefaultValue();
+        // }
+        // }
+
+        if (ch == ',') {
+            this.ch = charAt(++bp);
+            matchStat = VALUE;
+            return strVal;
+        } else if (ch == '}') {
+            ch = charAt(++bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                this.bp = startPos;
+                this.ch = startChar;
+                matchStat = NOT_MATCH;
+                return stringDefaultValue();
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+
+            return stringDefaultValue();
+        }
+
+        return strVal;
+    }
+
+    public String scanFieldSymbol(char[] fieldName, final SymbolTable symbolTable) {
+        matchStat = UNKOWN;
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return null;
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+        if (ch != '"') {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        String strVal;
+        int start = index;
+        int hash = 0;
+        for (;;) {
+            ch = charAt(index++);
+            if (ch == '\"') {
+                bp = index;
+                this.ch = ch = charAt(bp);
+                // strVal = text.substring(start, index - 1).intern();
+                strVal = symbolTable.addSymbol(text, start, index - start - 1, hash);
+                break;
+            }
+
+            hash = 31 * hash + ch;
+
+            if (ch == '\\') {
+                matchStat = NOT_MATCH;
+                return null;
+            }
+        }
+
+        if (ch == ',') {
+            this.ch = charAt(++bp);
+            matchStat = VALUE;
+            return strVal;
+        } else if (ch == '}') {
+            ch = charAt(++bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                matchStat = NOT_MATCH;
+                return null;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        return strVal;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Collection<String> scanFieldStringArray(char[] fieldName, Class<?> type) {
+        matchStat = UNKOWN;
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return null;
+        }
+
+        Collection<String> list;
+
+        if (type.isAssignableFrom(HashSet.class)) {
+            list = new HashSet<String>();
+        } else if (type.isAssignableFrom(ArrayList.class)) {
+            list = new ArrayList<String>();
+        } else {
+            try {
+                list = (Collection<String>) type.newInstance();
+            } catch (Exception e) {
+                throw new JSONException(e.getMessage(), e);
+            }
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+
+        if (ch != '[') {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        ch = charAt(index++);
+
+        for (;;) {
+            if (ch != '"') {
+                matchStat = NOT_MATCH;
+                return null;
+            }
+
+            String strVal;
+            int start = index;
+            for (;;) {
+                ch = charAt(index++);
+                if (ch == '\"') {
+                    strVal = text.substring(start, index - 1);
+                    // strVal = new String(buf, start, index - start - 1);
+                    list.add(strVal);
+                    ch = charAt(index++);
+                    break;
+                }
+
+                if (ch == '\\') {
+                    matchStat = NOT_MATCH;
+                    return null;
+                }
+            }
+
+            if (ch == ',') {
+                ch = charAt(index++);
+                continue;
+            }
+
+            if (ch == ']') {
+                ch = charAt(index++);
+                break;
+            }
+
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        bp = index;
+        if (ch == ',') {
+            this.ch = charAt(bp);
+            matchStat = VALUE;
+            return list;
+        } else if (ch == '}') {
+            ch = charAt(bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+                this.ch = ch;
+            } else {
+                matchStat = NOT_MATCH;
+                return null;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        return list;
+    }
+
+    public long scanFieldLong(char[] fieldName) {
+        matchStat = UNKOWN;
+        int startPos = this.bp;
+        char startChar = this.ch;
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return 0;
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+
+        long value;
+        if (ch >= '0' && ch <= '9') {
+            value = digits[ch];
+            for (;;) {
+                ch = charAt(index++);
+                if (ch >= '0' && ch <= '9') {
+                    value = value * 10 + digits[ch];
+                } else if (ch == '.') {
+                    matchStat = NOT_MATCH;
+                    return 0;
+                } else {
+                    bp = index - 1;
+                    break;
+                }
+            }
+            if (value < 0) {
+                this.bp = startPos;
+                this.ch = startChar;
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+        } else {
+            this.bp = startPos;
+            this.ch = startChar;
+            matchStat = NOT_MATCH;
+            return 0;
+        }
+
+        if (ch == ',') {
+            ch = charAt(++bp);
+            matchStat = VALUE;
+            token = JSONToken.COMMA;
+            return value;
+        } else if (ch == '}') {
+            ch = charAt(++bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                this.bp = startPos;
+                this.ch = startChar;
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return 0;
+        }
+
+        return value;
+    }
+
+    public boolean scanFieldBoolean(char[] fieldName) {
+        matchStat = UNKOWN;
+
+        if (!charArrayCompare(text, bp, fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return false;
+        }
+
+        int index = bp + fieldName.length;
+
+        char ch = charAt(index++);
+
+        boolean value;
+        if (ch == 't') {
+            if (charAt(index++) != 'r') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            if (charAt(index++) != 'u') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            if (charAt(index++) != 'e') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+
+            bp = index;
+            ch = charAt(bp);
+            value = true;
+        } else if (ch == 'f') {
+            if (charAt(index++) != 'a') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            if (charAt(index++) != 'l') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            if (charAt(index++) != 's') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            if (charAt(index++) != 'e') {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+
+            bp = index;
+            ch = charAt(bp);
+            value = false;
+        } else {
+            matchStat = NOT_MATCH;
+            return false;
+        }
+
+        if (ch == ',') {
+            this.ch = charAt(++bp);
+            matchStat = VALUE;
+            token = JSONToken.COMMA;
+        } else if (ch == '}') {
+            ch = charAt(++bp);
+            if (ch == ',') {
+                token = JSONToken.COMMA;
+                this.ch = charAt(++bp);
+            } else if (ch == ']') {
+                token = JSONToken.RBRACKET;
+                this.ch = charAt(++bp);
+            } else if (ch == '}') {
+                token = JSONToken.RBRACE;
+                this.ch = charAt(++bp);
+            } else if (ch == EOI) {
+                token = JSONToken.EOF;
+            } else {
+                matchStat = NOT_MATCH;
+                return false;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return false;
+        }
+
+        return value;
+    }
+
+    protected final void arrayCopy(int srcPos, char[] dest, int destPos, int length) {
+        text.getChars(srcPos, srcPos + length, dest, destPos);
+    }
 }

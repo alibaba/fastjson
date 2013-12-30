233e8ce70c0f10acfbcbac29d776d16e88bdccbe
wenshao <szujobs@hotmail.com>
2013-12-30 21:32:13 +0800

    bug fixed for unicode space

--- src/test/java/com/alibaba/json/bvt/serializer/TestSpecial3.java
+++ src/test/java/com/alibaba/json/bvt/serializer/TestSpecial3.java
@@ -4,14 +4,14 @@ import junit.framework.TestCase;
 
 import com.alibaba.fastjson.JSON;
 
-public class TestSpecial2 extends TestCase {
+public class TestSpecial3 extends TestCase {
 
     public void test_0() throws Exception {
         StringBuilder buf = new StringBuilder();
         buf.append('\r');
         buf.append('\r');
         for (int i = 0; i < 1000; ++i) {
-            buf.append((char) 160);
+            buf.append('\u000B');
         }
 
         VO vo = new VO();

79e5def2acb3d496e97ea5ecdc323fcf9a98eec1
kimmking <kimmking.cn@gmail.com>
2014-02-17 22:33:04 +0800

    test for issues#135
    test for issues#135

---
+++ src/test/java/com/alibaba/json/test/generic/TBean.java
@@ -0,0 +1,14 @@
+package com.alibaba.json.test.generic;
+import java.io.Serializable;
+
+
+public class TBean implements Serializable {
+
+	String s;
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+}

b8ee45aa1c6c0a050bb6281e23ccb7f91324c291
wenshao <szujobs@hotmail.com>
2014-10-03 16:46:34 +0800

    full support ISO8601 dateformat

--- src/main/java/com/alibaba/fastjson/serializer/CalendarCodec.java
+++ src/main/java/com/alibaba/fastjson/serializer/CalendarCodec.java
@@ -22,8 +22,13 @@ public class CalendarCodec implements ObjectSerializer, ObjectDeserializer {
 
     @SuppressWarnings("unchecked")
     public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
-        Date date = DateDeserializer.instance.deserialze(parser, type, fieldName);
+        Object value = DateDeserializer.instance.deserialze(parser, type, fieldName);
         
+        if (value instanceof Calendar) {
+            return (T) value;
+        }
+        
+        Date date = (Date) value;
         if (date == null) {
             return null;
         }
--- src/test/java/com/alibaba/json/bvt/serializer/DateTest3.java
+++ src/test/java/com/alibaba/json/bvt/serializer/DateTest3.java
@@ -11,7 +11,7 @@ import com.alibaba.fastjson.JSON;
 public class DateTest3 extends TestCase {
 
     public void test_date() throws Exception {
-        String text = "{\"gmtCreate\":\"2014-08-21T09:51:36.25+08:00\"}";
+        String text = "{\"gmtCreate\":\"2014-08-21T09:51:36.25+07:00\"}";
         
         Date date = JSON.parseObject(text, VO.class).getGmtCreate();
         Assert.assertNotNull(date);

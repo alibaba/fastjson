a1ada50bda8bea4a22b0a7f1ac33f47ff3e79867
wenshao <shaojin.wensj@alibaba-inc.com>
2014-10-12 21:50:29 +0800

    improve jsonpath support

---
+++ src/test/java/com/alibaba/json/bvt/path/JSONPath_field_wildcard.java
@@ -0,0 +1,48 @@
+package com.alibaba.json.bvt.path;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSONPath;
+
+public class JSONPath_field_wildcard extends TestCase {
+
+    public void test_list_map() throws Exception {
+        Entity entity = new Entity(123, "wenshao");
+
+        List<Object> fieldValues = (List<Object>) new JSONPath("$.*").eval(entity);
+        Assert.assertSame(entity.getId(), fieldValues.get(0));
+        Assert.assertSame(entity.getName(), fieldValues.get(1));
+    }
+
+    public static class Entity {
+
+        private Integer id;
+        private String  name;
+
+        public Entity(Integer id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+}

4fe026f1d25252b708bccd2d1486be6bd673e68e
wenshao <shaojin.wensj@alibaba-inc.com>
2014-10-13 00:48:46 +0800

    improve jsonpath support

--- src/main/java/com/alibaba/fastjson/support/odps/udf/GetJSONObject.java
+++ src/main/java/com/alibaba/fastjson/support/odps/udf/GetJSONObject.java
@@ -6,14 +6,13 @@ import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.aliyun.odps.udf.UDF;
 
 public class GetJSONObject extends UDF {
-    public GetJSONObject() {
-        // SerializeConfig.getGlobalInstance().setAsmEnable(false);
+    public GetJSONObject(){
+        SerializeConfig.getGlobalInstance().setAsmEnable(false);
     }
 
     public String evaluate(String jsonString, String path) throws Exception {
         Object json = JSON.parse(jsonString);
-        JSONPath json_path = new JSONPath(path);
-        Object result = json_path.eval(json);
+        Object result = JSONPath.eval(json, path);
         return JSON.toJSONString(result);
     }
 }

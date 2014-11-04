2377067cc9d0b9be143ee55a370f368ed1463a0b
Yako <yako@YakotekiMacBook-Pro.local>
2014-11-04 19:04:44 +0800

    bugfix for substring

--- src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
@@ -94,15 +94,19 @@ public final class JSONScanner extends JSONLexerBase {
      */
     public final String stringVal() {
         if (!hasSpecial) {
-            // return new String(buf, np + 1, sp);
-            return text.substring(np + 1, np + 1 + sp);
+            // return text.substring(np + 1, np + 1 + sp);
+            return this.subString(np + 1, sp);
         } else {
             return new String(sbuf, 0, sp);
         }
     }
 
     public final String subString(int offset, int count) {
-        return text.substring(offset, offset + count);
+        char[] chars = new char[count];
+        for (int i = offset; i < offset + count; ++i) {
+            chars[i - offset] = text.charAt(i);
+        }
+        return new String(chars);
     }
 
     public final String numberString() {
@@ -113,8 +117,8 @@ public final class JSONScanner extends JSONLexerBase {
             sp--;
         }
 
-        return text.substring(np, np + sp);
-        // return new String(buf, np, sp);
+        // return text.substring(np, np + sp);
+        return this.subString(np, sp);
     }
 
     public final int ISO8601_LEN_0 = "0000-00-00".length();

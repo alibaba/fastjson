fbe996818d337c428ca8dadb7fd2740d6338173a
{李彦慧}({026834}) <yanhui.liyh@alibaba-inc.com>
2014-11-19 11:37:41 +0800

    bugfix for true and false

--- src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
@@ -2392,7 +2392,7 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
         next();
 
         if (ch == ' ' || ch == ',' || ch == '}' || ch == ']' || ch == '\n' || ch == '\r' || ch == '\t' || ch == EOI
-            || ch == '\f' || ch == '\b') {
+            || ch == '\f' || ch == '\b' || ch == ':') {
             token = JSONToken.TRUE;
         } else {
             throw new JSONException("scan true error");
@@ -2567,7 +2567,7 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
         next();
 
         if (ch == ' ' || ch == ',' || ch == '}' || ch == ']' || ch == '\n' || ch == '\r' || ch == '\t' || ch == EOI
-            || ch == '\f' || ch == '\b') {
+            || ch == '\f' || ch == '\b' || ch == ':') {
             token = JSONToken.FALSE;
         } else {
             throw new JSONException("scan false error");

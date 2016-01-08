44c700391f41c3c64669e0f2bd71f17cbed47c71
高铁 <shaojin.wensj@alibaba-inc.com>
2016-01-08 11:59:41 +0800

    immutable value object support, https://github.com/alibaba/fastjson/issues/473

---
+++ src/test/java/com/alibaba/json/bvt/builder/BuilderTest0_private.java
@@ -0,0 +1,51 @@
+package com.alibaba.json.bvt.builder;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONType;
+
+import junit.framework.TestCase;
+
+public class BuilderTest0_private extends TestCase {
+    
+    public void test_0() throws Exception {
+        VO vo = JSON.parseObject("{\"id\":12304,\"name\":\"ljw\"}", VO.class);
+        
+        Assert.assertEquals(12304, vo.getId());
+        Assert.assertEquals("ljw", vo.getName());
+    }
+
+    @JSONType(builder=VOBuilder.class)
+    public static class VO {
+        private int id;
+        private String name;
+        
+        public int getId() {
+            return id;
+        }
+        
+        public String getName() {
+            return name;
+        }
+    }
+
+    private static class VOBuilder {
+
+        private VO vo = new VO();
+
+        public VO build() {
+            return vo;
+        }
+        
+        public VOBuilder withId(int id) {
+            vo.id = id;
+            return this;
+        }
+        
+        public VOBuilder withName(String name) {
+            vo.name = name;
+            return this;
+        }
+    }
+}

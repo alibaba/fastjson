a4c0c225a81bbeb5f4f52755b328285aefd33b8e
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-04 00:34:55 +0800

    decrement footprint

--- src/test/java/com/alibaba/json/bvt/parser/deser/ResolveFieldDeserializerTest.java
+++ src/test/java/com/alibaba/json/bvt/parser/deser/ResolveFieldDeserializerTest.java
@@ -1,5 +1,7 @@
 package com.alibaba.json.bvt.parser.deser;
 
+import java.util.Map;
+
 import com.alibaba.fastjson.parser.deserializer.ResolveFieldDeserializer;
 
 import junit.framework.TestCase;
@@ -7,7 +9,7 @@ import junit.framework.TestCase;
 
 public class ResolveFieldDeserializerTest extends TestCase {
     public void test_0 () throws Exception {
-        new ResolveFieldDeserializer(null, null).parseField(null, null, null, null);
+        new ResolveFieldDeserializer((Map) null, null).parseField(null, null, null, null);
         new ResolveFieldDeserializer(null, null, 0).parseField(null, null, null, null);
     }
 }

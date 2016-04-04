3ce55fbee3589d4b3dd2509cb32eb4b77dbd5ce6
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-04 17:27:05 +0800

    removed deprecated api

--- src/test/java/com/alibaba/json/bvt/serializer/JSONSerializerMapTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/JSONSerializerMapTest.java
@@ -1,16 +1,17 @@
 package com.alibaba.json.bvt.serializer;
 
 import org.junit.Assert;
-import junit.framework.TestCase;
 
 import com.alibaba.fastjson.serializer.IntegerCodec;
-import com.alibaba.fastjson.serializer.JSONSerializerMap;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+
+import junit.framework.TestCase;
 
 @SuppressWarnings("deprecation")
 public class JSONSerializerMapTest extends TestCase {
 
     public void test_0() throws Exception {
-        JSONSerializerMap map = new JSONSerializerMap();
+        SerializeConfig map = new SerializeConfig();
 
         Assert.assertFalse(0 == map.size());
         Assert.assertEquals(true, map.get(Integer.class) == IntegerCodec.instance);

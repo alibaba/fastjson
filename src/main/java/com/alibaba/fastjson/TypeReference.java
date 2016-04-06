a7e6952427f63aefb99ccd34f8d62447ed524071
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-06 09:33:35 +0800

    add comments

--- src/main/java/com/alibaba/fastjson/TypeReference.java
+++ src/main/java/com/alibaba/fastjson/TypeReference.java
@@ -3,6 +3,21 @@ package com.alibaba.fastjson;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+/**
+ * Represents a generic type {@code T}. Java doesn't yet provide a way to
+ * represent generic types, so this class does. Forces clients to create a
+ * subclass of this class which enables retrieval the type information even at
+ * runtime.
+ *
+ * <p>For example, to create a type literal for {@code List<String>}, you can
+ * create an empty anonymous inner class:
+ *
+ * <p>
+ * {@code} List<String> list = JSON.parseObject("...", new TypeReference<List<String>>() {});
+ *
+ *
+ * @author wenshao[szujobs@hotmail.com]
+ */
 public class TypeReference<T> {
 
     private final Type type;

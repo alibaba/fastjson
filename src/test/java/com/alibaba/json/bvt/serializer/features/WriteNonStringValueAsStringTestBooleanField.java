49d13c22d7658a67bac0203fb35057c96f84e49f
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-08 12:30:11 +0800

    bug fixed for EmtpyMap writeClassName, fixed issue 'https://github.com/alibaba/fastjson/issues/536'

--- src/test/java/com/alibaba/json/bvt/serializer/features/WriteNonStringValueAsStringTestBooleanField.java
+++ src/test/java/com/alibaba/json/bvt/serializer/features/WriteNonStringValueAsStringTestBooleanField.java
@@ -8,15 +8,17 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import junit.framework.TestCase;
 
 public class WriteNonStringValueAsStringTestBooleanField extends TestCase {
+
     public void test_0() throws Exception {
         VO vo = new VO();
         vo.id = true;
-        
-         Assert.assertEquals("{\"id\":\"true\"}", JSON.toJSONString(vo, SerializerFeature.WriteNonStringValueAsString));
-         Assert.assertEquals("{\"id\":true}", JSON.toJSONString(vo));
+
+        Assert.assertEquals("{\"id\":\"true\"}", JSON.toJSONString(vo, SerializerFeature.WriteNonStringValueAsString));
+        Assert.assertEquals("{\"id\":true}", JSON.toJSONString(vo));
     }
-    
+
     public static class VO {
+
         public boolean id;
     }
 }

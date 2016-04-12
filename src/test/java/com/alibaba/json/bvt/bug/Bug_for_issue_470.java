e2367b5a56cb2cf4ff40b99d70274de74d1f30d6
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-12 11:15:02 +0800

    add testcase for issue 470. https://github.com/alibaba/fastjson/issues/470

--- src/test/java/com/alibaba/json/bvt/bug/Bug_for_issue_470.java
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_issue_470.java
@@ -16,8 +16,14 @@ public class Bug_for_issue_470 extends TestCase {
         Assert.assertEquals(false, list.get(0).value);
     }
     
-    public void test_for_issue_1() throws Exception {
+    public void test_for_issue_private() throws Exception {
         List<V1> list = JSON.parseArray("[{\"value\":null}]", V1.class);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(false, list.get(0).value);
+    }
+    
+    public void test_for_issue_method() throws Exception {
+        List<V2> list = JSON.parseArray("[{\"value\":null}]", V2.class);
         Assert.assertEquals(1, list.size());
         Assert.assertEquals(false, list.get(0).value);
     }
@@ -28,5 +34,19 @@ public class Bug_for_issue_470 extends TestCase {
     
     private static class V1 {
         public boolean value;
+    }
+    
+    public static class V2 {
+        private boolean value;
+        
+        public boolean isValue() {
+            return value;
+        }
+
+        
+        public void setValue(boolean value) {
+            this.value = value;
+        }
+        
     }
 }

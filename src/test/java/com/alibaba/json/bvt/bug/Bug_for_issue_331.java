2aecc57d77ae29d288c7ceb1aaf203ccbd1485e3
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-13 20:35:09 +0800

    add testcase for issue 331. https://github.com/alibaba/fastjson/issues/331

--- src/test/java/com/alibaba/json/bvt/bug/Bug_for_issue_331.java
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_issue_331.java
@@ -7,8 +7,6 @@ import java.util.Date;
 import org.junit.Assert;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.JSONSerializer;
-import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import junit.framework.TestCase;

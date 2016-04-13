93d19f2d83ffa97a4c48c18c9b192a55c24ba433
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-13 17:50:04 +0800

    bug fixed for issue 291 & 406. https://github.com/alibaba/fastjson/issues/291 https://github.com/alibaba/fastjson/issues/406

--- src/test/java/com/alibaba/json/bvt/parser/fieldTypeResolver/FieldTypeResolverTest.java
+++ src/test/java/com/alibaba/json/bvt/parser/fieldTypeResolver/FieldTypeResolverTest.java
@@ -13,24 +13,27 @@ import junit.framework.TestCase;
 public class FieldTypeResolverTest extends TestCase {
 
     public void test_0() throws Exception {
-        String text = "{\"item_0\":{},\"item_1\":{}}";
-        
+        String text = "{\"item_0\":{\"value\":1001},\"item_1\":{\"value\":1002},\"item_2\":1001}";
+
         FieldTypeResolver fieldResolver = new FieldTypeResolver() {
 
             public Type resolve(Object object, String fieldName) {
                 if (fieldName.startsWith("item_")) {
                     return Item.class;
                 }
-                
+
                 return null;
             }
         };
-        
+
         JSONObject jsonObject = JSON.parseObject(text, JSONObject.class, fieldResolver);
         Assert.assertTrue(jsonObject.get("item_0") instanceof Item);
+        Assert.assertTrue(jsonObject.get("item_1") instanceof Item);
+        Assert.assertTrue(jsonObject.get("item_2") instanceof Integer);
     }
 
     public static class Item {
-        
+
+        public int value;
     }
 }

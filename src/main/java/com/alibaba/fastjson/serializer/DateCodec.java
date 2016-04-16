5ee9b2c3bf8b9adb4dbd07a4091c291fdbb000a9
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-16 22:44:13 +0800

    remove unused import

--- src/main/java/com/alibaba/fastjson/serializer/DateCodec.java
+++ src/main/java/com/alibaba/fastjson/serializer/DateCodec.java
@@ -22,7 +22,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;

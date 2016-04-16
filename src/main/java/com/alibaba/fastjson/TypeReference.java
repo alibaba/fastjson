2ce509c1fd60a55ba22b95af95709597133964fe
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-17 00:44:47 +0800

    remove typeference improved

--- src/main/java/com/alibaba/fastjson/TypeReference.java
+++ src/main/java/com/alibaba/fastjson/TypeReference.java
@@ -32,29 +32,6 @@ public class TypeReference<T> {
         type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
     }
     
-    /**
-     * @since 1.2.9 back port to 1.1.49.android
-     * @param actualTypeArguments
-     */
-    protected TypeReference(Type... actualTypeArguments){
-        Type superClass = getClass().getGenericSuperclass();
-
-        ParameterizedType argType = (ParameterizedType) ((ParameterizedType) superClass).getActualTypeArguments()[0];
-        Type rawType = argType.getRawType();
-        Type[] argTypes = argType.getActualTypeArguments();
-        
-        int actualIndex = 0;
-        for (int i = 0; i < argTypes.length; ++i) {
-            if (argTypes[i] instanceof TypeVariable) {
-                argTypes[i] = actualTypeArguments[actualIndex++];
-                if (actualIndex >= actualTypeArguments.length) {
-                    break;
-                }
-            }
-        }
-        type = new ParameterizedTypeImpl(argTypes, this.getClass(), rawType);
-    }
-    
     public Type getType() {
         return type;
     }

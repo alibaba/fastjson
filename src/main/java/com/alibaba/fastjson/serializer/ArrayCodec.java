dddbd43c35eef260c5bc395232a239edace0d742
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-17 12:34:27 +0800

    removed unused code

--- src/main/java/com/alibaba/fastjson/serializer/ArrayCodec.java
+++ src/main/java/com/alibaba/fastjson/serializer/ArrayCodec.java
@@ -170,41 +170,8 @@ public final class ArrayCodec implements ObjectSerializer, ObjectDeserializer {
             return (T) bytes;
         }
 
-        Class componentClass;
-        Type componentType;
-        if (type instanceof GenericArrayType) {
-            GenericArrayType clazz = (GenericArrayType) type;
-            componentType = clazz.getGenericComponentType();
-            if (componentType instanceof TypeVariable) {
-                TypeVariable typeVar = (TypeVariable) componentType;
-                Type objType = parser.getContext().type;
-                if (objType instanceof ParameterizedType) {
-                    ParameterizedType objParamType = (ParameterizedType) objType;
-                    Type objRawType = objParamType.getRawType();
-                    Type actualType = null;
-                    if (objRawType instanceof Class) {
-                        TypeVariable[] objTypeParams = ((Class) objRawType).getTypeParameters();
-                        for (int i = 0; i < objTypeParams.length; ++i) {
-                            if (objTypeParams[i].getName().equals(typeVar.getName())) {
-                                actualType = objParamType.getActualTypeArguments()[i];
-                            }
-                        }
-                    }
-                    if (actualType instanceof Class) {
-                        componentClass = (Class) actualType;
-                    } else {
-                        componentClass = Object.class;
-                    }
-                } else {
-                    componentClass = TypeUtils.getClass(typeVar.getBounds()[0]);
-                }
-            } else {
-                componentClass = TypeUtils.getClass(componentType);
-            }
-        } else {
-            Class clazz = (Class) type;
-            componentType = componentClass = clazz.getComponentType();
-        }
+        Class clazz = (Class) type;
+        Class componentClass = clazz.getComponentType();
         JSONArray array = new JSONArray();
         parser.parseArray(componentClass, array, fieldName);
 

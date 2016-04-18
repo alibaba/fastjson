34342bec4f11298c9a52997ad066c19e47726e1e
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-18 19:04:57 +0800

    refactor

--- src/main/java/com/alibaba/fastjson/serializer/SerialContext.java
+++ src/main/java/com/alibaba/fastjson/serializer/SerialContext.java
@@ -6,14 +6,12 @@ public class SerialContext {
     public final Object        object;
     public final Object        fieldName;
     public final int           features;
-    private int                fieldFeatures;
 
     public SerialContext(SerialContext parent, Object object, Object fieldName, int features, int fieldFeatures){
         this.parent = parent;
         this.object = object;
         this.fieldName = fieldName;
         this.features = features;
-        this.fieldFeatures = fieldFeatures;
     }
 
     public String toString() {
@@ -27,14 +25,5 @@ public class SerialContext {
             }
 
         }
-    }
-
-    public int getFeatures() {
-        return features;
-    }
-
-    public boolean isEnabled(SerializerFeature feature) {
-        int mask = feature.mask;
-        return (features & mask) != 0 || (fieldFeatures & mask) != 0;
     }
 }

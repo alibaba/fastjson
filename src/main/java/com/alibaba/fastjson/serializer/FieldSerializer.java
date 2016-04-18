6393f39c5f82149afe405f22434995ac5f1a1ac5
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-18 17:48:11 +0800

    refactor

--- src/main/java/com/alibaba/fastjson/serializer/FieldSerializer.java
+++ src/main/java/com/alibaba/fastjson/serializer/FieldSerializer.java
@@ -17,6 +17,7 @@ package com.alibaba.fastjson.serializer;
 
 import java.io.IOException;
 import java.lang.reflect.Member;
+import java.util.Collection;
 
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.annotation.JSONField;
@@ -25,14 +26,15 @@ import com.alibaba.fastjson.util.FieldInfo;
 /**
  * @author wenshao[szujobs@hotmail.com]
  */
-public abstract class FieldSerializer implements Comparable<FieldSerializer> {
+public final class FieldSerializer implements Comparable<FieldSerializer> {
     public final FieldInfo  fieldInfo;
     protected final boolean writeNull;
     protected final int     features;
     protected final String  format;
     
+    private RuntimeSerializerInfo runtimeInfo;
+    
     public FieldSerializer(FieldInfo fieldInfo){
-        super();
         this.fieldInfo = fieldInfo;
 
         boolean writeNull = false;
@@ -95,7 +97,75 @@ public abstract class FieldSerializer implements Comparable<FieldSerializer> {
         }
     }
 
-    public abstract void writeValue(JSONSerializer serializer, Object propertyValue) throws Exception;
+    public void writeValue(JSONSerializer serializer, Object propertyValue) throws Exception {
+        if (format != null) {
+            serializer.writeWithFormat(propertyValue, format);
+            return;
+        }
+
+        if (runtimeInfo == null) {
+
+            Class<?> runtimeFieldClass;
+            if (propertyValue == null) {
+                runtimeFieldClass = this.fieldInfo.fieldClass;
+            } else {
+                runtimeFieldClass = propertyValue.getClass();
+            }
+
+            ObjectSerializer fieldSerializer = serializer.config.get(runtimeFieldClass);
+            runtimeInfo = new RuntimeSerializerInfo(fieldSerializer, runtimeFieldClass);
+        }
+        
+        final RuntimeSerializerInfo runtimeInfo = this.runtimeInfo;
+
+        if (propertyValue == null) {
+            if ((features & SerializerFeature.WriteNullNumberAsZero.mask) != 0 // 
+                    && Number.class.isAssignableFrom(runtimeInfo.runtimeFieldClass)) {
+                serializer.out.write('0');
+                return;
+            } else if ((features & SerializerFeature.WriteNullStringAsEmpty.mask) != 0 // 
+                    && String.class == runtimeInfo.runtimeFieldClass) {
+                serializer.out.write("\"\"");
+                return;
+            } else if ((features & SerializerFeature.WriteNullBooleanAsFalse.mask) != 0 // 
+                    && Boolean.class == runtimeInfo.runtimeFieldClass) {
+                serializer.out.write("false");
+                return;
+            } else if ((features & SerializerFeature.WriteNullListAsEmpty.mask) != 0 // 
+                    && Collection.class.isAssignableFrom(runtimeInfo.runtimeFieldClass)) {
+                serializer.out.write("[]");
+                return;
+            }
+
+            runtimeInfo.fieldSerializer.write(serializer, null, fieldInfo.name, runtimeInfo.runtimeFieldClass);
+            return;
+        }
+
+        if ((features & SerializerFeature.WriteEnumUsingToString.mask) != 0 && runtimeInfo.runtimeFieldClass.isEnum()) {
+            serializer.out.writeString(((Enum<?>) propertyValue).name());
+            return;
+        }
+
+        Class<?> valueClass = propertyValue.getClass();
+        if (valueClass == runtimeInfo.runtimeFieldClass) {
+            runtimeInfo.fieldSerializer.write(serializer, propertyValue, fieldInfo.name, fieldInfo.fieldType);
+            return;
+        }
+
+        ObjectSerializer valueSerializer = serializer.config.get(valueClass);
+        valueSerializer.write(serializer, propertyValue, fieldInfo.name, fieldInfo.fieldType);
+    }
+    
+    static class RuntimeSerializerInfo {
+
+        ObjectSerializer fieldSerializer;
+        Class<?>         runtimeFieldClass;
+
+        public RuntimeSerializerInfo(ObjectSerializer fieldSerializer, Class<?> runtimeFieldClass){
+            this.fieldSerializer = fieldSerializer;
+            this.runtimeFieldClass = runtimeFieldClass;
+        }
+    }
     
     public int compareTo(FieldSerializer o) {
         return this.fieldInfo.compareTo(o.fieldInfo);

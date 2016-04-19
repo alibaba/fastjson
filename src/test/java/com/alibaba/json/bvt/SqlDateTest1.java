e1030370ce35dd6ec724bf6ba1339eca47d580d8
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-19 22:20:50 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/SqlDateTest1.java
+++ src/test/java/com/alibaba/json/bvt/SqlDateTest1.java
@@ -1,22 +1,28 @@
 package com.alibaba.json.bvt;
 
-import java.util.Date;
-
-import junit.framework.TestCase;
+import java.sql.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import org.junit.Assert;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
-public class DateTest extends TestCase {
+import junit.framework.TestCase;
 
+public class SqlDateTest1 extends TestCase {
+    protected void setUp() throws Exception {
+        JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        JSON.defaultLocale = new Locale("zh_CN");
+    }
+    
     public void test_date() throws Exception {
         long millis = 1324138987429L;
         Date date = new Date(millis);
 
         Assert.assertEquals("1324138987429", JSON.toJSONString(date));
-        Assert.assertEquals("new Date(1324138987429)", JSON.toJSONString(date, SerializerFeature.WriteClassName));
+        Assert.assertEquals("{\"@type\":\"java.sql.Date\",\"val\":1324138987429}", JSON.toJSONString(date, SerializerFeature.WriteClassName));
 
         Assert.assertEquals("\"2011-12-18 00:23:07\"",
                             JSON.toJSONString(date, SerializerFeature.WriteDateUseDateFormat));

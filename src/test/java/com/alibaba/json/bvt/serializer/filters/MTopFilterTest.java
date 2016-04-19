7aac9404cecf7058133bfd6f5480093946aa3eb3
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-19 15:03:26 +0800

    bug fixed for ValueFilter

--- src/test/java/com/alibaba/json/bvt/serializer/filters/MTopFilterTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/filters/MTopFilterTest.java
@@ -8,10 +8,13 @@ import com.alibaba.fastjson.serializer.ValueFilter;
 import junit.framework.TestCase;
 
 public class MTopFilterTest extends TestCase {
+
     public void test_0() throws Exception {
         Model model = new Model();
         model.id = 1001;
-        
+        model.user = new Person();
+        model.user.id = 2002;
+
         ValueFilter valueFilter = new ValueFilter() {
 
             @Override
@@ -22,22 +25,43 @@ public class MTopFilterTest extends TestCase {
         };
 
         String jsonString = JSON.toJSONString(model, valueFilter);
-        Assert.assertEquals("{\"id\":1001}", jsonString);
+        Assert.assertEquals("{\"id\":1001,\"user\":{\"id\":2002}}", jsonString);
     }
 
-    
     public static class Model {
-        private int id;
 
-        
+        private int    id;
+        private Person user;
+
         public int getId() {
             return id;
         }
 
-        
         public void setId(int id) {
             this.id = id;
         }
-        
+
+        public Person getUser() {
+            return user;
+        }
+
+        public void setUser(Person user) {
+            this.user = user;
+        }
+
+    }
+
+    public static class Person {
+
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
     }
 }

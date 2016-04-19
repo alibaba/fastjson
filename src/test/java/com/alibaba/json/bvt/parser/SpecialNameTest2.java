3ab9e9848ac971af8abf6c1bcc1a07c765f54e93
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-20 05:23:37 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/parser/SpecialNameTest2.java
+++ src/test/java/com/alibaba/json/bvt/parser/SpecialNameTest2.java
@@ -8,25 +8,25 @@ import com.alibaba.fastjson.annotation.JSONField;
 
 import junit.framework.TestCase;
 
-public class SpecialNameTest extends TestCase {
+public class SpecialNameTest2 extends TestCase {
     public void test_special_key() throws Exception {
-        String text = "{\"name\\\\\\\"\":1001,\"value\":\"xxx\\\\\\\"\"}";
+        String text = "{\"name\\\\\":1001,\"value\":\"xxx\\\\\"}";
         
         JSONObject obj = (JSONObject) JSON.parse(text);
-        Assert.assertEquals(1001, obj.get("name\\\""));
-        Assert.assertEquals("xxx\\\"", obj.get("value"));
+        Assert.assertEquals(1001, obj.get("name\\"));
+        Assert.assertEquals("xxx\\", obj.get("value"));
     }
     
     public void test_special_key_1() throws Exception {
-        String text = "{\"name\\\\\\\"\":1001,\"value\":\"xxx\\\\\\\"\"}";
+        String text = "{\"name\\\\\":1001,\"value\":\"xxx\\\\\"}";
         
         Model obj = JSON.parseObject(text, Model.class);
         Assert.assertEquals(1001, obj.name);
-        Assert.assertEquals("xxx\\\"", obj.value);
+        Assert.assertEquals("xxx\\", obj.value);
     }
     
     public static class Model {
-        @JSONField(name="name\\\"")
+        @JSONField(name="name\\")
         public int name;
         public String value;
     }

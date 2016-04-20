1ab6e6be55ea3fb4e4320d083bd198e907f52dfa
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-20 16:42:15 +0800

    bug fixed for comment

--- src/test/java/com/alibaba/json/test/benchmark/BenchmarkMain.java
+++ src/test/java/com/alibaba/json/test/benchmark/BenchmarkMain.java
@@ -10,8 +10,8 @@ public class BenchmarkMain {
 
         BenchmarkExecutor executor = new BenchmarkExecutor();
         executor.setExecuteCount(3);
-        executor.getCodecList().add(new FastjsonSCodec());
-//        executor.getCodecList().add(new FastjsonCodec());
+//        executor.getCodecList().add(new FastjsonSCodec());
+        executor.getCodecList().add(new FastjsonCodec());
 //        executor.getCodecList().add(new FastjsonBeanToArrayCodec());
 //        executor.getCodecList().add(new GsonCodec());
 //        executor.getCodecList().add(new Jackson2Codec());
@@ -25,8 +25,8 @@ public class BenchmarkMain {
 //        executor.getCaseList().add(new CartObjectParse());
 //        executor.getCaseList().add(new TradeParse());
 //        executor.getCaseList().add(new XueluDecode());
-         executor.getCaseList().add(new EishayEncode());
-//         executor.getCaseList().add(new EishayDecode());
+//         executor.getCaseList().add(new EishayEncode());
+         executor.getCaseList().add(new EishayDecode());
 //         executor.getCaseList().add(new GetHomePageResponseDecode());
 //         executor.getCaseList().add(new EishayTreeDecode());
 //         executor.getCaseList().add(new Eishay3Encode());

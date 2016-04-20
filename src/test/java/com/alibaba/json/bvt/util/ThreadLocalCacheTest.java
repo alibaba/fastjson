f5cf23e2244b2eb7d19be4da0d16b7bf7ef60c58
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-20 22:59:26 +0800

    fixed testcase & removed unused code

--- src/test/java/com/alibaba/json/bvt/util/ThreadLocalCacheTest.java
+++ src/test/java/com/alibaba/json/bvt/util/ThreadLocalCacheTest.java
@@ -1,5 +1,7 @@
 package com.alibaba.json.bvt.util;
 
+import java.lang.reflect.Field;
+
 import org.junit.Assert;
 
 import com.alibaba.fastjson.serializer.SerialWriterStringEncoder;
@@ -29,19 +31,28 @@ public class ThreadLocalCacheTest extends TestCase {
 
     public void testBytes() throws Exception {
 
-        SerialWriterStringEncoder.clearBytes();
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 1024);
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(1024).length, 1024);
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(2048).length, 2048);
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 2048);
+        clearBytes();
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 8192);
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(1024).length, 8192);
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(8192 * 2).length, 8192 * 2);
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 8192);
+        Assert.assertSame(SerialWriterStringEncoder.getBytes(0), SerialWriterStringEncoder.getBytes(1204));
+        Assert.assertNotSame(SerialWriterStringEncoder.getBytes(9000), SerialWriterStringEncoder.getBytes(9000));
 
-        SerialWriterStringEncoder.clearBytes();
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(2048).length, 2048);
+        clearBytes();
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(2048).length, 8192);
 
-        SerialWriterStringEncoder.clearBytes();
+        clearBytes();
         Assert.assertEquals(SerialWriterStringEncoder.getBytes(1024 * 256).length, 1024 * 256);
-        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 1024);
-        SerialWriterStringEncoder.clearBytes();
+        Assert.assertEquals(SerialWriterStringEncoder.getBytes(0).length, 8192);
+        clearBytes();
 
+    }
+    
+    public static void clearBytes() throws Exception {
+        Field field = SerialWriterStringEncoder.class.getDeclaredField("bytesBufLocal");
+        field.setAccessible(true);
+        ThreadLocal<byte[]> bytesBufLocal = (ThreadLocal<byte[]>) field.get(null);
+        bytesBufLocal.set(null);
     }
 }

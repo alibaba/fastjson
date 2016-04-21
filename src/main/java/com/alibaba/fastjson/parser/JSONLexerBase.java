d98801a92d056c3adc0916f9d7cfa53be98e376c
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-21 21:52:06 +0800

    add error info

--- src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
@@ -2963,18 +2963,22 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
 
     public final Number decimalValue(boolean decimal) {
         char chLocal = charAt(np + sp - 1);
-        if (chLocal == 'F') {
-            return Float.parseFloat(numberString());
-        }
-
-        if (chLocal == 'D') {
-            return Double.parseDouble(numberString());
-        }
-
-        if (decimal) {
-            return decimalValue();
-        } else {
-            return doubleValue();
+        try {
+            if (chLocal == 'F') {
+                return Float.parseFloat(numberString());
+            }
+    
+            if (chLocal == 'D') {
+                return Double.parseDouble(numberString());
+            }
+    
+            if (decimal) {
+                return decimalValue();
+            } else {
+                return doubleValue();
+            }
+        } catch (NumberFormatException ex) {
+            throw new JSONException(ex.getMessage() + ", " + info());
         }
     }
 

3790e91e91ca08a535bb74041ce60b091336628a
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-21 21:54:30 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/parser/number/NumberValueTest_error_3.java
@@ -0,0 +1,22 @@
+package com.alibaba.json.bvt.parser.number;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+
+import junit.framework.TestCase;
+
+public class NumberValueTest_error_3 extends TestCase {
+
+    public void test_0() throws Exception {
+        Exception error = null;
+        try {
+            String text = "{\"value\":33e+}";
+            JSON.parse(text);
+        } catch (JSONException e) {
+            error = e;
+        }
+        Assert.assertNotNull(error);
+    }
+}

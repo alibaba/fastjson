daf6f56fed5ce4c3844734d100f789ec2d6bb208
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-21 22:30:54 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/parser/number/NumberValueTest2.java
@@ -0,0 +1,18 @@
+package com.alibaba.json.bvt.parser.number;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import junit.framework.TestCase;
+
+public class NumberValueTest2 extends TestCase {
+
+    public void test_0() throws Exception {
+        String text = "{\"value\":3F}";
+        JSONObject obj = (JSONObject) JSON.parse(text);
+        Assert.assertTrue(3F == obj.getFloat("value").floatValue()); 
+
+    }
+}

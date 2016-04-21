66564fc87748af5ebc82797b0fb90cf5fa7cf5da
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-21 21:54:30 +0800

    add testcase

--- src/main/java/com/alibaba/fastjson/parser/JSONLexer.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexer.java
@@ -1777,17 +1777,15 @@ public final class JSONLexer {
 
     public final Number decimalValue(boolean decimal) {
         char chLocal = charAt(np + sp - 1);
-        if (chLocal == 'F') {
-            return Float.parseFloat(numberString());
-            // return Float.parseFloat(new String(buf, np, sp - 1));
-        }
-
-        if (chLocal == 'D') {
-            return Double.parseDouble(numberString());
-            // return Double.parseDouble(new String(buf, np, sp - 1));
-        }
-
         try {
+            if (chLocal == 'F') {
+                return Float.parseFloat(numberString());
+            }
+
+            if (chLocal == 'D') {
+                return Double.parseDouble(numberString());
+            }
+
             if (decimal) {
                 return decimalValue();
             } else {

b0615323c02d8f6cf56701defb7c0bd6e4811302
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-22 14:22:23 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/parser/error/ParseErrorTest_3.java
@@ -0,0 +1,25 @@
+package com.alibaba.json.bvt.parser.error;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+
+import junit.framework.TestCase;
+
+public class ParseErrorTest_3 extends TestCase {
+
+    public void test_for_error() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("{\"value\":", Model.class);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public static class Model {
+        public String value;
+    }
+}

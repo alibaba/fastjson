a48c1ebd94376b1e47355c998720b0995bb10bfc
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-23 17:46:18 +0800

    fastjson-1.1.51.android SNAPSHOT

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -617,5 +617,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         }
     }
 
-    public final static String VERSION = "1.1.50";
+    public final static String VERSION = "1.1.51";
 }

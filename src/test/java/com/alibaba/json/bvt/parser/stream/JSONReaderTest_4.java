1ef6df6e2e3726aa6fcd028dcc0594f4fa545f9d
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-23 15:20:42 +0800

    fixed testcase

--- src/test/java/com/alibaba/json/bvt/parser/stream/JSONReaderTest_4.java
+++ src/test/java/com/alibaba/json/bvt/parser/stream/JSONReaderTest_4.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
-import org.junit.Assert;
-
-import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONReader;
 
 import junit.framework.TestCase;
@@ -17,21 +14,15 @@ public class JSONReaderTest_4 extends TestCase {
         String text = "1001";
         JSONReader reader = new JSONReader(new MyReader(text));
 
-        Exception error = null;
-        try {
-            reader.close();
-        } catch (JSONException ex) {
-            error = ex;
-        }
-        Assert.assertNotNull(error);
+        reader.close();
     }
-    
+
     public static class MyReader extends BufferedReader {
 
         public MyReader(String s){
             super(new StringReader(s));
         }
-        
+
         public void close() throws IOException {
             throw new IOException();
         }

8314246674395f49d0786c97619057179c42b8fb
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-23 14:38:17 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/parser/setter/SetterTest_boolean.java
+++ src/test/java/com/alibaba/json/bvt/parser/setter/SetterTest_boolean.java
@@ -1,36 +1,45 @@
-package com.alibaba.json.bvt.parser.field;
+package com.alibaba.json.bvt.parser.setter;
 
 import com.alibaba.fastjson.JSON;
 
 import org.junit.Assert;
 import junit.framework.TestCase;
 
-public class PublicFieldTest_boolean extends TestCase {
-
-    public static class VO {
-
-        public boolean id;
-    }
+public class SetterTest_boolean extends TestCase {
 
     public void test_codec_false() throws Exception {
         VO vo = new VO();
         vo.id = false;
-        
+
         String str = JSON.toJSONString(vo);
-        
+
         VO vo1 = JSON.parseObject(str, VO.class);
-        
+
         Assert.assertTrue(vo1.id == vo.id);
     }
     
     public void test_codec_true() throws Exception {
         VO vo = new VO();
         vo.id = true;
-        
+
         String str = JSON.toJSONString(vo);
-        
+
         VO vo1 = JSON.parseObject(str, VO.class);
-        
+
         Assert.assertTrue(vo1.id == vo.id);
+    }
+
+    public static class VO {
+
+        private boolean id;
+
+        public boolean isId() {
+            return id;
+        }
+
+        public void setId(boolean id) {
+            this.id = id;
+        }
+
     }
 }

2ace6a9bb8e68a3a608a1b2d0d2f4428ed221f3e
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-28 12:26:53 +0800

    fixed testcase

--- src/test/java/com/alibaba/json/bvt/parser/array/BeanToArrayTest_date.java
+++ src/test/java/com/alibaba/json/bvt/parser/array/BeanToArrayTest_date.java
@@ -2,6 +2,8 @@ package com.alibaba.json.bvt.parser.array;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import org.junit.Assert;
 
@@ -11,7 +13,11 @@ import com.alibaba.fastjson.parser.Feature;
 import junit.framework.TestCase;
 
 public class BeanToArrayTest_date extends TestCase {
-
+    protected void setUp() throws Exception {
+        JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        JSON.defaultLocale = Locale.CHINA;
+    }
+    
     public void test_date() throws Exception {
         long time = System.currentTimeMillis();
         Model model = JSON.parseObject("[" + time + "]", Model.class, Feature.SupportArrayToBean);
@@ -24,7 +30,8 @@ public class BeanToArrayTest_date extends TestCase {
     }
     
     public void test_date2() throws Exception {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", JSON.defaultLocale);
+        dateFormat.setTimeZone(JSON.defaultTimeZone);
         Model model = JSON.parseObject("[\"2016-01-01\"]", Model.class, Feature.SupportArrayToBean);
         Assert.assertEquals(dateFormat.parse("2016-01-01").getTime(), model.v1.getTime());
     }

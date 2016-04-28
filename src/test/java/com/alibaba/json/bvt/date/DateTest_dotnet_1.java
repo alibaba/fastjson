c28ca5ad6d82cd76db99fb12f710defe1333de43
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-28 20:48:15 +0800

    improved dotnet date support

--- src/test/java/com/alibaba/json/bvt/date/DateTest_dotnet_1.java
+++ src/test/java/com/alibaba/json/bvt/date/DateTest_dotnet_1.java
@@ -8,22 +8,26 @@ import com.alibaba.fastjson.JSON;
 
 import junit.framework.TestCase;
 
-public class DateTest_net extends TestCase {
+public class DateTest_dotnet_1 extends TestCase {
+
     public void test_date() throws Exception {
         String text = "{\"date\":\"/Date(1461081600000)/\"}";
-        
+
         Model model = JSON.parseObject(text, Model.class);
         Assert.assertEquals(1461081600000L, model.date.getTime());
     }
-    
-    public void test_date_1() throws Exception {
-        String text = "{\"date\":\"/Date(1461081600000)/\"}";
-        
-        Model model = JSON.parseObject(text, Model.class);
-        Assert.assertEquals(1461081600000L, model.date.getTime());
-    }
-    
+
     public static class Model {
-        public Date date;
+
+        private Date date;
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
     }
 }
---
+++ src/test/java/com/alibaba/json/bvt/date/DateTest_dotnet_4.java
@@ -0,0 +1,34 @@
+package com.alibaba.json.bvt.date;
+
+import java.util.Date;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import junit.framework.TestCase;
+
+public class DateTest_dotnet_4 extends TestCase {
+
+    public void test_date() throws Exception {
+        String text = "{\"date\":\"/Date(1461081600321+5000)/\"}";
+
+        JSONObject model = JSON.parseObject(text);
+        Assert.assertEquals(1461081600321L, ((java.util.Date) model.getObject("date", java.util.Date.class)).getTime());
+    }
+
+    private static class Model {
+
+        private Date date;
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+    }
+}

0dd8e631c2e710314eb25651cc0c3e2ba3886b49
高铁 <shaojin.wensj@alibaba-inc.com>
2016-04-28 13:31:15 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/parser/array/BeanToArrayTest_long.java
+++ src/test/java/com/alibaba/json/bvt/parser/array/BeanToArrayTest_long.java
@@ -10,13 +10,19 @@ import junit.framework.TestCase;
 
 public class BeanToArrayTest_long extends TestCase {
 
-    public void test_bool() throws Exception {
+    public void test_long() throws Exception {
         Model model = JSON.parseObject("[-100,100]", Model.class, Feature.SupportArrayToBean);
         Assert.assertEquals(-100L, model.v1);
         Assert.assertEquals(100L, model.v2);
     }
+    
+    public void test_long_space() throws Exception {
+        Model model = JSON.parseObject("[-100 ,100 ]", Model.class, Feature.SupportArrayToBean);
+        Assert.assertEquals(-100L, model.v1);
+        Assert.assertEquals(100L, model.v2);
+    }
 
-    public void test_bool_error() throws Exception {
+    public void test_long_error() throws Exception {
         Exception error = null;
         try {
             JSON.parseObject("[-", Model.class, Feature.SupportArrayToBean);
@@ -26,7 +32,17 @@ public class BeanToArrayTest_long extends TestCase {
         Assert.assertNotNull(error);
     }
     
-    public void test_bool_error_max() throws Exception {
+    public void test_long_error_2() throws Exception {
+        Exception error = null;
+        try {
+            JSON.parseObject("[-1,", Model.class, Feature.SupportArrayToBean);
+        } catch (JSONException ex) {
+            error = ex;
+        }
+        Assert.assertNotNull(error);
+    }
+    
+    public void test_long_error_max() throws Exception {
         Exception error = null;
         try {
             JSON.parseObject("[1,92233720368547758000}", Model.class, Feature.SupportArrayToBean);
@@ -36,7 +52,7 @@ public class BeanToArrayTest_long extends TestCase {
         Assert.assertNotNull(error);
     }
     
-    public void test_bool_error_min() throws Exception {
+    public void test_long_error_min() throws Exception {
         Exception error = null;
         try {
             JSON.parseObject("[1,-92233720368547758000}", Model.class, Feature.SupportArrayToBean);

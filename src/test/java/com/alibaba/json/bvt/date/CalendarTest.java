a8cf16a0cc1c89d8a623770aac661cba9e1c7edb
高铁 <shaojin.wensj@alibaba-inc.com>
2016-05-01 18:59:37 +0800

    add testcase for Calendar

--- src/test/java/com/alibaba/json/bvt/date/CalendarTest.java
+++ src/test/java/com/alibaba/json/bvt/date/CalendarTest.java
@@ -7,14 +7,47 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class CalendarTest extends TestCase {
 
-    public void test_0() throws Exception {
+    public void test_null() throws Exception {
         String text = "{\"calendar\":null}";
         
         VO vo = JSON.parseObject(text, VO.class);
         Assert.assertNull(vo.getCalendar());
+    }
+    
+    public void test_codec() throws Exception {
+        Calendar calendar = Calendar.getInstance(JSON.defaultTimeZone, JSON.defaultLocale);
+        VO vo = new VO();
+        vo.setCalendar(calendar);
+        String text = JSON.toJSONString(vo);
+        
+        VO vo2 = JSON.parseObject(text, VO.class);
+        Assert.assertEquals(vo.getCalendar().getTimeInMillis(), vo2.getCalendar().getTimeInMillis());
+    }
+    
+    public void test_codec_iso88591() throws Exception {
+        Calendar calendar = Calendar.getInstance(JSON.defaultTimeZone, JSON.defaultLocale);
+        VO vo = new VO();
+        vo.setCalendar(calendar);
+        String text = JSON.toJSONString(vo, SerializerFeature.UseISO8601DateFormat);
+        
+        VO vo2 = JSON.parseObject(text, VO.class);
+        Assert.assertEquals(vo.getCalendar().getTimeInMillis(), vo2.getCalendar().getTimeInMillis());
+    }
+    
+    public void test_codec_iso88591_2() throws Exception {
+        Calendar calendar = Calendar.getInstance(JSON.defaultTimeZone, JSON.defaultLocale);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        VO vo = new VO();
+        vo.setCalendar(calendar);
+        String text = JSON.toJSONString(vo, SerializerFeature.UseISO8601DateFormat);
+        
+        VO vo2 = JSON.parseObject(text, VO.class);
+        Assert.assertEquals(vo.getCalendar().getTimeInMillis(), vo2.getCalendar().getTimeInMillis());
     }
 
     public static class VO {

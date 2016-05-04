04965ae933a71d4c9b6330cc9d83095d8947c1ee
高铁 <shaojin.wensj@alibaba-inc.com>
2016-05-04 09:42:32 +0800

    removed unused import

--- src/main/java/com/alibaba/fastjson/serializer/ASMSerializerFactory.java
+++ src/main/java/com/alibaba/fastjson/serializer/ASMSerializerFactory.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.annotation.JSONType;

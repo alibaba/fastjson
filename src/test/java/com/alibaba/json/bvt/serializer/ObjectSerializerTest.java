ff70f3b33142f62e67c88f2fb9016c4d8506a429
高铁 <shaojin.wensj@alibaba-inc.com>
2016-05-04 19:51:08 +0800

    add java doc

--- src/test/java/com/alibaba/json/bvt/serializer/ObjectSerializerTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/ObjectSerializerTest.java
@@ -17,43 +17,34 @@ public class ObjectSerializerTest extends TestCase {
     public void test_serialize() throws Exception {
         SerializeConfig config = new SerializeConfig();
         config.put(ResultCode.class, new ResultCodeSerilaizer());
-        
+
         Result result = new Result();
         result.code = ResultCode.SIGN_ERROR;
         String json = JSON.toJSONString(result, config);
         Assert.assertEquals("{\"code\":17}", json);
     }
-    
+
     public static class Result {
+
         public ResultCode code;
     }
 
     public static enum ResultCode {
-                            SUCCESS(1), 
-                            ERROR(-1), 
-                            UNKOWN_ERROR(999), 
-                            LOGIN_FAILURE(8), 
-                            INVALID_ARGUMENT(0),
-                            SIGN_ERROR(17);
+                                   SUCCESS(1), ERROR(-1), UNKOWN_ERROR(999), LOGIN_FAILURE(8), INVALID_ARGUMENT(0),
+                                   SIGN_ERROR(17);
 
-        private final int value;
+        public final int value;
 
         ResultCode(int value){
             this.value = value;
         }
-
-        public int getValue() {
-            return value;
-        }
     }
-    
+
     public static class ResultCodeSerilaizer implements ObjectSerializer {
-        public void write(JSONSerializer serializer, 
-                          Object object, 
-                          Object fieldName, 
-                          Type fieldType,
+
+        public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType,
                           int features) throws IOException {
-            serializer.write(((ResultCode) object).value); 
+            serializer.write(((ResultCode) object).value);
         }
     }
 }

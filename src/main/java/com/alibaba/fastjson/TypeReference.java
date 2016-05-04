26a2deeec688e4ad2856f380ef2ce502901af488
高铁 <shaojin.wensj@alibaba-inc.com>
2016-05-04 20:03:33 +0800

    add java doc

--- src/main/java/com/alibaba/fastjson/TypeReference.java
+++ src/main/java/com/alibaba/fastjson/TypeReference.java
@@ -6,10 +6,33 @@ import java.lang.reflect.TypeVariable;
 
 import com.alibaba.fastjson.util.ParameterizedTypeImpl;
 
+/** 
+ * Represents a generic type {@code T}. Java doesn't yet provide a way to
+ * represent generic types, so this class does. Forces clients to create a
+ * subclass of this class which enables retrieval the type information even at
+ * runtime.
+ *
+ * <p>For example, to create a type literal for {@code List<String>}, you can
+ * create an empty anonymous inner class:
+ *
+ * <pre>
+ * TypeReference&lt;List&lt;String&gt;&gt; list = new TypeReference<List<String>>() {};
+ * </pre>
+ * This syntax cannot be used to create type literals that have wildcard
+ * parameters, such as {@code Class<?>} or {@code List<? extends CharSequence>}.
+ */
 public class TypeReference<T> {
 
     protected final Type type;
 
+    /**
+     * Constructs a new type literal. Derives represented class from type
+     * parameter.
+     *
+     * <p>Clients create an empty anonymous subclass. Doing so embeds the type
+     * parameter in the anonymous class's type hierarchy so we can reconstitute it
+     * at runtime despite erasure.
+     */
     protected TypeReference(){
         Type superClass = getClass().getGenericSuperclass();
 
@@ -39,6 +62,9 @@ public class TypeReference<T> {
         type = new ParameterizedTypeImpl(argTypes, this.getClass(), rawType);
     }
     
+    /**
+     * Gets underlying {@code Type} instance.
+     */
     public Type getType() {
         return type;
     }

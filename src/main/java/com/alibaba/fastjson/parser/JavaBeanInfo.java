6f5465c1e8d5add7dd4b04bcbb9ccd74670dc694
高铁 <shaojin.wensj@alibaba-inc.com>
2016-05-07 03:46:00 +0800

    merged patch from pull 615. https://github.com/alibaba/fastjson/pull/615

--- src/main/java/com/alibaba/fastjson/parser/JavaBeanInfo.java
+++ src/main/java/com/alibaba/fastjson/parser/JavaBeanInfo.java
@@ -462,24 +462,22 @@ class JavaBeanInfo {
             }
         }
         
-        Field[] classfields;
+        List<Field> classfields;
         {
-            List<Field> result = new ArrayList<Field>(declaredFields.length);
+            classfields = new ArrayList<Field>(declaredFields.length);
             for (Field f : declaredFields) {
                 if ((f.getModifiers() & Modifier.PUBLIC) != 0) {
-                    result.add(f);
+                    classfields.add(f);
                 }
             }
             
             for (Class<?> c = clazz.getSuperclass(); c != null && c != Object.class; c = c.getSuperclass()) {
                 for (Field f : c.getDeclaredFields()) {
                     if ((f.getModifiers() & Modifier.PUBLIC) != 0) {
-                        result.add(f);
+                        classfields.add(f);
                     }
                 }
             }
-            classfields = new Field[result.size()];
-            result.toArray(classfields);
         }
 
         for (Field field : classfields) {

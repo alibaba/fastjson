2f8c9ddc7652c41b567e18cff978c52a44f4feb6
宇智 <wanxin.twx@alibaba-inc.com>
2016-05-11 16:23:10 +0800

    testcase for #606

--- src/test/java/com/alibaba/json/bvt/support/spring/mock/controller/FastJsonControllerTest.java
+++ src/test/java/com/alibaba/json/bvt/support/spring/mock/controller/FastJsonControllerTest.java
@@ -8,15 +8,17 @@
 package com.alibaba.json.bvt.support.spring.mock.controller;
 
 import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.json.bvt.support.spring.mock.entity.FastJsonParentTestVO;
 import com.alibaba.json.bvt.support.spring.mock.entity.FastJsonTestVO;
+import com.alibaba.json.test.entity.Company;
 
 /**
  * <p>Title: FastJsonControllerTest</p>
@@ -54,4 +56,11 @@ public class FastJsonControllerTest {
 		
 		return jsonObj;
 	}
+	
+	@ResponseBody
+    @RequestMapping(value = "test3", method = RequestMethod.POST)
+    public Company test3(HttpServletRequest request, HttpServletResponse response) {
+        Company company=new Company();
+        return company;
+    }
 }
--- src/test/java/com/alibaba/json/bvt/support/spring/mock/testcase/FastJsonHttpMessageConverterTest.java
+++ src/test/java/com/alibaba/json/bvt/support/spring/mock/testcase/FastJsonHttpMessageConverterTest.java
@@ -77,7 +77,7 @@ public class FastJsonHttpMessageConverterTest {
 	
 	@Test
     public void test3() throws Exception {
-	    List<Object> list=this.mockMvc.perform(post("/fastjson/test2")).andReturn().getResponse().getHeaderValues("Content-Length");
+	    List<Object> list=this.mockMvc.perform(post("/fastjson/test3")).andReturn().getResponse().getHeaderValues("Content-Length");
         Assert.assertNotEquals(list.size(), 0);
 	}
 }

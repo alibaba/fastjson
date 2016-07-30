f0cee9533691db2fd58e64fbfddefe9d06fd37f4
wenshao <szujobs@hotmail.com>
2016-07-30 20:55:08 +0800

    refacotr Swagger support

---
+++ src/main/java/com/alibaba/fastjson/support/springfox/SwaggerJsonSerializer.java
@@ -0,0 +1,31 @@
+package com.alibaba.fastjson.support.springfox;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+
+import springfox.documentation.spring.web.json.Json;
+
+/**
+ * Swagger的Json处理，解决/v2/api-docs获取不到内容导致获取不到API页面内容的问题
+ *
+ * @Author zhaiyongchao
+ * @Blog http://blog.didispace.com
+ * @Date 2016/07/04
+ */
+public class SwaggerJsonSerializer implements ObjectSerializer {
+
+    public final static SwaggerJsonSerializer instance = new SwaggerJsonSerializer();
+
+    public void write(JSONSerializer serializer, //
+                      Object object, Object fieldName, Type fieldType, int features) throws IOException {
+        SerializeWriter out = serializer.getWriter();
+        Json json = (Json) object;
+        String value = json.value();
+        out.write(value);
+    }
+
+}

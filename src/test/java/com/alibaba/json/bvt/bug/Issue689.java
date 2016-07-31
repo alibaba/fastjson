78ee339c12df03f977ec818a3d2efc0dfb35a2df
wenshao <szujobs@hotmail.com>
2016-07-31 17:32:06 +0800

    add testcase for issue #689

---
+++ src/test/java/com/alibaba/json/bvt/bug/Issue689.java
@@ -0,0 +1,22 @@
+package com.alibaba.json.bvt.bug;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import junit.framework.TestCase;
+
+public class Issue689 extends TestCase {
+    public void test_0() throws Exception {
+        Map<String, Object> map = Collections.singletonMap("value", "A B");
+        String json = JSON.toJSONString(map);
+        Assert.assertEquals("{\"value\":\"A\\u00A0B\"}", json);
+        
+        JSONObject obj = JSON.parseObject(json);
+        Assert.assertEquals(obj.get("value"), map.get("value"));
+    }
+}

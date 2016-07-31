fdba4894d9313f7c998c964348dcff2f72e5c9d6
wenshao <szujobs@hotmail.com>
2016-07-31 12:21:53 +0800

    fixed javadoc error

--- src/main/java/com/alibaba/fastjson/support/springfox/SwaggerJsonSerializer.java
+++ src/main/java/com/alibaba/fastjson/support/springfox/SwaggerJsonSerializer.java
@@ -12,9 +12,8 @@ import springfox.documentation.spring.web.json.Json;
 /**
  * Swagger的Json处理，解决/v2/api-docs获取不到内容导致获取不到API页面内容的问题
  *
- * @Author zhaiyongchao
- * @Blog http://blog.didispace.com
- * @Date 2016/07/04
+ * @author zhaiyongchao [http://blog.didispace.com]
+ * @since 1.2.15
  */
 public class SwaggerJsonSerializer implements ObjectSerializer {
 

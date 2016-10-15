4dac6a5cb7f04847e973629f3e3ffa6efd0a5cc9
wenshao <szujobs@hotmail.com>
2016-10-15 18:00:46 +0800

    1.2.19-SNAPSHOT

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -924,7 +924,7 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         return TypeUtils.cast(this, clazz, ParserConfig.getGlobalInstance());
     }
 
-    public final static String VERSION = "1.2.18";
+    public final static String VERSION = "1.2.19";
     
     private final static ThreadLocal<byte[]> bytesLocal = new ThreadLocal<byte[]>();
     private static byte[] allocateBytes(int length) {

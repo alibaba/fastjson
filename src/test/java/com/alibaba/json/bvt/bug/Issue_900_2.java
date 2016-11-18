e053ea42dc45ff4dd7e15911cd61509656965e7c
wenshao <szujobs@hotmail.com>
2016-11-18 15:26:36 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/bug/Issue_900_2.java
@@ -0,0 +1,24 @@
+package com.alibaba.json.bvt.bug;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONType;
+import com.alibaba.fastjson.parser.Feature;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+/**
+ * Created by wenshao on 2016/11/18.
+ */
+public class Issue_900_2 extends TestCase {
+    public void test_for_issue() throws Exception {
+        Model model = JSON.parseObject("{\"items\":[\"abc\"]}", Model.class);
+        assertEquals(1, model.items.size());
+        assertEquals("abc", model.items.get(0));
+    }
+
+    @JSONType(parseFeatures = Feature.SupportNonPublicField)
+    public static class Model {
+        private List<String> items;
+    }
+}

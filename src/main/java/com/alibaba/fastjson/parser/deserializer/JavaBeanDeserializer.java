4e6f7715b21b5972b995d025f18f9e7dcfb97fcb
wenshao <szujobs@hotmail.com>
2016-11-20 17:01:00 +0800

    non public field support ignore static fields.

--- src/main/java/com/alibaba/fastjson/parser/deserializer/JavaBeanDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/JavaBeanDeserializer.java
@@ -685,7 +685,7 @@ public class JavaBeanDeserializer implements ObjectDeserializer {
                         continue;
                     }
                     int fieldModifiers = field.getModifiers();
-                    if ((fieldModifiers & Modifier.FINAL) != 0) {
+                    if ((fieldModifiers & Modifier.FINAL) != 0 || (fieldModifiers & Modifier.STATIC) != 0) {
                         continue;
                     }
                     extraFieldDeserializers.put(fieldName, field);

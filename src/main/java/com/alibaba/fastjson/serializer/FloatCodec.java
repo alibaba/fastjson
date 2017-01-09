5aa64d99688249bf04c8e0c1eb1e1b9bbb298dc7
wenshao <szujobs@hotmail.com>
2017-01-09 22:47:17 +0800

    float/double/Float/Double serialize support format.

--- src/main/java/com/alibaba/fastjson/serializer/FloatCodec.java
+++ src/main/java/com/alibaba/fastjson/serializer/FloatCodec.java
@@ -17,6 +17,8 @@ package com.alibaba.fastjson.serializer;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser;
 import com.alibaba.fastjson.parser.JSONLexer;
@@ -28,8 +30,21 @@ import com.alibaba.fastjson.util.TypeUtils;
  * @author wenshao[szujobs@hotmail.com]
  */
 public class FloatCodec implements ObjectSerializer, ObjectDeserializer {
+    private NumberFormat decimalFormat;
 
     public static FloatCodec instance = new FloatCodec();
+
+    public FloatCodec(){
+
+    }
+
+    public FloatCodec(DecimalFormat decimalFormat){
+        this.decimalFormat = decimalFormat;
+    }
+
+    public FloatCodec(String decimalFormat){
+        this(new DecimalFormat(decimalFormat));
+    }
 
     public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
         SerializeWriter out = serializer.out;
@@ -39,8 +54,13 @@ public class FloatCodec implements ObjectSerializer, ObjectDeserializer {
             return;
         }
 
-        float floatValue = ((Float) object).floatValue(); 
-        out.writeFloat(floatValue, true);
+        float floatValue = ((Float) object).floatValue();
+        if (decimalFormat != null) {
+            String floatText = decimalFormat.format(floatValue);
+            out.write(floatText);
+        } else {
+            out.writeFloat(floatValue, true);
+        }
     }
     
     @SuppressWarnings("unchecked")
--- src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest2.java
+++ src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest2.java
@@ -2,15 +2,12 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sun.javafx.sg.prism.NGShape;
 import junit.framework.TestCase;
-
-import java.text.DecimalFormat;
 
 /**
  * Created by wenshao on 09/01/2017.
  */
-public class DoubleFormatTest extends TestCase {
+public class DoubleFormatTest2 extends TestCase {
     public void test_format() throws Exception {
         Model model = new Model();
         model.value = 123.45678D;
@@ -21,6 +18,6 @@ public class DoubleFormatTest extends TestCase {
 
     public static class Model {
         @JSONField(format = "0.00")
-        public double value;
+        public Double value;
     }
 }

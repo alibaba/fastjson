e89511ab142c43b04037a06bf7c6b2278993edff
wenshao <szujobs@hotmail.com>
2017-01-11 09:41:05 +0800

    improved BigDecimal parse performance.

--- src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONScanner.java
@@ -15,6 +15,7 @@
  */
 package com.alibaba.fastjson.parser;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -157,6 +158,25 @@ public final class JSONScanner extends JSONLexerBase {
         }
 
         return this.subString(np, sp);
+    }
+
+    public final BigDecimal decimalValue() {
+        char chLocal = charAt(np + sp - 1);
+
+        int sp = this.sp;
+        if (chLocal == 'L' || chLocal == 'S' || chLocal == 'B' || chLocal == 'F' || chLocal == 'D') {
+            sp--;
+        }
+
+        int offset = np, count = sp;
+        if (count < sbuf.length) {
+            text.getChars(offset, offset + count, sbuf, 0);
+            return new BigDecimal(sbuf, 0, count);
+        } else {
+            char[] chars = new char[count];
+            text.getChars(offset, offset + count, chars, 0);
+            return new BigDecimal(chars);
+        }
     }
 
     public final static int ISO8601_LEN_0 = "0000-00-00".length();

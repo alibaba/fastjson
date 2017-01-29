e92a6ea1ed10fc3f8b40c989ef71b64e4f0f1d01
wenshao <szujobs@hotmail.com>
2017-01-29 22:29:49 +0800

    restore ThreadLocalCache for compatible.

---
+++ src/test/java/com/alibaba/json/bvt/compatible/ThreadLocalCacheTest.java
@@ -0,0 +1,13 @@
+package com.alibaba.json.bvt.compatible;
+
+import com.alibaba.fastjson.util.ThreadLocalCache;
+import junit.framework.TestCase;
+
+/**
+ * Created by wenshao on 29/01/2017.
+ */
+public class ThreadLocalCacheTest extends TestCase{
+    public void test_threadCache() throws Exception {
+        ThreadLocalCache.getBytes(10);
+    }
+}

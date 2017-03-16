d5e16ff2e92d040d1b4cb081749f09566ace81df
wenshao <szujobs@hotmail.com>
2017-03-16 17:46:37 +0800

    add testcase for #1074

---
+++ src/test/java/com/alibaba/json/bvt/bug/Issue1074.java
@@ -0,0 +1,14 @@
+package com.alibaba.json.bvt.bug;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+
+/**
+ * Created by wenshao on 16/03/2017.
+ */
+public class Issue1074 extends TestCase {
+    public void test_for_issue() throws Exception {
+        String json = "/*xxx*/{}";
+        JSON.parseObject(json);
+    }
+}

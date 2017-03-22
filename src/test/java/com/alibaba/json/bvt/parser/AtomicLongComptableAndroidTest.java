edd5f7f256b0b3656b4bb1f4df747966a798b385
wenshao <szujobs@hotmail.com>
2017-03-22 12:08:19 +0800

    improved parser AtomicInteger/AtomicLong support. compatible for 1.1.x version.

---
+++ src/test/java/com/alibaba/json/bvt/parser/AtomicLongComptableAndroidTest.java
@@ -0,0 +1,30 @@
+package com.alibaba.json.bvt.parser;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by wenshao on 20/03/2017.
+ */
+public class AtomicLongComptableAndroidTest extends TestCase {
+    public void test_for_compatible_zero() throws Exception {
+        String text = "{\"andIncrement\":-1,\"andDecrement\":0}";
+
+        assertEquals(0, JSON.parseObject(text, AtomicLong.class).intValue());
+    }
+
+    public void test_for_compatible_six() throws Exception {
+        String text = "{\"andIncrement\":5,\"andDecrement\":6}";
+
+        assertEquals(6, JSON.parseObject(text, AtomicLong.class).intValue());
+    }
+
+    public void test_for_compatible_five() throws Exception {
+        String text = "{\"andDecrement\":6,\"andIncrement\":5}";
+
+        assertEquals(5, JSON.parseObject(text, AtomicLong.class).intValue());
+    }
+}

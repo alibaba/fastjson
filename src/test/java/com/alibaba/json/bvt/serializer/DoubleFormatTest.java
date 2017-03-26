2eac320a8cd6d0c1a8206285f8595380b5dec96e
wenshao <szujobs@hotmail.com>
2017-03-26 10:57:39 +0800

    fixed testcase. remove unused import.

--- src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
@@ -2,10 +2,7 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sun.javafx.sg.prism.NGShape;
 import junit.framework.TestCase;
-
-import java.text.DecimalFormat;
 
 /**
  * Created by wenshao on 09/01/2017.

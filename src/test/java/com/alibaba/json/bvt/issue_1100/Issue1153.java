56a5fbece746f5627ad20d21181d49284e034e9d
wenshao <szujobs@hotmail.com>
2017-05-08 19:24:03 +0800

    add testcase for issue #1153

--- src/test/java/com/alibaba/json/bvt/issue_1100/Issue1153.java
+++ src/test/java/com/alibaba/json/bvt/issue_1100/Issue1153.java
@@ -1,6 +1,7 @@
 package com.alibaba.json.bvt.issue_1100;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import junit.framework.TestCase;
 
 /**
@@ -13,6 +14,7 @@ public class Issue1153 extends TestCase {
                 "test : '//helo'\n" +
                 "}";
 
-        JSON.parseObject(json);
+        JSONObject jsonObject =JSON.parseObject(json);
+        System.out.println(jsonObject);
     }
 }

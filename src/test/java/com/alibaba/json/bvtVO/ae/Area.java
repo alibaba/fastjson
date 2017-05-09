726ff5c6596fb0cd92debd6e5fe0e4172463825d
wenshao <szujobs@hotmail.com>
2017-05-09 10:40:10 +0800

    bug fixed for TypeUtils.castJavaBean.

--- src/test/java/com/alibaba/json/bvtVO/ae/Area.java
+++ src/test/java/com/alibaba/json/bvtVO/ae/Area.java
@@ -5,16 +5,9 @@ import com.alibaba.fastjson.annotation.JSONType;
 /**
  * Created by huangliang on 17/4/12.
  */
-@JSONType(seeAlso = { FloorV2.class, FloorV1.class, Section.class })
 public interface Area {
+    public static final String TYPE_FLOOR = "floor";
+    public static final String TYPE_ITEM = "item";
 
-    public String SECTION_TYPE = "section";
-
-    public String FLOORV1_TYPE = "floorV1";
-
-    public String FLOORV2_TYPE = "floorV2";
-
-    public String getTemplateId();
-
-    public String getType();
+    String getName();
 }

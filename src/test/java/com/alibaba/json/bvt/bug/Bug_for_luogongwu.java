81b3e06dca0ce68193a3717dd0133de6c68b8e91
wenshao <szujobs@hotmail.com>
2017-06-19 08:37:52 +0800

    revert testcase.

--- src/test/java/com/alibaba/json/bvt/bug/Bug_for_luogongwu.java
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_luogongwu.java
@@ -2,6 +2,9 @@ package com.alibaba.json.bvt.bug;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.annotation.JSONType;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import junit.framework.TestCase;
 
@@ -12,72 +15,74 @@ import java.util.List;
  * Created by wenshao on 15/06/2017.
  */
 public class Bug_for_luogongwu extends TestCase {
+
     public void test_for_issue() throws Exception {
-        Foo foo = new Foo();
         List<IflowItemImage> imageList = new ArrayList<IflowItemImage>();
         IflowItemImage image = new IflowItemImage();
-        image.id = "72c7275c6c";
-        image.localUrl = "file:///data/user/0/com.uc.iflow.ark/cache/img_cache/offline_1ab359f34f78f28ff6fa2bf09c1492ea.jpg";
-        image.optimalHeight = 299;
-        image.optimalWidth = 0;
-        image.original_save_url = "offline@!@http://hl-img.peco.uodoo.com/hubble/app/sm/ddc78ebbc17a5145b2357a6a65fa16be.jpg";
-        image.title = "Manisha Kumari";
-        image.type = "jpg";
-        image.url = "offline@!@http://hl-img.peco.uodoo.com/hubble/app/sm/ddc78ebbc17a5145b2357a6a65fa16be.jpg;,,jpg;3,480x";
+        image.id = "72c7275c6b";
         imageList.add(image);
-        //、foo.images = imageList;
 
         imageList = new ArrayList();
         image = new IflowItemImage();
         image.id = "72c7275c6c";
-        image.localUrl = "file:///data/user/0/com.uc.iflow.ark/cache/img_cache/offline_23ade9680a67359bdc067667aad94ff5.jpg";
-        image.optimalHeight = 299;
-        image.optimalWidth = 480;
-        image.original_save_url = "offline@!@http://hl-img.peco.uodoo.com/hubble/app/sm/ddc78ebbc17a5145b2357a6a65fa16be.jpg";
-        image.title = "";
-        image.type = "JPG";
-        image.url = "offline@!@http://hl-img.peco.uodoo.com/hubble/app/sm/ddc78ebbc17a5145b2357a6a65fa16be.jpg;,,JPG;3,208x";
         imageList.add(image);
+
+        // force ASM
+        boolean asm = SerializeConfig.globalInstance.isAsmEnable();
+        SerializeConfig.globalInstance.setAsmEnable(true);
+
+        // Test ASM
+        Foo foo = new Foo();
         foo.thumbnails = imageList;
 
         String jsonString = JSON.toJSONString(foo);
         System.out.println(jsonString);
 
-        Foo foo1 = JSON.parseObject(jsonString, Foo.class);//fool的thumbnails反序列化不出来
+        Foo foo1 = JSON.parseObject(jsonString, Foo.class);
 
         assertEquals(1, foo1.thumbnails.size());
         assertNotNull(foo1.thumbnails.get(0));
         assertSame(foo1.getThumbnail(), foo1.thumbnails.get(0));
+
+
+        // test Not ASM
+        SerializeConfig.globalInstance.setAsmEnable(false);
+        FooNotAsm fooNotAsm = new FooNotAsm();
+        fooNotAsm.thumbnails = imageList;
+
+        jsonString = JSON.toJSONString(foo);
+        System.out.println(jsonString);
+
+        FooNotAsm fooNotAsm1 = JSON.parseObject(jsonString, FooNotAsm.class);
+
+        assertEquals(1, fooNotAsm1.thumbnails.size());
+        assertNotNull(fooNotAsm1.thumbnails.get(0));
+        assertSame(fooNotAsm1.getThumbnail(), fooNotAsm1.thumbnails.get(0));
+
+        // restore
+        SerializeConfig.globalInstance.setAsmEnable(asm);
+    }
+
+    @JSONType(asm=false)
+    public static class FooNotAsm {
+        @JSONField(serialzeFeatures = SerializerFeature.DisableCircularReferenceDetect)
+        public List<IflowItemImage> thumbnails;
+
+        public IflowItemImage getThumbnail() {
+            return thumbnails != null && thumbnails.size() > 0 ? thumbnails.get(0) : null;
+        }
     }
 
     public static class Foo {
-        public List<IflowItemImage> thumbnails;
-        public List<IflowItemImage> images;
-
         @JSONField(serialzeFeatures = SerializerFeature.DisableCircularReferenceDetect)
+        public List<IflowItemImage> thumbnails;
+
         public IflowItemImage getThumbnail() {
             return thumbnails != null && thumbnails.size() > 0 ? thumbnails.get(0) : null;
         }
     }
 
     public static class IflowItemImage {
-
         public String id;
-
-        public String title;
-
-        public String url;
-
-        public String type;
-
-        public int optimalWidth;
-
-        public int optimalHeight;
-
-        public String original_save_url;
-
-        public String phash;
-        // FIXME: 17/6/9 客户端则使用的字段，预读功能中下载图片的本地url
-        public String localUrl;
     }
 }

0db4602af1bb38edaa2a2b3996f36c2fb5f9f230
kimmking <kimmking@163.com>
2017-06-22 22:12:09 +0800

    add timezone in testcase

--- src/test/java/com/alibaba/json/bvt/issue_1200/Issue1285.java
+++ src/test/java/com/alibaba/json/bvt/issue_1200/Issue1285.java
@@ -2,23 +2,31 @@ package com.alibaba.json.bvt.issue_1200;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import junit.framework.TestCase;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by kimmking on 22/06/2017.
  */
 public class Issue1285 extends TestCase {
+
+    protected void setUp() throws Exception {
+        JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        JSON.defaultLocale = Locale.CHINA;
+    }
+
     public void test_for_issue() throws Exception {
 
         Date date = new Date();
         SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+        df1.setTimeZone(JSON.defaultTimeZone);
         SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        df2.setTimeZone(JSON.defaultTimeZone);
         String json1 = "{\"createDate\":\""+df1.format(date)+"\"}";
         String json2 = "{\"createDate\":\""+df2.format(date)+"\"}";
 

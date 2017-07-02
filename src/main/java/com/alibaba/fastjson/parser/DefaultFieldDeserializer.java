0b76a4651800db3e0a948b36293847e921bbe883
wenshao <szujobs@hotmail.com>
2017-07-02 15:01:47 +0800

    bug fixed for multilevel-generic. for issue #569

--- src/main/java/com/alibaba/fastjson/parser/DefaultFieldDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/DefaultFieldDeserializer.java
@@ -2,6 +2,7 @@ package com.alibaba.fastjson.parser;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.Map;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser.ResolveTask;
@@ -9,6 +10,8 @@ import com.alibaba.fastjson.parser.deserializer.FieldDeserializer;
 import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
 import com.alibaba.fastjson.serializer.DateCodec;
 import com.alibaba.fastjson.util.FieldInfo;
+import com.alibaba.fastjson.util.ParameterizedTypeImpl;
+import com.alibaba.fastjson.util.TypeUtils;
 
 public class DefaultFieldDeserializer extends FieldDeserializer {
 
@@ -37,12 +40,31 @@ public class DefaultFieldDeserializer extends FieldDeserializer {
             objContext.type = objectType;
         }
 
+        Type fieldType = fieldInfo.fieldType;
+        if (fieldType instanceof ParameterizedType
+                && objectType instanceof ParameterizedType) {
+            ParameterizedType fieldParamType = (ParameterizedType) fieldType;
+            ParameterizedType objectParamType = (ParameterizedType) objectType;
+
+            Type[] args = fieldParamType.getActualTypeArguments();
+
+
+            Type rawType = objectParamType.getRawType();
+            if (rawType instanceof Class) {
+                Class<?> rawClass = (Class) rawType;
+                TypeVariable[] typeParameters = rawClass.getTypeParameters();
+                boolean changed = TypeUtils.getArgument(args, typeParameters, objectParamType.getActualTypeArguments());
+                if (changed) {
+                    fieldType = new ParameterizedTypeImpl(args, fieldParamType.getOwnerType(), fieldParamType.getRawType());
+                }
+            }
+        }
         String format = fieldInfo.format;
         Object value;
         if (format != null && fieldValueDeserilizer instanceof DateCodec) {
-            value = ((DateCodec) fieldValueDeserilizer).deserialze(parser, fieldInfo.fieldType, fieldInfo.name, format);
+            value = ((DateCodec) fieldValueDeserilizer).deserialze(parser, fieldType, fieldInfo.name, format);
         } else {
-            value = fieldValueDeserilizer.deserialze(parser, fieldInfo.fieldType, fieldInfo.name);
+            value = fieldValueDeserilizer.deserialze(parser, fieldType, fieldInfo.name);
         }
         if (parser.resolveStatus == DefaultJSONParser.NeedToResolve) {
             ResolveTask task = parser.getLastResolveTask();
--- src/main/java/com/alibaba/fastjson/util/FieldInfo.java
+++ src/main/java/com/alibaba/fastjson/util/FieldInfo.java
@@ -189,7 +189,7 @@ public class FieldInfo implements Comparable<FieldInfo> {
                     if (superType instanceof ParameterizedType) {
                         ParameterizedType p_superType = (ParameterizedType) superType;
                         Type[] p_superType_args = p_superType.getActualTypeArguments();
-                        getArgument(p_superType_args, c.getTypeParameters(), arguments);
+                        TypeUtils.getArgument(p_superType_args, c.getTypeParameters(), arguments);
                         arguments = p_superType_args;
                     }
                 }
@@ -310,35 +310,6 @@ public class FieldInfo implements Comparable<FieldInfo> {
         }
 
         return fieldType;
-    }
-
-    private static boolean getArgument(Type[] typeArgs, TypeVariable[] typeVariables, Type[] arguments) {
-        if (arguments == null || typeVariables.length == 0) {
-            return false;
-        }
-
-        boolean changed = false;
-        for (int i = 0; i < typeArgs.length; ++i) {
-            Type typeArg = typeArgs[i];
-            if (typeArg instanceof ParameterizedType) {
-                ParameterizedType p_typeArg = (ParameterizedType) typeArg;
-                Type[] p_typeArg_args = p_typeArg.getActualTypeArguments();
-                boolean p_changed = getArgument(p_typeArg_args, typeVariables, arguments);
-                if (p_changed) {
-                    typeArgs[i] = new ParameterizedTypeImpl(p_typeArg_args, p_typeArg.getOwnerType(), p_typeArg.getRawType());
-                    changed = true;
-                }
-            } else if (typeArg instanceof TypeVariable) {
-                for (int j = 0; j < typeVariables.length; ++j) {
-                    if (typeArg.equals(typeVariables[j])) {
-                        typeArgs[i] = arguments[j];
-                        changed = true;
-                    }
-                }
-            }
-        }
-
-        return changed;
     }
 
     public String toString() {

ea8544e8807cb2aba1cc3d255799323b8b2ba8c8
wenshao <szujobs@hotmail.com>
2017-07-16 12:14:37 +0800

    add javadoc

--- src/main/java/com/alibaba/fastjson/parser/deserializer/PropertyProcessable.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/PropertyProcessable.java
@@ -3,9 +3,20 @@ package com.alibaba.fastjson.parser.deserializer;
 import java.lang.reflect.Type;
 
 /**
- * Created by wenshao on 15/07/2017.
+ * @author wenshao[szujobs@hotmail.com]
  */
 public interface PropertyProcessable extends ParseProcess {
+    /**
+     * provide property's type, {@code java.lang.Class} or {@code Type}
+     * @param name property name
+     * @return property's type
+     */
     Type getType(String name);
+
+    /**
+     * apply property name and value
+     * @param name property name
+     * @param value property name
+     */
     void apply(String name, Object value);
 }

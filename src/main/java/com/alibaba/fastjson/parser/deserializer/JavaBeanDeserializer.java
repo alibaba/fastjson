c50bf15e78dd1c752299ff126661cce2de24ff74
wenshao <szujobs@hotmail.com>
2017-07-16 12:39:23 +0800

    bug fixed.

--- src/main/java/com/alibaba/fastjson/parser/deserializer/JavaBeanDeserializer.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/JavaBeanDeserializer.java
@@ -223,7 +223,7 @@ public class JavaBeanDeserializer implements ObjectDeserializer {
     }
     
     public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
-        return deserialze(parser, type, fieldName, null, 0, null);
+        return deserialze(parser, type, fieldName, 0);
     }
 
     public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName, int features) {

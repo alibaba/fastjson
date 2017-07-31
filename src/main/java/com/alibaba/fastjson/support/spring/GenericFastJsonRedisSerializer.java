91ea274b6d507f46a38bdd6f3864aae0fc2a1d55
kimmking <kimmking@163.com>
2017-08-01 00:26:26 +0800

    fixed for GBK encoding

--- src/main/java/com/alibaba/fastjson/support/spring/GenericFastJsonRedisSerializer.java
+++ src/main/java/com/alibaba/fastjson/support/spring/GenericFastJsonRedisSerializer.java
@@ -3,6 +3,7 @@ package com.alibaba.fastjson.support.spring;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.util.IOUtils;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
@@ -33,7 +34,7 @@ public class GenericFastJsonRedisSerializer implements RedisSerializer<Object> {
             return null;
         }
         try {
-            return JSON.parseObject(new String(bytes), Object.class, defaultRedisConfig);
+            return JSON.parseObject(new String(bytes, IOUtils.UTF8), Object.class, defaultRedisConfig);
         } catch (Exception ex) {
             throw new SerializationException("Could not deserialize: " + ex.getMessage(), ex);
         }

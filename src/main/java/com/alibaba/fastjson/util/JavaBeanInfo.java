2e31a4374ae23377e53dbad7e35ed15ad92adf7d
wenshao <szujobs@hotmail.com>
2017-08-05 20:39:25 +0800

    imporved koltin support.

--- src/main/java/com/alibaba/fastjson/util/JavaBeanInfo.java
+++ src/main/java/com/alibaba/fastjson/util/JavaBeanInfo.java
@@ -661,7 +661,8 @@ public class JavaBeanInfo {
     public static Constructor<?> getCreatorConstructor(Class<?> clazz) {
         Constructor<?> creatorConstructor = null;
 
-        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
             JSONCreator annotation = constructor.getAnnotation(JSONCreator.class);
             if (annotation != null) {
                 if (creatorConstructor != null) {
@@ -670,6 +671,35 @@ public class JavaBeanInfo {
 
                 creatorConstructor = constructor;
                 // 不应该break，否则多个构造方法上存在 JSONCreator 注解时，并不会触发上述异常抛出
+            }
+        }
+        if (creatorConstructor != null) {
+            return creatorConstructor;
+        }
+
+        for (Constructor constructor : constructors) {
+            Annotation[][] paramAnnotationArrays = constructor.getParameterAnnotations();
+            boolean match = true;
+            for (Annotation[] paramAnnotationArray : paramAnnotationArrays) {
+                boolean paramMatch = false;
+                for (Annotation paramAnnotation : paramAnnotationArray) {
+                    if (paramAnnotation instanceof JSONField) {
+                        paramMatch = true;
+                        break;
+                    }
+                }
+                if (!paramMatch) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                if (creatorConstructor != null) {
+                    throw new JSONException("multi-JSONCreator");
+                }
+
+                creatorConstructor = constructor;
             }
         }
         return creatorConstructor;

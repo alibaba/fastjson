7020ab1647944c753076f298b2c2a2bc1c2acf82
wenshao <szujobs@hotmail.com>
2017-08-10 14:04:02 +0800

    bug fixed for kotlin nullable dataclass. #1141

--- src/test/java/com/alibaba/json/bvt/kotlin/ResponseKotlin2Test.java
+++ src/test/java/com/alibaba/json/bvt/kotlin/ResponseKotlin2Test.java
@@ -10,10 +10,10 @@ import java.io.InputStream;
 /**
  * Created by wenshao on 10/08/2017.
  */
-public class ResponseKotlinTest extends TestCase {
+public class ResponseKotlin2Test extends TestCase {
     public void test_kotlin() throws Exception {
         ExtClassLoader classLoader = new ExtClassLoader();
-        Class clazz = classLoader.loadClass("ResponseKotlin");
+        Class clazz = classLoader.loadClass("ResponseKotlin2");
 
         String json = "{\"text\":\"robohorse\",\"value\":99}";
         Object obj = JSON.parseObject(json, clazz);
@@ -21,7 +21,7 @@ public class ResponseKotlinTest extends TestCase {
 
         String json2 = "{\"text\":\"robohorse\"}";
         Object obj2 = JSON.parseObject(json2, clazz);
-        assertEquals("{\"t1ext\":\"robohorse\"}", JSON.toJSONString(obj2));
+        assertEquals("{\"text\":\"robohorse\"}", JSON.toJSONString(obj2));
     }
 
     public static class ExtClassLoader extends ClassLoader {
@@ -31,11 +31,11 @@ public class ResponseKotlinTest extends TestCase {
 
             {
                 byte[] bytes;
-                InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("kotlin/ResponseKotlin.clazz");
+                InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("kotlin/ResponseKotlin2.clazz");
                 bytes = IOUtils.toByteArray(is);
                 is.close();
 
-                super.defineClass("ResponseKotlin", bytes, 0, bytes.length);
+                super.defineClass("ResponseKotlin2", bytes, 0, bytes.length);
             }
         }
     }

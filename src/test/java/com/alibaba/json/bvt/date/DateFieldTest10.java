3970466221df2e7641942b449dbb0a020dca8f0c
wenshao <szujobs@hotmail.com>
2017-08-16 11:05:48 +0800

    improved datacodec.

--- src/test/java/com/alibaba/json/bvt/date/DateFieldTest10.java
+++ src/test/java/com/alibaba/json/bvt/date/DateFieldTest10.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by wenshao on 07/04/2017.
@@ -12,6 +13,14 @@ import java.util.Date;
 public class DateFieldTest10 extends TestCase {
     public void test_for_zero() throws Exception {
         String text = "{\"date\":\"0000-00-00\"}";
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-DD");
+        Object object = format.parse("0000-00-00");
+        JSON.parseObject(text, Model.class);
+    }
+
+    public void test_1() throws Exception {
+        String text = "{\"date\":\"2017-08-14 19:05:30.000|America/Los_Angeles\"}";
 
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-DD");
         Object object = format.parse("0000-00-00");

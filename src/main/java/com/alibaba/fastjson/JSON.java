1843ac338a367be096222eb89fb98e3a94983fa4
wenshao <szujobs@hotmail.com>
2017-08-17 14:26:58 +0800

    1.2.38

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -1007,5 +1007,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         parser.handleResovleTask(value);
     }
 
-    public final static String VERSION = "1.2.37";
+    public final static String VERSION = "1.2.38";
 }

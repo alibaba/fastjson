78ea667d027b72bb2266c527c52dc8bc42ca6b2d
wenshao <szujobs@hotmail.com>
2017-08-18 23:48:48 +0800

    improved BigDecimal/BigInteger property performance.

--- src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
@@ -3362,30 +3362,22 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
 
         BigDecimal value;
         if (chLocal >= '0' && chLocal <= '9') {
-            long intVal = chLocal - '0';
-
             for (;;) {
                 chLocal = charAt(bp + (offset++));
                 if (chLocal >= '0' && chLocal <= '9') {
-                    intVal = intVal * 10 + (chLocal - '0');
                     continue;
                 } else {
                     break;
                 }
             }
 
-            int scale = 1;
             boolean small = (chLocal == '.');
             if (small) {
                 chLocal = charAt(bp + (offset++));
                 if (chLocal >= '0' && chLocal <= '9') {
-                    intVal = intVal * 10 + (chLocal - '0');
-                    scale = 1;
                     for (;;) {
                         chLocal = charAt(bp + (offset++));
                         if (chLocal >= '0' && chLocal <= '9') {
-                            intVal = intVal * 10 + (chLocal - '0');
-                            scale++;
                             continue;
                         } else {
                             break;
@@ -3429,6 +3421,138 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
 
             char[] chars = this.sub_chars(start, count);
             value = new BigDecimal(chars);
+        } else if (chLocal == 'n' && charAt(bp + offset) == 'u' && charAt(bp + offset + 1) == 'l' && charAt(bp + offset + 2) == 'l') {
+            matchStat = VALUE_NULL;
+            value = null;
+            offset += 3;
+            chLocal = charAt(bp + offset++);
+
+            if (quote && chLocal == '"') {
+                chLocal = charAt(bp + offset++);
+            }
+
+            for (;;) {
+                if (chLocal == ',') {
+                    bp += offset;
+                    this.ch = charAt(bp);
+                    matchStat = VALUE_NULL;
+                    token = JSONToken.COMMA;
+                    return value;
+                } else if (chLocal == '}') {
+                    bp += offset;
+                    this.ch = charAt(bp);
+                    matchStat = VALUE_NULL;
+                    token = JSONToken.RBRACE;
+                    return value;
+                } else if (isWhitespace(chLocal)) {
+                    chLocal = charAt(bp + offset++);
+                    continue;
+                }
+                break;
+            }
+            matchStat = NOT_MATCH;
+            return null;
+        } else {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        if (chLocal == ',') {
+            bp += offset;
+            this.ch = this.charAt(bp);
+            matchStat = VALUE;
+            token = JSONToken.COMMA;
+            return value;
+        }
+
+        if (chLocal == '}') {
+            chLocal = charAt(bp + (offset++));
+            if (chLocal == ',') {
+                token = JSONToken.COMMA;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == ']') {
+                token = JSONToken.RBRACKET;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == '}') {
+                token = JSONToken.RBRACE;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == EOI) {
+                token = JSONToken.EOF;
+                bp += (offset - 1);
+                ch = EOI;
+            } else {
+                matchStat = NOT_MATCH;
+                return null;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return null;
+        }
+
+        return value;
+    }
+
+    public BigInteger scanFieldBigInteger(char[] fieldName) {
+        matchStat = UNKNOWN;
+
+        if (!charArrayCompare(fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return null;
+        }
+
+        int offset = fieldName.length;
+        char chLocal = charAt(bp + (offset++));
+        final boolean quote = chLocal == '"';
+        if (quote) {
+            chLocal = charAt(bp + (offset++));
+        }
+
+        boolean negative = chLocal == '-';
+        if (negative) {
+            chLocal = charAt(bp + (offset++));
+        }
+
+        BigInteger value;
+        if (chLocal >= '0' && chLocal <= '9') {
+            long intVal = chLocal - '0';
+            for (;;) {
+                chLocal = charAt(bp + (offset++));
+                if (chLocal >= '0' && chLocal <= '9') {
+                    intVal = intVal * 10 + (chLocal - '0');
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            int start, count;
+            if (quote) {
+                if (chLocal != '"') {
+                    matchStat = NOT_MATCH;
+                    return null;
+                } else {
+                    chLocal = charAt(bp + (offset++));
+                }
+                start = bp + fieldName.length + 1;
+                count = bp + offset - start - 2;
+            } else {
+                start = bp + fieldName.length;
+                count = bp + offset - start - 1;
+            }
+
+            if (count < 20 || (negative && count < 21)) {
+                value = BigInteger.valueOf(negative ? -intVal : intVal);
+            } else {
+
+//            char[] chars = this.sub_chars(negative ? start + 1 : start, count);
+//            value = new BigInteger(chars, )
+                String strVal = this.subString(start, count);
+                value = new BigInteger(strVal);
+            }
         } else if (chLocal == 'n' && charAt(bp + offset) == 'u' && charAt(bp + offset + 1) == 'l' && charAt(bp + offset + 2) == 'l') {
             matchStat = VALUE_NULL;
             value = null;
--- src/test/java/com/alibaba/json/test/benchmark/basic/BigIntegerBenchmark.java
+++ src/test/java/com/alibaba/json/test/benchmark/basic/BigIntegerBenchmark.java
@@ -2,28 +2,28 @@ package com.alibaba.json.test.benchmark.basic;
 
 import com.alibaba.fastjson.JSON;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  * Created by wenshao on 04/08/2017.
  */
-public class BigDecimalBenchmark {
-    static String json = "{\"v1\":0.4430165316544028,\"v2\":0.22676692048907365,\"v3\":0.9766986818812096,\"v4\":0.3423751102308744,\"v5\":0.4262177938610565}";
-    static String json2 = "{\"v1\":\"0.4430165316544028\",\"v2\":\"0.22676692048907365\",\"v3\":\"0.9766986818812096\",\"v4\":\"0.3423751102308744\",\"v5\":\"0.4262177938610565\"}";
+public class BigIntegerBenchmark {
+    static String json = "{\"v1\":-1883391953414482124,\"v2\":-3019416596934963650,\"v3\":6497525620823745793,\"v4\":2136224289077142499,\"v5\":-2090575024006307745}";
+    static String json2 = "{\"v1\":\"-1883391953414482124\",\"v2\":\"-3019416596934963650\",\"v3\":\"6497525620823745793\",\"v4\":\"2136224289077142499\",\"v5\":\"-2090575024006307745\"}";
 
     public static void main(String[] args) throws Exception {
 //        Model model = new Model();
-//        model.v1 = new Random().nextDouble();
-//        model.v2 = new Random().nextDouble();
-//        model.v3 = new Random().nextDouble();
-//        model.v4 = new Random().nextDouble();
-//        model.v5 = new Random().nextDouble();
+//        model.v1 = new Random().nextLong();
+//        model.v2 = new Random().nextLong();
+//        model.v3 = new Random().nextLong();
+//        model.v4 = new Random().nextLong();
+//        model.v5 = new Random().nextLong();
 //
 //        System.out.println(JSON.toJSONString(model));
 
         for (int i = 0; i < 10; ++i) {
-//             perf(); // 6806
-            perf2(); // 7181
+             perf(); // 3162
+//            perf2(); // 3265
         }
     }
 
@@ -46,10 +46,10 @@ public class BigDecimalBenchmark {
     }
 
     public static class Model {
-        public BigDecimal v1;
-        public BigDecimal v2;
-        public BigDecimal v3;
-        public BigDecimal v4;
-        public BigDecimal v5;
+        public BigInteger v1;
+        public BigInteger v2;
+        public BigInteger v3;
+        public BigInteger v4;
+        public BigInteger v5;
     }
 }

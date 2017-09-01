8e3d1c1b5790e94a566e88a270d265e58e7f10ac
wenshao <szujobs@hotmail.com>
2017-09-01 22:13:56 +0800

    ParserConfig add method constainsKey. #1242

--- src/test/java/com/alibaba/json/bvt/parser/ParserConfigRegisterTest2.java
+++ src/test/java/com/alibaba/json/bvt/parser/ParserConfigRegisterTest2.java
@@ -11,10 +11,12 @@ import junit.framework.TestCase;
 
 public class ParserConfigRegisterTest2 extends TestCase {
     public void test_register() throws Exception {
+        assertFalse(ParserConfig.getGlobalInstance().containsKey(Model.class));
         ObjectDeserializer obj = ParserConfig.getGlobalInstance() //
                 .registerIfNotExists(Model.class, Modifier.PUBLIC, true, false, false, false);
         ObjectDeserializer obj2 = ParserConfig.getGlobalInstance().registerIfNotExists(Model.class);
         Assert.assertSame(obj, obj2);
+        assertTrue(ParserConfig.getGlobalInstance().containsKey(Model.class));
     }
     
     public static class Model {

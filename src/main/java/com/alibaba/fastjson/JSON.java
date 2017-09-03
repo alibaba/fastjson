406cac3383cdeb68b67a71024fc23e0780af5122
wenshao <szujobs@hotmail.com>
2017-09-03 21:17:52 +0800

    1.1.63.android-SNAPSHOT

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -629,5 +629,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         }
     }
 
-    public final static String VERSION = "1.1.62";
+    public final static String VERSION = "1.1.63";
 }

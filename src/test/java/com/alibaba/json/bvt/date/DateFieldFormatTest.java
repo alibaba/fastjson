2e17cf837e5394c3a0dd2709eb0383b09b907696
kimmking <kimmking@163.com>
2017-10-05 18:35:06 +0800

    add test code for #1510

--- src/test/java/com/alibaba/json/bvt/date/DateFieldFormatTest.java
+++ src/test/java/com/alibaba/json/bvt/date/DateFieldFormatTest.java
@@ -1,11 +1,13 @@
 package com.alibaba.json.bvt.date;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 
 public class DateFieldFormatTest extends TestCase {
     
@@ -14,11 +16,21 @@ public class DateFieldFormatTest extends TestCase {
         Model model = new Model();
         model.serverTime = now;
         model.publishTime = now;
+        model.setStartDate( now );
         
         String text = JSON.toJSONString(model);
         System.out.println(text);
+
+        String t1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(now);
+        String t2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(now);
+        String t3 = new SimpleDateFormat("yyyy-MM-dd").format(now);
+
+        Assert.assertEquals("{\"publishTime\":\""+t2+"\",\"serverTime\":\""+t1+"\",\"startDate\":\""+t3+"\"}",text);
         
         Model model2 = JSON.parseObject(text, Model.class);
+        Assert.assertEquals(t2,new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(model2.publishTime));
+        Assert.assertEquals(t1,new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(model2.serverTime));
+        Assert.assertEquals(t3,new SimpleDateFormat("yyyy-MM-dd").format(model2.getStartDate()));
         
     }
 
@@ -29,5 +41,16 @@ public class DateFieldFormatTest extends TestCase {
 
         @JSONField(format = "yyyy/MM/dd HH:mm:ss")
         public Date publishTime;
+
+        @JSONField(format = "yyyy-MM-dd")
+        private Date startDate;
+
+        public Date getStartDate() {
+            return startDate;
+        }
+
+        public void setStartDate(Date startDate) {
+            this.startDate = startDate;
+        }
     }
 }

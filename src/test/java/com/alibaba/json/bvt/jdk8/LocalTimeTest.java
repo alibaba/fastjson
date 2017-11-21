b60ede5bf196be2dfa4aedadaa267ffc4aacfcc8
wenshao <szujobs@hotmail.com>
2017-11-21 19:18:32 +0800

    add timezone support, for pull request #1594

--- src/test/java/com/alibaba/json/bvt/jdk8/LocalTimeTest.java
+++ src/test/java/com/alibaba/json/bvt/jdk8/LocalTimeTest.java
@@ -1,6 +1,8 @@
 package com.alibaba.json.bvt.jdk8;
 
 import java.time.LocalTime;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import junit.framework.TestCase;
 
@@ -9,7 +11,11 @@ import org.junit.Assert;
 import com.alibaba.fastjson.JSON;
 
 public class LocalTimeTest extends TestCase {
-
+    protected void setUp() throws Exception {
+        JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        JSON.defaultLocale = Locale.CHINA;
+    }
+    
     public void test_for_issue() throws Exception {
         VO vo = new VO();
         vo.setDate(LocalTime.now());

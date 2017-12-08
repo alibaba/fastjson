c8c4fa9763db3da219228c0d65c986bc171ab006
wenshao <szujobs@hotmail.com>
2017-12-08 11:45:09 +0800

    add more toJSONBytes method. for issue #1628

--- src/test/java/com/alibaba/json/bvt/issue_1600/Issue1628.java
+++ src/test/java/com/alibaba/json/bvt/issue_1600/Issue1628.java
@@ -1,6 +1,8 @@
 package com.alibaba.json.bvt.issue_1600;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeFilter;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
 import junit.framework.TestCase;
 
@@ -13,6 +15,22 @@ public class Issue1628 extends TestCase {
         map.put("a", 1001);
         map.put("b", 2002);
         byte[] bytes = JSON.toJSONBytes(map, new SimplePropertyPreFilter("a"));
+        assertEquals("{\"a\":1001}", new String(bytes));
+    }
+
+    public void test_toJSONBytes_1() throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("a", 1001);
+        map.put("b", 2002);
+        byte[] bytes = JSON.toJSONBytes(map, new SerializeFilter[] {new SimplePropertyPreFilter("a")});
+        assertEquals("{\"a\":1001}", new String(bytes));
+    }
+
+    public void test_toJSONBytes_2() throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("a", 1001);
+        map.put("b", 2002);
+        byte[] bytes = JSON.toJSONBytes(map, SerializeConfig.globalInstance, new SimplePropertyPreFilter("a"));
         assertEquals("{\"a\":1001}", new String(bytes));
     }
 }

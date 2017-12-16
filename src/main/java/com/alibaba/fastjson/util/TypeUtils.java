b273b032cac979d514e464f12ee57e1e3dd167e9
wenshao <szujobs@hotmail.com>
2017-12-16 12:30:57 +0800

    TypeUtils.cast support java.sql.Time. for issue #1644

--- src/main/java/com/alibaba/fastjson/util/TypeUtils.java
+++ src/main/java/com/alibaba/fastjson/util/TypeUtils.java
@@ -415,6 +415,47 @@ public class TypeUtils{
         return new java.sql.Date(longValue);
     }
 
+    public static java.sql.Time castToSqlTime(Object value){
+        if(value == null){
+            return null;
+        }
+        if(value instanceof java.sql.Time){
+            return (java.sql.Time) value;
+        }
+        if(value instanceof java.util.Date){
+            return new java.sql.Time(((java.util.Date) value).getTime());
+        }
+        if(value instanceof Calendar){
+            return new java.sql.Time(((Calendar) value).getTimeInMillis());
+        }
+        long longValue = 0;
+        if(value instanceof Number){
+            longValue = ((Number) value).longValue();
+        }
+        if(value instanceof String){
+            String strVal = (String) value;
+            if(strVal.length() == 0 //
+                    || "null".equals(strVal) //
+                    || "NULL".equals(strVal)){
+                return null;
+            }
+            if(isNumber(strVal)){
+                longValue = Long.parseLong(strVal);
+            } else{
+                JSONScanner scanner = new JSONScanner(strVal);
+                if(scanner.scanISO8601DateIfMatch(false)){
+                    longValue = scanner.getCalendar().getTime().getTime();
+                } else{
+                    throw new JSONException("can not cast to Timestamp, value : " + strVal);
+                }
+            }
+        }
+        if(longValue <= 0){
+            throw new JSONException("can not cast to Date, value : " + value); // TODO 忽略 1970-01-01 之前的时间处理？
+        }
+        return new java.sql.Time(longValue);
+    }
+
     public static java.sql.Timestamp castToTimestamp(Object value){
         if(value == null){
             return null;
@@ -706,6 +747,9 @@ public class TypeUtils{
         }
         if(clazz == java.sql.Date.class){
             return (T) castToSqlDate(obj);
+        }
+        if(clazz == java.sql.Time.class){
+            return (T) castToSqlTime(obj);
         }
         if(clazz == java.sql.Timestamp.class){
             return (T) castToTimestamp(obj);

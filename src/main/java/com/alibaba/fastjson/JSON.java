121ce9039014835c5d0ba3865d70f415294ca158
wenshao <szujobs@hotmail.com>
2018-01-21 20:56:25 +0800

    1.2.46-SNAPSHOT

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -1059,5 +1059,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         parser.handleResovleTask(value);
     }
 
-    public final static String VERSION = "1.2.45";
+    public final static String VERSION = "1.2.46";
 }

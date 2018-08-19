41a26cf34c74989d0cd8b2218f0a59370d319405
wenshao <szujobs@hotmail.com>
2018-08-19 12:53:00 +0800

    remove unused code & add testcase.

---
+++ src/test/java/com/alibaba/json/bvt/util/JavaBeanInfoTest.java
@@ -0,0 +1,10 @@
+package com.alibaba.json.bvt.util;
+
+import com.alibaba.fastjson.util.JavaBeanInfo;
+import junit.framework.TestCase;
+
+public class JavaBeanInfoTest extends TestCase {
+    public void test_0() throws Exception {
+        JavaBeanInfo.getBuilderClass(null);
+    }
+}

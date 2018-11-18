2926bb840b4a5f0503543e205b774fecdfe8157d
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2018-11-18 13:06:17 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/issue_2000/Issue2086.java
+++ src/test/java/com/alibaba/json/bvt/issue_2000/Issue2086.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
 public class Issue2086 extends TestCase {
     public void test_for_issue() throws Exception {
         JSON.parseObject("{\"id\":123}", Model.class);
+        JSON.toJSONString(new Model());
     }
 
     public static class Model {

18077cb8c4b476d492c3ac1ea7e18c4a2456e233
ORDTesters <ordtesters@gmail.com>
2018-11-22 17:00:20 -0800

    Fixed other issues with timezone and locale incorrectly set in DateTest, DateTest_tz, Issue1298, Issue1679, and Issue1977

--- src/test/java/com/alibaba/json/bvt/issue_1900/Issue1977.java
+++ src/test/java/com/alibaba/json/bvt/issue_1900/Issue1977.java
@@ -4,7 +4,15 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import junit.framework.TestCase;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class Issue1977 extends TestCase {
+    protected void setUp() throws Exception {
+        JSON.defaultTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        JSON.defaultLocale = Locale.CHINA;
+    }
+
     public void test_for_issue() throws Exception {
         java.sql.Date date = new java.sql.Date(1533265119604L);
         String json = JSON.toJSONString(date, SerializerFeature.UseISO8601DateFormat);

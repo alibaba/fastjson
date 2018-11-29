0f8067d91391280a3d2cdf908d9525b6d4dfcabb
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2018-11-29 20:08:59 +0800

    update version to 1.2.55

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -1062,5 +1062,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         parser.handleResovleTask(value);
     }
 
-    public final static String VERSION = "1.2.54";
+    public final static String VERSION = "1.2.55";
 }

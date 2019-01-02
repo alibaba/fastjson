41beb7102a1970cf106e535f1bf55edebc16a788
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2019-01-02 11:23:53 +0800

    bug fixed for toJavaObject, for issue #2216

--- src/test/java/com/alibaba/json/bvt/issue_2100/Issue2216.java
+++ src/test/java/com/alibaba/json/bvt/issue_2100/Issue2216.java
@@ -14,6 +14,13 @@ public class Issue2216 extends TestCase {
         assertEquals(1546072129000L, model.value.getTime());
     }
 
+    public void test_for_issue_2() throws Exception {
+        Model model = JSON.parseObject("{\"value\":\"20181229162849000+0800\"}").toJavaObject(Model.class);
+        assertNotNull(model);
+        assertNotNull(model.value);
+        assertEquals(1546072129000L, model.value.getTime());
+    }
+
     public static class Model {
         @JSONField(format = "yyyyMMddHHmmssSSSZ")
         public Date value;

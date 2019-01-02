04007da969d0875eb8fbbf5631fc40368f06975d
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2019-01-02 11:17:43 +0800

    add testcase for issue #2216

---
+++ src/test/java/com/alibaba/json/bvt/issue_2100/Issue2216.java
@@ -0,0 +1,21 @@
+package com.alibaba.json.bvt.issue_2100;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+public class Issue2216 extends TestCase {
+    public void test_for_issue() throws Exception {
+        Model model = JSON.parseObject("{\"value\":\"20181229162849000+0800\"}", Model.class);
+        assertNotNull(model);
+        assertNotNull(model.value);
+        assertEquals(1546072129000L, model.value.getTime());
+    }
+
+    public static class Model {
+        @JSONField(format = "yyyyMMddHHmmssSSSZ")
+        public Date value;
+    }
+}

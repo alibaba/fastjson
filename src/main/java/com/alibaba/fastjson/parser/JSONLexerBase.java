63319fd55737e9c69316f4081abdb9a5386d4bc0
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2019-01-18 22:25:55 +0800

    improved enum deserialize, for issue #2249

--- src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexerBase.java
@@ -1473,6 +1473,77 @@ public abstract class JSONLexerBase implements JSONLexer, Closeable {
         return hash;
     }
 
+    public long scanEnumSymbol(char[] fieldName) {
+        matchStat = UNKNOWN;
+
+        if (!charArrayCompare(fieldName)) {
+            matchStat = NOT_MATCH_NAME;
+            return 0;
+        }
+
+        int offset = fieldName.length;
+        char chLocal = charAt(bp + (offset++));
+
+        if (chLocal != '"') {
+            matchStat = NOT_MATCH;
+            return 0;
+        }
+
+        long hash = 0xcbf29ce484222325L;
+        for (;;) {
+            chLocal = charAt(bp + (offset++));
+            if (chLocal == '\"') {
+                chLocal = charAt(bp + (offset++));
+                break;
+            }
+
+            hash ^= ((chLocal >= 'A' && chLocal <= 'Z') ? (chLocal + 32) : chLocal);
+            hash *= 0x100000001b3L;
+
+            if (chLocal == '\\') {
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+        }
+
+        if (chLocal == ',') {
+            bp += offset;
+            this.ch = this.charAt(bp);
+            matchStat = VALUE;
+            return hash;
+        }
+
+        if (chLocal == '}') {
+            chLocal = charAt(bp + (offset++));
+            if (chLocal == ',') {
+                token = JSONToken.COMMA;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == ']') {
+                token = JSONToken.RBRACKET;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == '}') {
+                token = JSONToken.RBRACE;
+                bp += offset;
+                this.ch = this.charAt(bp);
+            } else if (chLocal == EOI) {
+                token = JSONToken.EOF;
+                bp += (offset - 1);
+                ch = EOI;
+            } else {
+                matchStat = NOT_MATCH;
+                return 0;
+            }
+            matchStat = END;
+        } else {
+            matchStat = NOT_MATCH;
+            return 0;
+        }
+
+        return hash;
+    }
+
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public Enum<?> scanEnum(Class<?> enumClass, final SymbolTable symbolTable, char serperator) {
         String name = scanSymbolWithSeperator(symbolTable, serperator);

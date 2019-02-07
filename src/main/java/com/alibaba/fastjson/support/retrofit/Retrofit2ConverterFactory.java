2ce3e13e8a6b3573bd3b5fb1ea14cf67bfb2934c
VictorZeng <Victor.Zxy@outlook.com>
2019-02-07 23:14:28 +0800

    add comment for FastJsonConfig

--- src/main/java/com/alibaba/fastjson/support/retrofit/Retrofit2ConverterFactory.java
+++ src/main/java/com/alibaba/fastjson/support/retrofit/Retrofit2ConverterFactory.java
@@ -58,16 +58,16 @@ public class Retrofit2ConverterFactory extends Converter.Factory {
 
     @Override
     public Converter<ResponseBody, Object> responseBodyConverter(Type type, //
-                                                            Annotation[] annotations, //
-                                                            Retrofit retrofit) {
+                                                                 Annotation[] annotations, //
+                                                                 Retrofit retrofit) {
         return new ResponseBodyConverter<Object>(type);
     }
 
     @Override
     public Converter<Object, RequestBody> requestBodyConverter(Type type, //
-                                                          Annotation[] parameterAnnotations, //
-                                                          Annotation[] methodAnnotations, //
-                                                          Retrofit retrofit) {
+                                                               Annotation[] parameterAnnotations, //
+                                                               Annotation[] methodAnnotations, //
+                                                               Retrofit retrofit) {
         return new RequestBodyConverter<Object>();
     }
 
@@ -80,54 +80,129 @@ public class Retrofit2ConverterFactory extends Converter.Factory {
         return this;
     }
 
+    /**
+     * Gets parser config.
+     *
+     * @return the parser config
+     * @see FastJsonConfig#getParserConfig()
+     * @deprecated
+     */
     @Deprecated
     public ParserConfig getParserConfig() {
         return fastJsonConfig.getParserConfig();
     }
 
+    /**
+     * Sets parser config.
+     *
+     * @param config the config
+     * @return the parser config
+     * @see FastJsonConfig#setParserConfig(ParserConfig)
+     * @deprecated
+     */
     @Deprecated
     public Retrofit2ConverterFactory setParserConfig(ParserConfig config) {
         fastJsonConfig.setParserConfig(config);
         return this;
     }
 
+    /**
+     * Gets parser feature values.
+     *
+     * @return the parser feature values
+     * @see JSON#DEFAULT_PARSER_FEATURE
+     * @deprecated
+     */
     @Deprecated
     public int getParserFeatureValues() {
         return JSON.DEFAULT_PARSER_FEATURE;
     }
 
+    /**
+     * Sets parser feature values.
+     *
+     * @param featureValues the feature values
+     * @return the parser feature values
+     * @see JSON#DEFAULT_PARSER_FEATURE
+     * @deprecated
+     */
     @Deprecated
     public Retrofit2ConverterFactory setParserFeatureValues(int featureValues) {
         return this;
     }
 
+    /**
+     * Get parser features feature [].
+     *
+     * @return the feature []
+     * @see FastJsonConfig#getFeatures()
+     * @deprecated
+     */
     @Deprecated
     public Feature[] getParserFeatures() {
         return fastJsonConfig.getFeatures();
     }
 
+    /**
+     * Sets parser features.
+     *
+     * @param features the features
+     * @return the parser features
+     * @see FastJsonConfig#setFeatures(Feature...)
+     * @deprecated
+     */
     @Deprecated
     public Retrofit2ConverterFactory setParserFeatures(Feature[] features) {
         fastJsonConfig.setFeatures(features);
         return this;
     }
 
+    /**
+     * Gets serialize config.
+     *
+     * @return the serialize config
+     * @see FastJsonConfig#getSerializeConfig()
+     * @deprecated
+     */
     @Deprecated
     public SerializeConfig getSerializeConfig() {
         return fastJsonConfig.getSerializeConfig();
     }
 
+    /**
+     * Sets serialize config.
+     *
+     * @param serializeConfig the serialize config
+     * @return the serialize config
+     * @see FastJsonConfig#setSerializeConfig(SerializeConfig)
+     * @deprecated
+     */
     @Deprecated
     public Retrofit2ConverterFactory setSerializeConfig(SerializeConfig serializeConfig) {
         fastJsonConfig.setSerializeConfig(serializeConfig);
         return this;
     }
 
+    /**
+     * Get serializer features serializer feature [].
+     *
+     * @return the serializer feature []
+     * @see FastJsonConfig#getSerializerFeatures()
+     * @deprecated
+     */
     @Deprecated
     public SerializerFeature[] getSerializerFeatures() {
         return fastJsonConfig.getSerializerFeatures();
     }
 
+    /**
+     * Sets serializer features.
+     *
+     * @param features the features
+     * @return the serializer features
+     * @see FastJsonConfig#setSerializerFeatures(SerializerFeature...)
+     * @deprecated
+     */
     @Deprecated
     public Retrofit2ConverterFactory setSerializerFeatures(SerializerFeature[] features) {
         fastJsonConfig.setSerializerFeatures(features);

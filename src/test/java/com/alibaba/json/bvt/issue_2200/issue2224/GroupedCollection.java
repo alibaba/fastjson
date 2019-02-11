acd2acf08ffa84270c345f0a69b32343162e3ada
TimAndy <xuchonglei@126.com>
2019-02-11 15:04:22 +0800

    add parameterized item testcase for issue 2224

---
+++ src/test/java/com/alibaba/json/bvt/issue_2200/issue2224/GroupedCollection.java
@@ -0,0 +1,6 @@
+package com.alibaba.json.bvt.issue_2200.issue2224;
+
+import java.util.List;
+
+abstract class GroupedCollection<TKey, TItem> extends KeyedCollection<TKey, List<TItem>> {
+}
---
+++ src/test/java/com/alibaba/json/bvt/issue_2200/issue2224/StringGroupedCollection.java
@@ -0,0 +1,4 @@
+package com.alibaba.json.bvt.issue_2200.issue2224;
+
+abstract class StringGroupedCollection<TItem> extends GroupedCollection<String, TItem> {
+}

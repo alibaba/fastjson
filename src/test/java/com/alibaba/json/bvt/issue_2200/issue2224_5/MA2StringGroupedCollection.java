07add5449ef0bc9dc977891207d5dbd3d5130ad3
TimAndy <xuchonglei@126.com>
2019-02-15 10:01:28 +0800

    testcase for issue #2224 of support inherit with other parameterized type and item type is generic array contains array

---
+++ src/test/java/com/alibaba/json/bvt/issue_2200/issue2224_5/MA2StringGroupedCollection.java
@@ -0,0 +1,6 @@
+package com.alibaba.json.bvt.issue_2200.issue2224_5;
+
+import java.util.Map;
+
+abstract class MA2StringGroupedCollection<TItem> extends MA2GroupedCollection<TItem, Map<TItem, TItem[]>> {
+}

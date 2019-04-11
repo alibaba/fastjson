4d83150f45bb92fb9aafc834f70cf1fffa0fe5c0
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2019-04-11 10:42:07 +0800

    add new method JSON.toString(SerializerFeature...), for issue #2074

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -950,6 +950,20 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         }
     }
 
+    /**
+     * @since 1.2.57
+     */
+    public String toString(SerializerFeature... features) {
+        SerializeWriter out = new SerializeWriter(null, JSON.DEFAULT_GENERATE_FEATURE, features);
+
+        try {
+            new JSONSerializer(out).write(this);
+            return out.toString();
+        } finally {
+            out.close();
+        }
+    }
+
     public void writeJSONString(Appendable appendable) {
         SerializeWriter out = new SerializeWriter();
         try {

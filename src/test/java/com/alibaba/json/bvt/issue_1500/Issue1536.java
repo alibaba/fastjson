435fec0d1def75f256b97c512443507a2e52ba9d
TommyLemon <tommylemon@qq.com>
2019-04-12 15:35:19 +0800

    optimize testcase and solution for PullRequest alibaba#1536

--- src/test/java/com/alibaba/json/bvt/issue_1500/Issue1536.java
+++ src/test/java/com/alibaba/json/bvt/issue_1500/Issue1536.java
@@ -8,33 +8,36 @@ import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.serializer.PropertyFilter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
-import apijson.demo.server.model.RealmBean;
 import io.realm.Realm;
 import io.realm.RealmMigration;
 import io.realm.RealmSchema;
 import io.realm.internal.OsSharedRealm;
 import io.realm.internal.RealmNotifier;
-import zuo.biao.apijson.Log;
+import io.realm.RealmList;
+import io.realm.RealmObject;
 
+import java.io.Serializable;
 import java.util.List;
+
 
 /**解决序列化热门数据库 Realm 崩溃 
  * https://github.com/alibaba/fastjson/issues/1536
- * @author TommyLemon
+ * @author http://github.com/TommyLemon
  */
 public class Issue1536 {
 	private static final String TAG = "JSON";
 
-	
+
 	public static void main(String[] args) {
 		RealmBean bean = new RealmBean();
 		bean.setId(123L);
 		bean.setUrl("https://github.com/TommyLemon/APIJSON");
-		
+
 		Assert.assertEquals("{\"id\":123,\"url\":\"https://github.com/TommyLemon/APIJSON\"}", toJSONString(bean));
 	}
-	
-	
+
+
+
 	/**实体类转json
 	 * @param obj
 	 * @return
@@ -43,7 +46,6 @@ public class Issue1536 {
 		if (obj instanceof String) {
 			return (String) obj;
 		}
-    
 		try {
 			PropertyFilter filter = new PropertyFilter() {
 				@Override
@@ -62,18 +64,46 @@ public class Issue1536 {
 							|| name.equalsIgnoreCase("valid")
 							|| name.equalsIgnoreCase("managed")
 							){
-						//false表示tel字段将被排除在外
 						return false;
 					}
+					
 					return true;
 				}
 			};
 
 			return com.alibaba.fastjson.JSON.toJSONString(obj, filter);
 		} catch (Exception e) {
-			Log.e(TAG, "toJSONString  catch \n" + e.getMessage());
+			System.out.println(TAG + ".toJSONString  catch \n" + e.getMessage());
 		}
 		return null;
 	}
-  
+
+
+	/**
+	 * 测试 JavaBean
+	 */
+	public class RealmBean extends RealmObject implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		private Long   id;
+		private String url;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+	}
+
 }

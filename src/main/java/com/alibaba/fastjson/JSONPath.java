f642994789d02dfbc7cdbe8d50b7fb1c777920f5
高铁 <shaojin.wensj@alibaba-inc.com>
2019-06-28 09:27:43 +0800

    jsonpath support new api reserve

--- src/main/java/com/alibaba/fastjson/JSONPath.java
+++ src/main/java/com/alibaba/fastjson/JSONPath.java
@@ -3864,7 +3864,25 @@ public class JSONPath implements JSONAware {
         return JSON.toJSONString(path);
     }
 
-    public static Object reserve(Object object, String... paths) {
+    public static Object reserveToArray(Object object, String... paths) {
+        JSONArray reserved = new JSONArray();
+
+        if (paths == null || paths.length == 0) {
+            return reserved;
+        }
+
+        for (String item : paths) {
+            JSONPath path = JSONPath.compile(item);
+            path.init();
+
+            Object value = path.eval(object);
+            reserved.add(value);
+        }
+
+        return reserved;
+    }
+
+    public static Object reserveToObject(Object object, String... paths) {
         if (paths == null || paths.length == 0) {
             return object;
         }

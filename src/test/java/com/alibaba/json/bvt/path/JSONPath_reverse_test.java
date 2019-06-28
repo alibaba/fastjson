08373a8a41d5412270d3a34589afeee865829811
高铁 <shaojin.wensj@alibaba-inc.com>
2019-06-28 09:27:43 +0800

    jsonpath support new api reserve

--- src/test/java/com/alibaba/json/bvt/path/JSONPath_reverse_test.java
+++ src/test/java/com/alibaba/json/bvt/path/JSONPath_reverse_test.java
@@ -10,7 +10,15 @@ public class JSONPath_reverse_test extends TestCase
     public void test_reserve() throws Exception {
         JSONObject object = JSON.parseObject("{\"id\":1001,\"name\":\"ljw\",\"age\":50}");
 
-        assertEquals("{\"id\":1001,\"name\":\"ljw\"}", JSONPath.reserve(object, "id", "name").toString());
-        assertEquals("{\"name\":\"ljw\",\"id\":1001}", JSONPath.reserve(object, "name", "id").toString());
+        assertEquals("[1001,\"ljw\"]", JSONPath.reserveToArray(object, "id", "name").toString());
+        assertEquals("[\"ljw\",1001]", JSONPath.reserveToArray(object, "name", "id").toString());
+        assertEquals("[\"ljw\",[\"ljw\",1001,50]]", JSONPath.reserveToArray(object, "name", "*").toString());
+    }
+
+    public void test_reserve2() throws Exception {
+        JSONObject object = JSON.parseObject("{\"id\":1001,\"name\":\"ljw\",\"age\":50}");
+
+        assertEquals("{\"id\":1001,\"name\":\"ljw\"}", JSONPath.reserveToObject(object, "id", "name").toString());
+        assertEquals("{\"name\":\"ljw\",\"id\":1001}", JSONPath.reserveToObject(object, "name", "id").toString());
     }
 }

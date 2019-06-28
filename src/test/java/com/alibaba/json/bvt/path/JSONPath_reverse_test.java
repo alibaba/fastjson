c9abb11051242e8436101fc7dbc4a6436211b86c
高铁 <shaojin.wensj@alibaba-inc.com>
2019-06-28 09:36:23 +0800

    jsonpath support new api reserve

--- src/test/java/com/alibaba/json/bvt/path/JSONPath_reverse_test.java
+++ src/test/java/com/alibaba/json/bvt/path/JSONPath_reverse_test.java
@@ -21,4 +21,14 @@ public class JSONPath_reverse_test extends TestCase
         assertEquals("{\"id\":1001,\"name\":\"ljw\"}", JSONPath.reserveToObject(object, "id", "name").toString());
         assertEquals("{\"name\":\"ljw\",\"id\":1001}", JSONPath.reserveToObject(object, "name", "id").toString());
     }
+
+
+    public void test_reserve3() throws Exception {
+        JSONObject object = JSON.parseObject("{\"player\":{\"id\":1001,\"name\":\"ljw\",\"age\":50}}");
+
+        assertEquals("{\"player\":{\"name\":\"ljw\",\"id\":1001}}", JSONPath.reserveToObject(object, "player.id", "player.name").toString());
+        assertEquals("{\"player\":{\"name\":\"ljw\",\"id\":1001}}", JSONPath.reserveToObject(object, "player.name", "player.id").toString());
+    }
+
+
 }

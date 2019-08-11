fa77171b3c8860c23acb85f9571dac72dacb6e5b
高铁 <shaojin.wensj@alibaba-inc.com>
2019-08-12 01:22:25 +0800

    add test for file

--- src/test/java/com/alibaba/fastjson/validate/JSONValidateTest_file.java
+++ src/test/java/com/alibaba/fastjson/validate/JSONValidateTest_file.java
@@ -2,7 +2,9 @@ package com.alibaba.fastjson.validate;
 
 import com.alibaba.fastjson.JSONValidator;
 import junit.framework.TestCase;
+import org.apache.commons.io.FileUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -24,6 +26,25 @@ public class JSONValidateTest_file extends TestCase
             System.out.println("millis " + (System.currentTimeMillis() - start));
         }
     }
+
+    public void test_for_file2() throws Exception {
+        File file = new File("/Users/wenshao/Downloads/000002_0.json");
+        byte[] bytes = FileUtils.readFileToByteArray(file);
+
+        for (int i = 0; i < 10; ++i) {
+            long start = System.currentTimeMillis();
+
+            ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+
+            JSONValidator validator = JSONValidator.fromUtf8(is);
+            assertTrue(validator.validate());
+            validator.close();
+
+            // 642 335 796
+            System.out.println("millis " + (System.currentTimeMillis() - start));
+        }
+    }
+
 
     public void test_for_fileReader() throws Exception {
         for (int i = 0; i < 10; ++i) {

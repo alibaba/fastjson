619c09aeb0ec9a7058e59de5e7fe2c5d2c38e4dd
yangchao16 <yangchao16@meituan.com>
2019-08-17 10:32:45 +0800

    change

--- src/test/java/com/alibaba/fastjson/deserializer/Issue2639/TestIssue2639.java
+++ src/test/java/com/alibaba/fastjson/deserializer/Issue2639/TestIssue2639.java
@@ -1,9 +1,25 @@
 package com.alibaba.fastjson.deserializer.Issue2639;
 
+import com.alibaba.fastjson.JSONArray;
+import org.junit.Test;
+
 /**
- * @Author:Yangchao16
+ * @Author:Yangchao16  chaoyang_sjtu@126.com
  * @Description:
  * @Data:Initialized in 10:30 AM 2019/8/17
  **/
 public class TestIssue2639 {
+
+    @Test
+    public  void test_Invlide(){
+
+//        String str="{\"name\":\"yangchao\",\"age\":18}";
+
+        String str="]";
+
+        Person person = JSONArray.parseObject(str, Person.class);
+
+        System.out.println(person);
+
+    }
 }

faf5b6d7f33992eb324481ca9b57a30fb9524144
chenjiechuan <654815312@qq.com>
2019-08-26 17:18:19 +0800

    Testcases added

---
+++ src/test/java/com/alibaba/json/bvt/mixins/MixinInheritanceTest.java
@@ -0,0 +1,64 @@
+package com.alibaba.json.bvt.mixins;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import junit.framework.TestCase;
+
+public class MixinInheritanceTest
+    extends TestCase
+{
+    static class Beano {
+        public int ido = 42;
+        public String nameo = "Bob";
+    }
+
+    static class BeanoMixinSuper {
+        @JSONField(name = "name")
+        public String nameo;
+    }
+
+    static class BeanoMixinSub extends BeanoMixinSuper {
+        @JSONField(name = "id")
+        public int ido;
+    }
+
+    static class Beano2 {
+        public int getIdo() { return 13; }
+        public String getNameo() { return "Bill"; }
+    }
+
+    static abstract class BeanoMixinSuper2 extends Beano2 {
+        @Override
+        @JSONField(name = "name")
+        public abstract String getNameo();
+    }
+
+    static abstract class BeanoMixinSub2 extends BeanoMixinSuper2 {
+        @Override
+        @JSONField(name = "id")
+        public abstract int getIdo();
+    }
+
+    public void test_field() throws Exception {
+        JSON.addMixIn(Beano.class, BeanoMixinSub.class);
+        String str = JSON.toJSONString(new Beano());
+        JSONObject result = JSONObject.parseObject(str);
+        assertEquals(2, result.size());
+        if (!result.containsKey("id")
+                || !result.containsKey("name")) {
+            fail("Should have both 'id' and 'name', but content = " + result);
+        }
+        JSON.removeMixIn(Beano.class);
+    }
+
+    public void test_method() throws Exception {
+        JSON.addMixIn(Beano2.class, BeanoMixinSub2.class);
+        String str = JSON.toJSONString(new Beano2());
+        JSONObject result = JSONObject.parseObject(str);
+        assertEquals(2, result.size());
+        assertTrue(result.containsKey("id"));
+        assertTrue(result.containsKey("name"));
+        JSON.removeMixIn(Beano2.class);
+    }
+}

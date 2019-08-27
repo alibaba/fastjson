f8b7b79f93cdb0484c98e6237fe989d8a56694ab
Omega-Ariston <654815312@qq.com>
2019-08-27 19:03:02 +0800

    add more testcase

--- src/test/java/com/alibaba/json/bvt/issue_2300/Issue2346.java
+++ src/test/java/com/alibaba/json/bvt/issue_2300/Issue2346.java
@@ -27,4 +27,57 @@ public class Issue2346 extends TestCase {
 
         }
     }
+
+    @JSONType(builder = TestEntity2.TestEntity2Builder.class)
+    @Getter
+    public static class TestEntity2 {
+        private String name;
+
+        private int age;
+
+        @JSONPOJOBuilder(withPrefix = "www")
+        public static class TestEntity2Builder{
+            private TestEntity2 testEntity2 = new TestEntity2();
+
+            public TestEntity2 build(){
+                return testEntity2;
+            }
+
+            public TestEntity2Builder wwwAge(int age) {
+                testEntity2.age = age;
+                return this;
+            }
+
+            public TestEntity2Builder wwwName(String name) {
+                testEntity2.name = name;
+                return this;
+            }
+        }
+    }
+
+    @JSONType(builder = TestEntity3.TestEntity3Builder.class)
+    @Getter
+    public static class TestEntity3 {
+        private String name;
+
+        private int age;
+
+        public static class TestEntity3Builder{
+            private TestEntity3 testEntity3 = new TestEntity3();
+
+            public TestEntity3 build(){
+                return testEntity3;
+            }
+
+            public TestEntity3Builder withAge(int age) {
+                testEntity3.age = age;
+                return this;
+            }
+
+            public TestEntity3Builder withName(String name) {
+                testEntity3.name = name;
+                return this;
+            }
+        }
+    }
 }

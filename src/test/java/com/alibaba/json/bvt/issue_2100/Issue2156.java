f18f44fbf1d0434373c0be211aba65618cc9663c
高铁 <shaojin.wensj@alibaba-inc.com>
2019-09-19 20:36:21 +0800

    add testcase

--- src/test/java/com/alibaba/json/bvt/issue_2100/Issue2156.java
+++ src/test/java/com/alibaba/json/bvt/issue_2100/Issue2156.java
@@ -3,6 +3,7 @@ package com.alibaba.json.bvt.issue_2100;
 import com.alibaba.fastjson.JSON;
 import junit.framework.TestCase;
 
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -13,6 +14,24 @@ public class Issue2156 extends TestCase {
     }
 
     public void test_for_issue() throws Exception {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        sf.setTimeZone(JSON.defaultTimeZone);
+        java.sql.Date date = new java.sql.Date(sf.parse("2018-07-15").getTime());
+        String str = JSON.toJSONStringWithDateFormat(date, JSON.DEFFAULT_DATE_FORMAT);
+        assertEquals("\"2018-07-15\"", str);
+    }
+
+    public void test_for_issue1() throws Exception {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        sf.setTimeZone(JSON.defaultTimeZone);
+        java.sql.Date date = new java.sql.Date(sf.parse("2018-07-15").getTime());
+        String str = JSON.toJSONStringWithDateFormat(date, JSON.DEFFAULT_DATE_FORMAT);
+        assertEquals("\"2018-07-15\"", str);
+    }
+
+    public void test_for_issue2() throws Exception {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        sf.setTimeZone(JSON.defaultTimeZone);
         java.sql.Date date = java.sql.Date.valueOf("2018-07-15");
         String str = JSON.toJSONStringWithDateFormat(date, JSON.DEFFAULT_DATE_FORMAT);
         assertEquals("\"2018-07-15\"", str);

a08711109489846903176988129e0f96a07d0f8c
高铁 <shaojin.wensj@alibaba-inc.com>
2019-10-07 22:42:00 +0800

    update version to 1.2.63

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -1284,5 +1284,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         return null;
     }
 
-    public final static String VERSION = "1.2.62";
+    public final static String VERSION = "1.2.63";
 }

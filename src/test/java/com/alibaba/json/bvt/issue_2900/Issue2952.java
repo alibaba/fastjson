1e69c7188313194297be397539563f81957673ff
auntyellow <webmaster@xqbase.com>
2019-12-26 16:10:44 +0800

    fix issue #2952

---
+++ src/test/java/com/alibaba/json/bvt/issue_2900/Issue2952.java
@@ -0,0 +1,34 @@
+package com.alibaba.json.bvt.issue_2900;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import junit.framework.TestCase;
+
+public class Issue2952 extends TestCase {
+    public void test_for_issue() throws Exception {
+        SerializeConfig serializeConfig = new SerializeConfig();
+        serializeConfig.setAsmEnable(true);
+        assertEquals("{\"l\":null,\"s\":null,\"b\":null,\"i\":null,\"o\":null}",
+                JSON.toJSONString(new Pojo(), serializeConfig,
+                        SerializerFeature.WriteNullListAsEmpty,
+                        SerializerFeature.WriteNullStringAsEmpty,
+                        SerializerFeature.WriteNullBooleanAsFalse,
+                        SerializerFeature.WriteNullNumberAsZero));
+    }
+
+    public static class Pojo {
+        @JSONField(serialzeFeatures=SerializerFeature.WriteMapNullValue, ordinal=0)
+        public Object[] l;
+        @JSONField(serialzeFeatures=SerializerFeature.WriteMapNullValue, ordinal=1)
+        public String s;
+        @JSONField(serialzeFeatures=SerializerFeature.WriteMapNullValue, ordinal=2)
+        public Boolean b;
+        @JSONField(serialzeFeatures=SerializerFeature.WriteMapNullValue, ordinal=3)
+        public Integer i;
+        @JSONField(serialzeFeatures=SerializerFeature.WriteMapNullValue, ordinal=4)
+        public Object o;
+    }
+}

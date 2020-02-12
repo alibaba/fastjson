0e5ab3b7f3999cf97bb9ab1029fbfc1b36e1704f
zeyuuuuuuuu <zeyu.huang0610@gmail.com>
2020-02-11 23:10:12 -0800

    Homework2

---
+++ src/test/java/com/wheelchair/validate/JSONValidatorTest.java
@@ -0,0 +1,13 @@
+package com.skateboard2wheelchair.fastjson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JSONValidatorTest {
+
+    @Test
+    public void validate throws Exception() {
+
+    }
+}

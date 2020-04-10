8a616c26f7ba5d03174b2d6d9766ca30d699e7f4
高铁 <shaojin.wensj@alibaba-inc.com>
2020-04-11 02:50:31 +0800

    improved geojson support

--- src/main/java/com/alibaba/fastjson/support/geo/Point.java
+++ src/main/java/com/alibaba/fastjson/support/geo/Point.java
@@ -1,5 +1,6 @@
 package com.alibaba.fastjson.support.geo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.annotation.JSONType;
 
 /**
@@ -7,17 +8,50 @@ import com.alibaba.fastjson.annotation.JSONType;
  */
 @JSONType(typeName = "Point", orders = {"type", "bbox", "coordinates"})
 public class Point extends Geometry {
-    private double[] coordinates;
+    private double longitude;
+    private double latitude;
 
     public Point() {
         super("Point");
     }
 
     public double[] getCoordinates() {
-        return coordinates;
+        return new double[] {longitude, latitude};
     }
 
     public void setCoordinates(double[] coordinates) {
-        this.coordinates = coordinates;
+        if (coordinates == null || coordinates.length == 0) {
+            this.longitude = 0;
+            this.latitude = 0;
+            return;
+        }
+
+        if (coordinates.length == 1) {
+            this.longitude = coordinates[0];
+            return;
+        }
+
+        this.longitude = coordinates[0];
+        this.latitude = coordinates[1];
+    }
+
+    @JSONField(serialize = false)
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @JSONField(serialize = false)
+    public double getLatitude() {
+        return latitude;
+    }
+
+    @JSONField(deserialize = false)
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @JSONField(deserialize = false)
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 }

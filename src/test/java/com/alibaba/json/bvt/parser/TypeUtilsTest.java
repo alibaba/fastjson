cb5760068cbbe6d97f0fc75e1b42cb71aac0fbde
zhibo.liu <zhibo6@aliyun.com>
2020-04-15 11:24:10 +0800

    删除一个多余的换行

--- src/test/java/com/alibaba/json/bvt/parser/TypeUtilsTest.java
+++ src/test/java/com/alibaba/json/bvt/parser/TypeUtilsTest.java
@@ -244,7 +244,6 @@ public class TypeUtilsTest extends TestCase {
         Assert.assertEquals(null, TypeUtils.castToTimestamp(null));
     }
 
-
     public void test_cast_to_Timestamp_1970_01_01_00_00_00() throws Exception {
         Assert.assertEquals(new Timestamp(0), TypeUtils.castToTimestamp("1970-01-01 08:00:00"));
     }

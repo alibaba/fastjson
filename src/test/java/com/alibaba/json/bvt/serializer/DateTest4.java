fcbb09017349e0805f1d95e281ada1513d8002cc
高铁 <shaojin.wensj@alibaba-inc.com>
2020-06-26 21:49:32 +0800

    add testcase

---
+++ src/test/java/com/alibaba/json/bvt/serializer/DateTest4.java
@@ -0,0 +1,36 @@
+package com.alibaba.json.bvt.serializer;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+public class DateTest4 extends TestCase {
+
+    public void test_date() throws Exception {
+        assertNotNull(
+                JSON.parseObject(
+                        "{\"gmtCreate\":\"1970-01-01 00:00:00\"}"
+                        , VO.class)
+                        .gmtCreate
+        );
+
+        assertNotNull(
+                JSON.parseObject(
+                        "{\"gmtCreate\":\"1970-01-01 00:00:00.000\"}"
+                        , VO.class)
+                        .gmtCreate
+        );
+
+        assertNotNull(
+                JSON.parseObject(
+                        "{\"gmtCreate\":\"1960-01-01 00:00:00.000\"}"
+                        , VO.class)
+                        .gmtCreate
+        );
+    }
+
+    public static class VO {
+        public Date gmtCreate;
+    }
+}

b4e0d7253f7b9b27989ac7f6eda72e302fe8f7f0
nanqi <yanjialong@hotmail.com>
2020-06-28 11:32:49 +0800

    Support pars Constructor & Setters

---
+++ src/test/java/com/alibaba/json/bvt/parser/ClassConstructorTest1.java
@@ -0,0 +1,39 @@
+package com.alibaba.json.bvt.parser;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+import org.springframework.util.Assert;
+
+public class ClassConstructorTest1 extends TestCase {
+    public void test_error() throws Exception {
+        String modelJSON = "{\"age\":12, \"name\":\"nanqi\"}";
+        Model model = JSON.parseObject(modelJSON, Model.class);
+        Assert.notNull(model.getName());
+    }
+
+    public static class Model {
+        public Model(int age) {
+            this.age = age;
+        }
+
+        private String name;
+
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+}

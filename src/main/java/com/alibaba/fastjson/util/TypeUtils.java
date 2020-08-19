e3dc4bfdbc085b9e3877a19a7b5151bfa314c6d4
暮光：城中城 <806783409@qq.com>
2020-08-19 11:23:46 +0800

    增加测试用例

--- src/main/java/com/alibaba/fastjson/util/TypeUtils.java
+++ src/main/java/com/alibaba/fastjson/util/TypeUtils.java
@@ -327,13 +327,10 @@ public class TypeUtils{
         if (value instanceof BigInteger) {
             return new BigDecimal((BigInteger) value);
         }
-        String strVal = value.toString();
-        if (strVal.length() == 0) {
-            return null;
-        }
         if (value instanceof Map && ((Map) value).size() == 0) {
             return null;
         }
+        String strVal = value.toString();
         if (strVal.length() > 65535) {
             throw new JSONException("decimal overflow");
         }

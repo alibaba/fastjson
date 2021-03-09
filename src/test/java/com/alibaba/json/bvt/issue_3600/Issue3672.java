36da94f66f3c4e7d45340c2df66984bccd5bb641
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-03-09 22:07:38 +0800

    Add Test Case for ensure that Except '\n','\t',' ', The JSON.toJSONString(Object) and it's addition of PrettyFormat are same.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
+++ src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
@@ -5,14 +5,23 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.google.common.collect.Lists;
 import lombok.Data;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class Issue3672 {
-    @Test
-    public void test1() {
-        Issue3672Root root = new Issue3672Root();
+    private static Issue3672Root root;
+    private static Issue3672Root2 root2;
+
+    @BeforeClass
+    public static void onlyOnce() {
+        init_root();
+        init_root2();
+    }
+
+    public static void init_root() {
+        root = new Issue3672Root();
         Issue3672A a = new Issue3672A();
         Issue3672B b = new Issue3672B();
         Issue3672C c = new Issue3672C();
@@ -22,58 +31,74 @@ public class Issue3672 {
         b.setC(c);
         c.setD(d);
         d.setE(Lists.newArrayList(c));
+    }
+
+    public static void init_root2() {
+        root2 = new Issue3672Root2();
+        Issue3672A2 a = new Issue3672A2();
+        Issue3672B b = new Issue3672B();
+        Issue3672C c = new Issue3672C();
+        Issue3672D d = new Issue3672D();
+        root2.setA(a);
+        a.setB(Lists.newArrayList(b).toArray(new Issue3672B[0]));
+        b.setC(c);
+        c.setD(d);
+        d.setE(Lists.newArrayList(c));
+    }
+
+    @Test
+    public void test1() {
         Assert.assertFalse(JSON.toJSONString(root).contains("null"));
         Assert.assertFalse(JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
     }
 
     @Test
     public void test2() {
-        Issue3672Root2 root = new Issue3672Root2();
-        Issue3672A2 a = new Issue3672A2();
-        Issue3672B b = new Issue3672B();
-        Issue3672C c = new Issue3672C();
-        Issue3672D d = new Issue3672D();
-        root.setA(a);
-        a.setB(Lists.newArrayList(b).toArray(new Issue3672B[0]));
-        b.setC(c);
-        c.setD(d);
-        d.setE(Lists.newArrayList(c));
-        Assert.assertFalse(JSON.toJSONString(root).contains("null"));
-        Assert.assertFalse(JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
+        Assert.assertFalse(JSON.toJSONString(root2).contains("null"));
+        Assert.assertFalse(JSON.toJSONString(root2, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
+    }
+
+    @Test
+    public void test_pretty_order_same_with_normal() {
+        String removeELements = "[\t\n ]";
+        Assert.assertEquals(JSON.toJSONString(root).replaceAll(removeELements, "")
+                , JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).replaceAll(removeELements, ""));
+        Assert.assertEquals(JSON.toJSONString(root2).replaceAll(removeELements, "")
+                , JSON.toJSONString(root2, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).replaceAll(removeELements, ""));
     }
 
     @Data
-    private class Issue3672Root {
+    private static class Issue3672Root {
         private Issue3672A a;
     }
 
     @Data
-    private class Issue3672Root2 {
+    private static class Issue3672Root2 {
         private Issue3672A2 a;
     }
 
     @Data
-    private class Issue3672A {
+    private static class Issue3672A {
         private Object[] b;
     }
 
     @Data
-    private class Issue3672A2 {
+    private static class Issue3672A2 {
         private Issue3672B[] b;
     }
 
     @Data
-    private class Issue3672B {
+    private static class Issue3672B {
         private Issue3672C c;
     }
 
     @Data
-    private class Issue3672C {
+    private static class Issue3672C {
         private Issue3672D d;
     }
 
     @Data
-    private class Issue3672D {
+    private static class Issue3672D {
         private ArrayList<Issue3672C> e;
     }
 

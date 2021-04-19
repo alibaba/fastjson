e4acabbec78e9bd07636ec4a06d6fa6913d60c74
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-20 01:25:58 +0800

    add testcase for issue #3721
    </subject>
    
    Branch: issue3721
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [x] New Test case
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    PS: this's ci will fail.
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ src/test/java/com/alibaba/json/bvt/issue_3700/Issue3721.kt
@@ -0,0 +1,31 @@
+package com.alibaba.json.bvt.issue_3700
+
+import com.alibaba.fastjson.JSON
+import org.junit.Test
+
+class Issue3721 {
+    @Test
+    fun kotlinFunSerializeErrorTest() {
+        val temp = fun() {};
+        try {
+            /**
+             *当执行该序列化时，会抛出异常，该异常会设置TypeUtils类中的静态变量kotlin_error变更为true,导致系统中所有的对象的反序列化出现
+             * com.alibaba.fastjson.JSONException: default constructor not found.异常
+             */
+            JSON.toJSONString(temp)
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
+
+
+        val toJSONString = JSON.toJSONString(ObjTest(1))
+        val parseObject = JSON.parseObject(toJSONString, ObjTest::class.java)
+
+        println(parseObject)
+
+    }
+
+    inner class ObjTest(val id: Long) {
+
+    }
+}

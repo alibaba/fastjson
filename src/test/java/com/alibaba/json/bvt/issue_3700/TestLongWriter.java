9534d9d50ba291e2fccff1c78b9bd4d90d044bf2
Tloops <11813207@mail.sustech.edu.cn>
2021-04-20 23:50:02 +0800

    move test file to package issue_3700

--- src/test/java/com/alibaba/json/bvt/issue_3700/TestLongWriter.java
+++ src/test/java/com/alibaba/json/bvt/issue_3700/TestLongWriter.java
@@ -1,4 +1,4 @@
-package com.alibaba.fastjson.serializer.issue3720;
+package com.alibaba.json.bvt.issue_3700;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializeConfig;

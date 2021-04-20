be63d71af1ac192ab51963f8858abee1a6a1fa07
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-20 20:36:15 +0800

    improve performance.
    </subject>
    
    Branch: issue3721
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3000/Issue3083.kt
+++ src/test/java/com/alibaba/json/bvt/issue_3000/Issue3083.kt
@@ -1,10 +1,15 @@
 package com.alibaba.json.bvt.issue_3000
 
 import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.annotation.JSONField
+import com.alibaba.fastjson.parser.ParserConfig
+import lombok.Data
+import org.junit.AfterClass
 import org.junit.Assert
+import org.junit.BeforeClass
 import org.junit.Test
 
-class TestBean {
+class Issue3083TestBean {
     var is_subscribe = 0
     var subscribe = 0
     var isHave = 0
@@ -12,19 +17,39 @@ class TestBean {
     var normal = 0
     var Abnormal = 0
 }
+class Issue3083TestBean2 {
+    var isOk: Int? = 0
+}
+
 class Issue3083 {
     private val s = "{'is_subscribe':1,'subscribe':2,'isHave':3, '_have':4, 'normal':5, 'Abnormal':6}"
-    private val s2 = "{\"is_subscribe\":1,\"subscribe\":2,\"isHave\":3, \"_have\":4, \"normal\":5, \"Abnormal\":6}"
-    // TODO
-    @Test
+    private val s2 =
+        "{\"is_subscribe\":1,\"subscribe\":2,\"isHave\":3, \"_have\":4, \"normal\":5, \"Abnormal\":6}"
+    private val s3083_1 = "{'isOk':3083}"
+    private val s3083_2 = "{\"isOk\":3083}"
+    private val test_strs = arrayOf(s, s2)
+    private val test_strs2 = arrayOf(s3083_1, s3083_2)
+
+    //@Test
     fun test_for_issue() {
-        val b = JSON.parseObject(s2, TestBean::class.java)
-        println("${b.is_subscribe}--${b.subscribe}--${b.isHave}--${b._have}--${b.normal}--${b.Abnormal}")
-        Assert.assertEquals(1,b.is_subscribe);
-        Assert.assertEquals(2,b.subscribe);
-        Assert.assertEquals(3,b.isHave);
-        Assert.assertEquals(4,b._have);
-        Assert.assertEquals(5,b.normal);
-        Assert.assertEquals(6,b.Abnormal);
+        for (test_str in test_strs) {
+            val b = JSON.parseObject(test_str, Issue3083TestBean::class.java);
+            println("${b.is_subscribe}--${b.subscribe}--${b.isHave}--${b._have}--${b.normal}--${b.Abnormal}")
+            Assert.assertEquals(1, b.is_subscribe);
+            Assert.assertEquals(2, b.subscribe);
+            Assert.assertEquals(3, b.isHave);
+            Assert.assertEquals(4, b._have);
+            Assert.assertEquals(5, b.normal);
+            Assert.assertEquals(6, b.Abnormal);
+        }
+    }
+
+    //@Test
+    fun test_for_issue2() {
+        for (test_str in test_strs2) {
+            val b = JSON.parseObject(test_str, Issue3083TestBean2::class.java);
+            Assert.assertEquals(3083, b.isOk);
+            //Assert.assertEquals(3083, b.getOk());
+        }
     }
 }
--- src/test/java/com/alibaba/json/bvt/issue_3700/Issue3721.kt
+++ src/test/java/com/alibaba/json/bvt/issue_3700/Issue3721.kt
@@ -2,30 +2,22 @@ package com.alibaba.json.bvt.issue_3700
 
 import com.alibaba.fastjson.JSON
 import org.junit.Test
+import kotlin.test.assertNotNull
 
 class Issue3721 {
     @Test
     fun kotlinFunSerializeErrorTest() {
         val temp = fun() {};
-        try {
-            /**
-             *当执行该序列化时，会抛出异常，该异常会设置TypeUtils类中的静态变量kotlin_error变更为true,导致系统中所有的对象的反序列化出现
-             * com.alibaba.fastjson.JSONException: default constructor not found.异常
-             */
-            JSON.toJSONString(temp)
-        } catch (ex: Exception) {
-            println(ex.message)
-        }
-
-
+        val tempStr = JSON.toJSONString(temp)
+        assertNotNull(tempStr)
         val toJSONString = JSON.toJSONString(ObjTest(1))
+        assertNotNull(toJSONString)
         val parseObject = JSON.parseObject(toJSONString, ObjTest::class.java)
-
+        assertNotNull(parseObject)
         println(parseObject)
-
     }
-
 }
+
 class ObjTest(val ids: Long) {
     // with kotlin 5009
     // without kotlin 5004

0b3bb2f9056a64ec8fadf2398ed7b8033e3ec5fc
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-20 12:40:42 +0800

    add build for the kotlin-testcase
    </subject>
    
    Branch: master
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [x] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    now kotlin testcase will be build will `mvn test`
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3000/Issue3083.kt
+++ src/test/java/com/alibaba/json/bvt/issue_3000/Issue3083.kt
@@ -1,6 +1,8 @@
 package com.alibaba.json.bvt.issue_3000
 
 import com.alibaba.fastjson.JSON
+import org.junit.Assert
+import org.junit.Test
 
 class TestBean {
     var is_subscribe = 0
@@ -10,9 +12,19 @@ class TestBean {
     var normal = 0
     var Abnormal = 0
 }
-
-fun main(args: Array<String>) {
-    val s = "{'is_subscribe':1,'subscribe':2,'isHave':3, '_have':4, 'normal':5, 'Abnormal':6}"
-    val b = JSON.parseObject(s, TestBean::class.java)
-    println("${b.is_subscribe}--${b.subscribe}--${b.isHave}--${b._have}--${b.normal}--${b.Abnormal}")
+class Issue3083 {
+    private val s = "{'is_subscribe':1,'subscribe':2,'isHave':3, '_have':4, 'normal':5, 'Abnormal':6}"
+    private val s2 = "{\"is_subscribe\":1,\"subscribe\":2,\"isHave\":3, \"_have\":4, \"normal\":5, \"Abnormal\":6}"
+    // TODO
+    //@Test
+    fun test_for_issue() {
+        val b = JSON.parseObject(s2, TestBean::class.java)
+        println("${b.is_subscribe}--${b.subscribe}--${b.isHave}--${b._have}--${b.normal}--${b.Abnormal}")
+        Assert.assertEquals(1,b.is_subscribe);
+        Assert.assertEquals(2,b.subscribe);
+        Assert.assertEquals(3,b.isHave);
+        Assert.assertEquals(4,b._have);
+        Assert.assertEquals(5,b.normal);
+        Assert.assertEquals(6,b.Abnormal);
+    }
 }

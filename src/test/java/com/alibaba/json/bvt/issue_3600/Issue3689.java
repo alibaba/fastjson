3dfc699a93462d51dc73e42beb55332c3f71d949
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-21 23:49:10 +0800

    rename testcases.
    </subject>
    
    Branch: issue3689
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3600/Issue3689.java
+++ src/test/java/com/alibaba/json/bvt/issue_3600/Issue3689.java
@@ -7,81 +7,81 @@ import org.junit.Test;
 public class Issue3689 {
 
     @Test(expected = JSONException.class)
-    public void test_without_type_0() {
+    public void test_without_type_0_meaningles_char() {
         JSONArray.parseArray("dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_1() {
+    public void test_without_type_1_meaningles_char() {
         JSONArray.parseArray("/dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_2() {
+    public void test_without_type_2_meaningles_char() {
         JSONArray.parseArray("//dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_3() {
+    public void test_without_type_3_meaningles_char() {
         JSONArray.parseArray("///dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_4() {
+    public void test_without_type_4_meaningles_char() {
         JSONArray.parseArray("////dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_5() {
+    public void test_without_type_5_meaningles_char() {
         JSONArray.parseArray("/////dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_without_type_6() {
+    public void test_without_type_6_meaningles_char() {
         JSONArray.parseArray("//////dfdfdf");
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_0() {
+    public void test_with_type_0_meaningles_char() {
         JSONArray.parseArray("dfdfdf", String.class);
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_1() {
+    public void test_with_type_1_meaningles_char() {
         JSONArray.parseArray("/dfdfdf", String.class);
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_2() {
+    public void test_with_type_2_meaningles_char() {
         JSONArray.parseArray("//dfdfdf", String.class);
 
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_3() {
+    public void test_with_type_3_meaningles_char() {
         JSONArray.parseArray("///dfdfdf", String.class);
 
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_4() {
+    public void test_with_type_4_meaningles_char() {
         JSONArray.parseArray("////dfdfdf", String.class);
 
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_5() {
+    public void test_with_type_5_meaningles_char() {
         JSONArray.parseArray("/////dfdfdf", String.class);
 
     }
 
     @Test(expected = JSONException.class)
-    public void test_with_type_6() {
+    public void test_with_type_6_meaningles_char() {
         JSONArray.parseArray("//////dfdfdf", String.class);
     }
 
     @Test
-    public void test_2() {
+    public void test_for_issue() {
         JSONArray.parseArray("[\"////dfdfdf\"]"); //不会抛异常
         JSONArray objects = JSONArray.parseArray("[\"dfdfdf\"]");//不会抛异常
         System.out.println(JSONArray.parseArray("[\"////dfdfdf\"]"));

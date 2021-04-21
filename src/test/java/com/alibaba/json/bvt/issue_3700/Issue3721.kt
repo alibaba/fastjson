2e0f8933c3d49560de2fe1e3c46535845764b65f
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-21 23:58:12 +0800

    add new testcase
    </subject>
    
    Branch: issue3721
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3700/Issue3721.kt
+++ src/test/java/com/alibaba/json/bvt/issue_3700/Issue3721.kt
@@ -16,6 +16,20 @@ class Issue3721 {
         assertNotNull(parseObject)
         println(parseObject)
     }
+    @Test
+    fun kotlinFunOneParamWithFunInsideSerializeErrorTest(){
+        // CS304 (manually written) Issue link: https://github.com/alibaba/fastjson/issues/3721
+        val temp = fun(x:Double){
+            val tempInside = fun (){}
+        }
+        val tempStr = JSON.toJSONString(temp)
+        assertNotNull(tempStr)
+        val toJSONString = JSON.toJSONString(ObjTest(1))
+        assertNotNull(toJSONString)
+        val parseObject = JSON.parseObject(toJSONString, ObjTest::class.java)
+        assertNotNull(parseObject)
+        println(parseObject)
+    }
 }
 
 class ObjTest(val ids: Long) {

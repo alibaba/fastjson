9ad9cdc2c4a13c094b80d4257bf927ef4f2b5abc
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-04-22 00:16:40 +0800

    fix error
    </subject>
    
    Branch: issue3672
    
    <type>:
    - [x] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
+++ src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
@@ -11,17 +11,12 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class Issue3672 {
-    private static Issue3672Root root;
-    private static Issue3672Root2 root2;
+    private Issue3672Root root;
+    private Issue3672Root2 root2;
 
-    @BeforeClass
-    public static void onlyOnce() {
-        init_root();
-        init_root2();
-    }
 
-    public static void init_root() {
-        root = new Issue3672Root();
+    public Issue3672Root init_root() {
+        Issue3672Root root = new Issue3672Root();
         Issue3672A a = new Issue3672A();
         Issue3672B b = new Issue3672B();
         Issue3672C c = new Issue3672C();
@@ -31,10 +26,11 @@ public class Issue3672 {
         b.setC(c);
         c.setD(d);
         d.setE(Lists.newArrayList(c));
+        return root;
     }
 
-    public static void init_root2() {
-        root2 = new Issue3672Root2();
+    public Issue3672Root2 init_root2() {
+        Issue3672Root2 root2 = new Issue3672Root2();
         Issue3672A2 a = new Issue3672A2();
         Issue3672B b = new Issue3672B();
         Issue3672C c = new Issue3672C();
@@ -44,22 +40,27 @@ public class Issue3672 {
         b.setC(c);
         c.setD(d);
         d.setE(Lists.newArrayList(c));
+        return root2;
     }
 
     @Test
     public void test_root_withObjectArray() {
+        root = init_root();
         Assert.assertFalse(JSON.toJSONString(root).contains("null"));
         Assert.assertFalse(JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
     }
 
     @Test
     public void test_root2_with_Issue3672BArray() {
+        root = init_root();
         Assert.assertFalse(JSON.toJSONString(root2).contains("null"));
         Assert.assertFalse(JSON.toJSONString(root2, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
     }
 
     @Test
     public void test_pretty_order_same_with_normal() {
+        root = init_root();
+        root2 = init_root2();
         String removeELements = "[\t\n ]";
         Assert.assertEquals(JSON.toJSONString(root).replaceAll(removeELements, "")
                 , JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).replaceAll(removeELements, ""));
@@ -78,7 +79,7 @@ public class Issue3672 {
     }
 
     @Data
-    privateclass Issue3672A {
+    private class Issue3672A {
         private Object[] b;
     }
 

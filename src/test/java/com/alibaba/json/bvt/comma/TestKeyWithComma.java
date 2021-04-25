af8c1e408efb9b5f8c740b7050dbd8f64b81c1a9
Zehua Zou <41586196+HuaHuaY@users.noreply.github.com>
2021-04-25 13:17:56 +0800

    Update TestKeyWithComma.java

--- src/test/java/com/alibaba/json/bvt/comma/TestKeyWithComma.java
+++ src/test/java/com/alibaba/json/bvt/comma/TestKeyWithComma.java
@@ -19,28 +19,67 @@ public class TestKeyWithComma {
             "       }\n" +
             "   }\n" +
             "}";
-    private static final String json_path2 = "$._aa.b_b.cc_";
-    private static final String json_path2_with_comma = "$.,aa.b,b.cc,";
+    private static final String json_path1 = "$._aa.b_b.cc_";
+    private static final String json_path1_with_comma = "$.,aa.b,b.cc,";
+
+    @Test
+    public void test_for_issue_1() {
+        String result = JSONPath.extract(short_json_str1, json_path1).toString();
+        Assert.assertEquals("dd", result);
+    }
+
+    @Test
+    public void test_for_issue_2() {
+        Assert.assertNull(JSONPath.extract(short_json_str1, json_path1_with_comma));
+    }
+
+    @Test
+    public void test_for_issue_3() {
+        Assert.assertNull(JSONPath.extract(short_json_str1_key_with_comma, json_path1));
+    }
+
+    @Test
+    public void test_for_issue_4() {
+        String result = JSONPath.extract(short_json_str1_key_with_comma, json_path1_with_comma).toString();
+        Assert.assertEquals("dd", result);
+    }
+
+    private static final String short_json_str2= "{\n" +
+            "   \"-_aa\":{\n" +
+            "      \"-b_b\":{\n" +
+            "           \"-cc_\":\"dd\"\n" +
+            "       }\n" +
+            "   }\n" +
+            "}";
+    private static final String short_json_str2_key_with_comma = "{\n" +
+            "   \"-,aa\":{\n" +
+            "      \"-b,b\":{\n" +
+            "           \"-cc,\":\"dd\"\n" +
+            "       }\n" +
+            "   }\n" +
+            "}";
+    private static final String json_path2 = "$.-_aa.-b_b.-cc_";
+    private static final String json_path2_with_comma = "$.-,aa.-b,b.-cc,";
 
     @Test
     public void test_for_issue_5() {
-        String result = JSONPath.extract(short_json_str1, json_path2).toString();
+        String result = JSONPath.extract(short_json_str2, json_path2).toString();
         Assert.assertEquals("dd", result);
     }
 
     @Test
     public void test_for_issue_6() {
-        Assert.assertNull(JSONPath.extract(short_json_str1, json_path2_with_comma));
+        Assert.assertNull(JSONPath.extract(short_json_str2, json_path2_with_comma));
     }
 
     @Test
     public void test_for_issue_7() {
-        Assert.assertNull(JSONPath.extract(short_json_str1_key_with_comma, json_path2));
+        Assert.assertNull(JSONPath.extract(short_json_str2_key_with_comma, json_path2));
     }
 
     @Test
     public void test_for_issue_8() {
-        String result = JSONPath.extract(short_json_str1_key_with_comma, json_path2_with_comma).toString();
+        String result = JSONPath.extract(short_json_str2_key_with_comma, json_path2_with_comma).toString();
         Assert.assertEquals("dd", result);
     }
 }

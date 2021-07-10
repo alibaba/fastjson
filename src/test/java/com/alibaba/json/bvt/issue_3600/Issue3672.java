aea8123b7d726777a78e599441f71bce144f5a5a
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-07-10 22:23:38 +0800

    add more-test-case and improve for #3672
    </subject>
    
    Branch: issue3672-2nd
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
+++ src/test/java/com/alibaba/json/bvt/issue_3600/Issue3672.java
@@ -1,18 +1,20 @@
 package com.alibaba.json.bvt.issue_3600;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.google.common.collect.Lists;
 import lombok.Data;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import junit.framework.TestCase;
+public class Issue3672 {
+    private Issue3672Root root;
+    private Issue3672Root2 root2;
 
-public class Issue3672 extends TestCase {
-    public void test_for_issue() throws Exception {
+
+    public Issue3672Root init_root() {
         Issue3672Root root = new Issue3672Root();
         Issue3672A a = new Issue3672A();
         Issue3672B b = new Issue3672B();
@@ -23,12 +25,46 @@ public class Issue3672 extends TestCase {
         b.setC(c);
         c.setD(d);
         d.setE(Lists.newArrayList(c));
-        String str1 = JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
-        String str2 = JSON.toJSONString(root);
+        return root;
+    }
 
-        JSONObject obj1 = JSON.parseObject(str1);
-        JSONObject obj2 = JSON.parseObject(str2);
-        assertEquals(obj1.toString(), obj2.toString());
+    public Issue3672Root2 init_root2() {
+        Issue3672Root2 root2 = new Issue3672Root2();
+        Issue3672A2 a = new Issue3672A2();
+        Issue3672B b = new Issue3672B();
+        Issue3672C c = new Issue3672C();
+        Issue3672D d = new Issue3672D();
+        root2.setA(a);
+        a.setB(Lists.newArrayList(b).toArray(new Issue3672B[0]));
+        b.setC(c);
+        c.setD(d);
+        d.setE(Lists.newArrayList(c));
+        return root2;
+    }
+
+    @Test
+    public void test_root_withObjectArray() {
+        root = init_root();
+        Assert.assertFalse(JSON.toJSONString(root).contains("null"));
+        Assert.assertFalse(JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
+    }
+
+    @Test
+    public void test_root2_with_Issue3672BArray() {
+        root2 = init_root2();
+        Assert.assertFalse(JSON.toJSONString(root2).contains("null"));
+        Assert.assertFalse(JSON.toJSONString(root2, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).contains("null"));
+    }
+
+    @Test
+    public void test_pretty_order_same_with_normal() {
+        root = init_root();
+        root2 = init_root2();
+        String removeELements = "[\t\n ]";
+        Assert.assertEquals(JSON.toJSONString(root).replaceAll(removeELements, "")
+                , JSON.toJSONString(root, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).replaceAll(removeELements, ""));
+        Assert.assertEquals(JSON.toJSONString(root2).replaceAll(removeELements, "")
+                , JSON.toJSONString(root2, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue).replaceAll(removeELements, ""));
     }
 
     @Data
@@ -37,8 +73,18 @@ public class Issue3672 extends TestCase {
     }
 
     @Data
+    private class Issue3672Root2 {
+        private Issue3672A2 a;
+    }
+
+    @Data
     private class Issue3672A {
         private Object[] b;
+    }
+
+    @Data
+    private class Issue3672A2 {
+        private Issue3672B[] b;
     }
 
     @Data

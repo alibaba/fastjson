560782c9ee12120304284ba98b61dc61e30324b3
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-02 14:16:19 +0800

    bug fix for autoType

--- src/test/java/com/alibaba/json/bvt/bug/Bug_for_Exception.java
+++ src/test/java/com/alibaba/json/bvt/bug/Bug_for_Exception.java
@@ -4,12 +4,18 @@ import junit.framework.TestCase;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Map;
+
 public class Bug_for_Exception extends TestCase {
 	public void test_exception() throws Exception {
 		RuntimeException ex = new RuntimeException("e1");
 		String text = JSON.toJSONString(ex);
 		System.out.println(text);
-		
-		RuntimeException ex2 = (RuntimeException) JSON.parse(text);
+
+
+		Object obj = JSON.parse(text);
+		assertTrue(obj instanceof Map);
+
+		RuntimeException ex2 = (RuntimeException) JSON.parseObject(text, Throwable.class);
 	}
 }

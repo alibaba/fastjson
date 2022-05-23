ae8ac867dc4225204f9441a7a06b8206bf3f9ede
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2022-05-23 08:55:52 +0800

    1.2.83

--- src/main/java/com/alibaba/fastjson/JSON.java
+++ src/main/java/com/alibaba/fastjson/JSON.java
@@ -1421,5 +1421,5 @@ public abstract class JSON implements JSONStreamAware, JSONAware {
         return null;
     }
 
-    public final static String VERSION = "1.2.76";
+    public final static String VERSION = "1.2.83";
 }

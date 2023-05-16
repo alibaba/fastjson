9763f5b707276cbdf01eab2ad466ad78f79120e9
S. Ezekiel Korede <59841476+zopeaj@users.noreply.github.com>
2023-05-16 14:00:21 +0100

    Update JSONMapper.java
    Update  JSONMapper file

--- src/main/java/com/alibaba/fastjson/JSONMapper.java
+++ src/main/java/com/alibaba/fastjson/JSONMapper.java
@@ -4,9 +4,19 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.InputStream;
+import java.net.URL;
+
 
 public class JSONMapper {
+  
+  public <T> readValue(InputStream src, JavaType Class<T> valueType) throws IOException, Exception {
+    InputStream input = new InputStream(src);
+  }
 
-
+  public String writeValueAsString(Object value) throws Exception {
+     String data = (String) value.toString();
+     return data;
+  }
 }
 
